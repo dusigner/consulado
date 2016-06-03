4d7c7e52f3f88f005fc4d09ee1e55bc52d7acb86
Marcela Afonso <marcela.bafonso3@gmail.com>
2016-06-03 17:54:49 -0300

    Formularios

--- src/scripts/custom/lead-newsletter.js
+++ src/scripts/custom/lead-newsletter.js
@@ -2,18 +2,60 @@ Nitro.module('lead-newsletter', function(){
 
 	'use strict';
 
-	var self = this;
+	var self = this,
+		$inputName = $('#form-newsletter input[name="name"]'),
+		$inputEmail = $('#form-newsletter input[name="email"]'),
+		$inputTermos = $inputTermos,
+		valid = false;
 
 	this.setup = function() {
-		$('.lead-newsletter .secondary-button').click(function(e){
+		$('.lead-newsletter #form-newsletter').submit(function(e){
 			e.preventDefault();
 
-			self.registerNewsletter();
+			self.validateForm();
+
+			return false;
 		});
 	};
 
-	this.registerNewsletter = function() {
-		console.log('Cadastrar newsletter');
+	this.validateForm = function() {
+		if($inputName.filter(':blank').length < 1 && $inputEmail.filter(':blank').length < 1 && $('input[type="checkbox"]').is(':checked')) {
+			valid = true;
+		} else {
+			if($inputName.filter(':blank').length >= 1 ) {
+				$inputName.addClass('error');
+			} else {
+				$inputEmail.removeClass('error');
+			}
+
+			if($inputEmail.filter(':blank').length >= 1 ) {
+				$inputEmail.addClass('error');
+			} else {
+				$inputEmail.removeClass('error');
+			}
+
+			if ( !$('input[type="checkbox"').is(':checked') ) {
+				$inputTermos.addClass('error');
+			} else {
+				$inputTermos.removeClass('error');
+			}
+		}
+
+		if(valid) {
+			var name = $inputName.val(),
+				email = $inputEmail.val(),
+				termos = true;
+
+			self.registerNewsletter(name,email,termos);
+		}
+	};
+
+	this.registerNewsletter = function(name,email,termos) {
+		var data = {};
+
+		data.name = name;
+		data.email = email;
+		data.termos = termos;
 
 		//quando finalizar
 		$('.lead-newsletter').addClass('success');
--- src/scripts/custom/modal.overlayLead.js
+++ src/scripts/custom/modal.overlayLead.js
@@ -1,25 +1,59 @@
+require('vendors/jquery.form-validator');
 require('vendors/vtex-modal-cookie');
 
 Nitro.module('modal.overlayLead', function(){
 
 	'use strict';
 
-	var self = this;
+	var self = this,
+		$inputName = $('#modal-overlay-leads input[name="name"]'),
+		$inputEmail = $('#modal-overlay-leads input[name="email"]'),
+		valid = false;
 
 	this.setup = function() {
 		$('#modal-overlay-leads').vtexModal({
 			cookieOptions: { expires: 0, path: '/' }
 		});
 
-		$('#vtex-modal-overlay-leads .secondary-button').click(function(e){
+		$('#vtex-modal-overlay-leads #form-newsletter-overlay').submit(function(e){
 			e.preventDefault();
 
-			self.registerNewsletter();
+			self.validateForm();
+
+			return false;
 		});
 	};
 
-	this.registerNewsletter = function() {
-		console.log('Cadastrar newsletter');
+	this.validateForm = function() {
+		if($inputName.filter(':blank').length < 1 && $inputEmail.filter(':blank').length < 1) {
+			valid = true;
+		} else {
+			if($inputName.filter(':blank').length >= 1 ) {
+				$inputName.addClass('error');
+			} else {
+				$inputName.removeClass('error');
+			}
+
+			if($inputEmail.filter(':blank').length >= 1 ) {
+				$inputEmail.addClass('error');
+			} else {
+				$inputEmail.removeClass('error');
+			}
+		}
+
+		if(valid) {
+			var name = $inputName.val(),
+				email = $inputEmail.val();
+
+			self.registerNewsletter(name,email);
+		}
+	};
+
+	this.registerNewsletter = function(name,email) {
+		var data = {};
+
+		data.name = name;
+		data.email = email;
 
 		//quando finalizar
 		$('#vtex-modal-overlay-leads .modal-holder').addClass('success');
