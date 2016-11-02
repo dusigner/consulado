71e651c805ea5cc02d3dc06ded11468234bbf401
Marcela Afonso <marcela.bafonso3@gmail.com>
2016-11-02 15:55:23 -0200

    PLAT-1544 Nova vitrine mobile

--- src/scripts/controllers/home.js
+++ src/scripts/controllers/home.js
@@ -8,34 +8,63 @@ require('custom/lead-newsletter');
 //require('custom/modal.blackfriday');
 
 Nitro.controller('home', ['slider-banner', 'modal.overlayLead', 'lead-newsletter'], function() {
+    var self = this,
+        $slider = $('.prateleira-slider .prateleira>ul').not('.slick-initialized');
 
-
-    var $slider = $('.prateleira-slider .prateleira ul').not('.product-field ul');
-
-    this.setupSlider = function() {
-
-        $slider.slick({
+    this.setupSlider = function($currentSlider) {
+        $currentSlider.not('.slick-initialized').slick({
             infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 4,
+            slidesToScroll: 4,
             responsive: [{
                 breakpoint: 1019,
                 settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }, {
+                breakpoint: 990,
+                settings: {
+                    dots: true,
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             }, {
                 breakpoint: 480,
                 settings: {
+                    dots: true,
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: true
+                    slidesToScroll: 1
                 }
             }]
         });
 
+        //ajusta para mobile - prateleira slider
+        $('.prateleira-slider .prateleira ul').find('.detalhes>a').addClass('col-xs-6 col-md-12');
+
     };
 
-    this.setupSlider();
 
+    //inicia automaticamente prateleiras sliders no desktop
+    if ($(window).width() > 768) {
+        self.setupSlider($slider);
+    }
+
+
+    //mobile - abrir vitrines
+    if ($(window).width() <= 768) {
+        $('.prateleira-slider .pre-title').click(function(e){
+            e.preventDefault();
+
+            $(this).toggleClass('open');
+
+            if ($(this).hasClass('open')) {
+                $(this).siblings().find('>ul').slideDown('slow',function(){
+                    self.setupSlider($(this));
+                });
+            } else {
+                $(this).siblings().find('>ul').slideUp();
+            }
+        });
+    }
 });
--- src/scripts/controllers/produto.js
+++ src/scripts/controllers/produto.js
@@ -79,24 +79,39 @@ Nitro.controller('produto', [ /*'video', */ 'sku-fetch', 'gallery', 'product-nav
     });
 
 
-    $('.prateleira-slider .prateleira ul').not('.product-field ul').slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [{
-            breakpoint: 1019,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
-    });
+    var $slider = $('.prateleira-slider .prateleira>ul').not('.slick-initialized');
+
+    this.setupSlider = function($currentSlider) {
+        $currentSlider.not('.slick-initialized').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [{
+                breakpoint: 1019,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }, {
+                breakpoint: 990,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+
+        //ajusta para mobile - prateleira slider
+        $('.prateleira-slider .prateleira ul').find('.detalhes>a').addClass('col-xs-6 col-md-12');
+    };
 
 
     //setup modal
@@ -169,6 +184,30 @@ Nitro.controller('produto', [ /*'video', */ 'sku-fetch', 'gallery', 'product-nav
         });
 
         $('body').addClass('google-pla');
+    }
+
+
+    //inicia automaticamente prateleiras sliders no desktop
+    if ($(window).width() > 768) {
+        self.setupSlider($slider);
+    }
+
+
+    //mobile - abrir vitrines
+    if ($(window).width() <= 768) {
+        $('section .pre-title').click(function(e){
+            e.preventDefault();
+
+            $(this).toggleClass('open');
+
+            if ($(this).hasClass('open')) {
+                $(this).siblings().find('.prateleira>ul').slideDown('slow',function(){
+                    self.setupSlider($(this));
+                });
+            } else {
+                $(this).siblings().find('.prateleira>ul').slideUp();
+            }
+        });
     }
 
 
