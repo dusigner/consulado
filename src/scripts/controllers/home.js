04e896011f5b984382a8f767ef2b13c9b5ec4c8c
Administrador-TI <Administrador-TI>
2017-02-10 18:24:34 -0200

    ajuste selo verao

--- src/scripts/controllers/home.js
+++ src/scripts/controllers/home.js
@@ -65,4 +65,6 @@ Nitro.controller('home', ['slider-banner', 'modal.overlayLead', 'lead-newsletter
 
         $('section.slider').eq(0).find('.pre-title').trigger('click');
     }
+
+    $('.customItemMenu').show();
 });
