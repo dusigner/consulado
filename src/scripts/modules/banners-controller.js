b7ea32625afc4b20df1a57a857729a21f3972082
luan santana <filipoids@gmail.com>
2017-04-12 10:56:15 -0300

    warning banners

--- src/scripts/modules/banners-controller.js
+++ src/scripts/modules/banners-controller.js
@@ -2,27 +2,27 @@
 
 var Index = {
 
-	init: function () {
-		this.calcQntBanners();
-	},
+    init: function () {
+        this.calcQntBanners();
+    },
 
-	calcQntBanners: function () {
+    calcQntBanners: function () {
 
-		var $banners = $('.banner-secundario-mosaico');
-		var qntBanners = $banners.size();
+        var $banners = $('.banner-secundario-mosaico');
+        var qntBanners = $banners.size();
 
-		$banners.removeClass('one-banner');
-		$banners.removeClass('two-banner');
+        $banners.removeClass('one-banner');
+        $banners.removeClass('two-banner');
 
-		if(qntBanners === 1) {
-			$banners.parent().addClass('one-banner');
-		} else if (qntBanners === 2) {
-			$banners.parent().addClass('two-banner');
-		} else {
-			$banners.parent().removeClass('one-banner');
-			$banners.parent().removeClass('two-banner');
-		}
-	}
+        if(qntBanners === 1) {
+            $banners.parent().addClass('one-banner');
+        } else if (qntBanners === 2) {
+            $banners.parent().addClass('two-banner');
+        } else {
+            $banners.parent().removeClass('one-banner');
+            $banners.parent().removeClass('two-banner');
+        }
+    }
 
 };
 
