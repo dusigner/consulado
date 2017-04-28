06157656ccf35f48c6ae78846c24f7998d04385a
pedro <pedro.henrick@jussi.com.br>
2017-04-28 18:40:35 -0300

    PLAT-3083 #comment desenvolvimento lp cns

--- src/pages/landing-concurso/scripts/consul-landing-concurso.js
+++ src/pages/landing-concurso/scripts/consul-landing-concurso.js
@@ -5,13 +5,15 @@ require('modules/helpers');
 //load Nitro Lib
 require('vendors/nitro');
 
+var CRM = require('modules/store/crm');
+
 Nitro.setup([], function () {
 
 	$(window).load(function() {
 		$('.inner, .inner-only').css('visibility', 'visible');
 
-		$('.flip-1, .flip-2, .flip-3, .flip-4, .flip-5, .flip-8').addClass('animated flipInX');
-		$('.flip-6, .flip-7').addClass('animated flipInY');
+		$('.box-one, .box-two, .box-three, .box-four, .box-five, .box-six, .box-seven').addClass('animated flipInY');
+		// $('.flip-6, .flip-7').addClass('animated flipInY');
 
 		// setTimeout(function() {
 		// 	$('.flip-8').addClass('flip-1-out');
