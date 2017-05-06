5d968aca51755661692402693e289d15f192dea5
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2017-05-05 21:58:36 -0300

    PLAT-3357

--- src/pages/pre-home/scripts/consul2-pre-home.js
+++ src/pages/pre-home/scripts/consul2-pre-home.js
@@ -4,6 +4,8 @@
 
 require('modules/helpers');
 
+require('vendors/slick');
+
 if (VERSION) {
 
     console.info('%c %c %c Jussi | %s Build Version: %s %c %c ', 'background:#dfdab0;padding:2px 0;', 'background:#666; padding:2px 0;', 'background:#222; color:#bada55;padding:2px 0;', (window.jsnomeLoja || '').replace(/\d/, '').capitalize(), VERSION, 'background:#666;padding:2px 0;', 'background:#dfdab0;padding:2px 0;');
@@ -18,6 +20,11 @@ require('vendors/nitro');
 
 Nitro.setup([], function() {
 
-    console.log('está na pré-home');
+    $('#info .row>div').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+
 
 });
