7222091594e2a725560d4765af27f19a929f7bde
Pedro Henrick <pedro.henrickcs@gmail.com>
2017-05-12 18:45:31 -0300

    ajuste

--- src/pages/landing-concurso/scripts/consul-landing-concurso.js
+++ src/pages/landing-concurso/scripts/consul-landing-concurso.js
@@ -88,6 +88,37 @@ Nitro.setup([], function () {
 		$('#cbirthdate').mask('99/99/9999');
 	});
 
+	jQuery.validator.addMethod('cpf', function(value, element) {
+   	value = jQuery.trim(value);
+
+	    value = value.replace('.','');
+	    value = value.replace('.','');
+	    var cpf = value.replace('-','');
+	    while(cpf.length < 11) cpf = '0'+ cpf;
+	    var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+	    var a = [];
+	    var b = new Number;
+	    var c = 11;
+	    var i = 0;
+	    var x = b % 11;
+	    var y = 0;
+	    for (i=0; i<11; i++){
+	        a[i] = cpf.charAt(i);
+	        if (i < 9) b += (a[i] * --c);
+	    }
+	    if ((x = b % 11) < 2) { a[9] = 0; } else { a[9] = 11-x; }
+	    b = 0;
+	    c = 11;
+	    for (y=0; y<10; y++) b += (a[y] * c--);
+	    if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11-x; }
+
+	    var retorno = true;
+	    if ((cpf.charAt(9) !== a[9]) || (cpf.charAt(10) !== a[10]) || cpf.match(expReg)) retorno = false;
+
+	    return this.optional(element) || retorno;
+
+	}, 'Informe um CPF válido');
+
 	// validação do form
 	$('#form-concurso').validate({ 
 		rules:{
@@ -101,6 +132,7 @@ Nitro.setup([], function () {
 				required: true
 			},
 			document:{
+				cpf: true,
 				required: true
 			},
 			phone: {
