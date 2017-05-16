b44f0a85b0cf1bc99a601e88e5f20695858754b5
Pedro Henrick <pedro.henrickcs@gmail.com>
2017-05-16 19:06:21 -0300

    PLAT-3448 #comment ajustes

--- src/pages/landing-concurso/scripts/consul-landing-concurso.js
+++ src/pages/landing-concurso/scripts/consul-landing-concurso.js
@@ -11,7 +11,7 @@ Nitro.setup([], function () {
 
 	$(window).load(function() {
 
-		// animação banner
+		// animação banner principal
 		var flippyIn = function() {
 			var boxes = [0,1,2,3,4,5,6,7,8,9],
 				remaining = boxes;
@@ -88,70 +88,107 @@ Nitro.setup([], function () {
 		$('#cbirthdate').mask('99/99/9999');
 	});
 
-	jQuery.validator.addMethod('cpf', function(value, element) {
-		value = jQuery.trim(value);
-		value = value.replace('.','');
-		value = value.replace('.','');
-		var cpf = value.replace('-','');
-		while (cpf.length < 11) {
-			cpf = '0'+ cpf;	
-		} 
-		var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
-		var a = [];
-		var b = new Number;
-		var c = 11;
-		var i = 0;
-		var x = b % 11;
-		var y = 0;
-		for (i=0; i<11; i++) {
-			a[i] = cpf.charAt(i);
-			if (i < 9) {
-				b += (a[i] * --c);	
-			} 
-		}
-		if ((x = b % 11) < 2) 
-			{ a[9] = 0; } 
-		else { a[9] = 11-x; 
-		}
-		b = 0;
-		c = 11;
-		for (y=0; y<10; y++) {
-			b += (a[y] * c--);	
-		} 
-		if ((x = b % 11) < 2) { 
-			a[10] = 0; } else { a[10] = 11-x; 
-		}
+	// valida cpf
+	jQuery.validator.addMethod('document', function(value) {
+		value = value.replace(/[^\d]+/g, '');
 
-		var retorno = true;
-		if ((cpf.charAt(9) !== a[9]) || (cpf.charAt(10) !== a[10]) || cpf.match(expReg)) {
-			retorno = false;	
-		} 
+		if (value === '') {
+			return false;
+		}
+        // Elimina values invalidos conhecidos
+		if (value.length !== 11 || value === '00000000000' || value === '11111111111' || value === '22222222222' || value === '33333333333' || value === '44444444444' || value === '55555555555' || value === '66666666666' || value === '77777777777' || value === '88888888888' || value === '99999999999') {
+			return false;
+		}
+        // Valida 1o digito
+		var add = 0;
+		for (var i = 0; i < 9; i++) {
+			add += parseInt(value.charAt(i), 10) * (10 - i);
+		}
+		var rev = 11 - (add % 11);
+		if (rev === 10 || rev === 11) {
+			rev = 0;
+		}
+		if (rev !== parseInt(value.charAt(9), 10)) {
+			return false;
+		}
+        // Valida 2o digito
+		add = 0;
+		for (i = 0; i < 10; i++) {
+			add += parseInt(value.charAt(i), 10) * (11 - i);
+		}
+		rev = 11 - (add % 11);
+		if (rev === 10 || rev === 11) {
+			rev = 0;
+		}
+		if (rev !== parseInt(value.charAt(10), 10)) {
+			return false;
+		}
+		return true;  
+	});
 
-		return this.optional(element) || retorno;
+	// valida telefone
+	jQuery.validator.addMethod('phone', function (value, element) {	
+		value = value.replace('(', '');
+		value = value.replace(')', '');
+		value = value.replace('-', '');
+		value = value.replace(' ', '').trim();
+		if (value === '0000000000') {
+			return (this.optional(element) || false);
+		} else if (value === '00000000000') {
+			return (this.optional(element) || false);
+		}
+		if (['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10'].indexOf(value.substring(0, 2)) !== -1) {
+			return (this.optional(element) || false);
+		}
+		if (value.length < 10 || value.length > 11) {
+			return (this.optional(element) || false);
+		}
+		return (this.optional(element) || true);
+	}, 'Informe um telefone válido'); 
 
-	}, 'Informe um CPF válido');
+	// valida data de nascimento
+	$.validator.addMethod('birthdate', function() {
+		var age =  18;
+		var day = $('#cbirthdate').val().split('/')[0];
+		var month = $('#cbirthdate').val().split('/')[1];
+		var year = $('#cbirthdate').val().split('/')[2];
+
+		var mydate = new Date();
+		mydate.setFullYear(year, month-1, day);
+
+		var currdate = new Date();
+		currdate.setFullYear(currdate.getFullYear() - age);
+
+		return currdate > mydate;
+
+	}, 'Para participar é necessário ter 18 anos');
 
 	// validação do form
 	$('#form-concurso').validate({ 
+		// regras form
 		rules:{
 			campoName:{
-				required: true, minlength: 2
+				minlength: 2,
+				required: true
 			},
 			campoEmail:{
-				required: true, email: true
+				email: true,
+				required: true
 			},
 			birthdate:{
+				birthdate: true,
 				required: true
 			},
 			document:{
-				cpf: true,
+				document: true,
 				required: true
 			},
 			phone: {
+				phone: true,
 				required: true
 			}
 		},
-
+		// mensagens de erro form
 		messages:{
 			campoName:{
 				required: 'Campo `nome` obrigatório',
@@ -162,60 +199,66 @@ Nitro.setup([], function () {
 				email: 'Digite um e-mail válido'
 			},
 			birthdate: {
+				birthdate: 'Para participar é necessário ter 18 anos',
 				required: 'Data de aniversário inválida'
 			},
 			document: {
-				required: 'CPF inválido'
+				required: 'Informe um CPF',
+				document: 'CPF inválido'
 			},
 			phone: {
-				required: 'Telefone inválido'	
+				required: 'Informe um telefone',
+				phone: 'Telefone inválido'
 			}
+		},
+
+		submitHandler: function() {
+			var $form = $('#form-concurso');
+
+			$form.submit(function(e) {
+				e.preventDefault();
+				var dia = $('#cbirthdate').val().split('/')[0],
+					mes = $('#cbirthdate').val().split('/')[1],
+					ano = $('#cbirthdate').val().split('/')[2];
+
+				var formData = {
+					name: $('#cname').val(),
+					email: $('#cemail').val(),
+					birthdate: new Date(mes + '/' + dia + '/' + ano).getTime(),
+					phone: $('#phone').val(),
+					document: $('#cpf').val(),
+					orderId: $('#orderId').val()
+				};
+
+				$.ajax({
+					type: 'POST',
+					url: 'https://consul-promo.herokuapp.com/lead',
+					data: formData,
+					beforeSend: function() {
+					//addclass
+						$('.btn.primary-button').addClass('loading');
+						$('.btn-enviar span').css('display', 'none');
+					}
+				}).then(function() {
+					//deu certo
+					$('#form input.error').removeClass('error');
+					$('.error').css('display', 'none');
+					$('.btn.primary-button').after('<span class="msg-form msg-sucesso">Formulário enviado!</span>');
+					$('.msg-erro').addClass('hide');
+					$('#form input').val('');
+					$('.msg-erro').addClass('hide');
+				}).fail(function () {
+					//deu errado
+					$('.btn.primary-button').after('<span class="msg-form msg-erro">Ocorreu um erro!</span>');
+					$('.msg-sucesso').addClass('hide');
+				}).always(function() {
+					//removeclass
+					$('.btn.primary-button').removeClass('loading');
+					$('.btn-enviar span').css('display', 'block');
+				});
+			}); 
 		}
 	});
-
-	var $form = $('#form-concurso');
-
-	$form.submit(function(e) {
-		e.preventDefault();
-		var dia = $('#cbirthdate').val().split('/')[0],
-			mes = $('#cbirthdate').val().split('/')[1],
-			ano = $('#cbirthdate').val().split('/')[2];
-
-		var formData = {
-			name: $('#cname').val(),
-			email: $('#cemail').val(),
-			birthdate: new Date(mes + '/' + dia + '/' + ano).getTime(),
-			phone: $('#phone').val(),
-			document: $('#cpf').val(),
-			orderId: $('#orderId').val()
-		};
-
-		$.ajax({
-			type: 'post',
-			url: 'https://consul-promo.herokuapp.com/lead',
-			data: formData,
-			beforeSend: function() {
-			//addclass
-				$('.btn.primary-button').addClass('loading');
-				$('.btn-enviar span').css('display', 'none');
-			}
-		}).then(function() {
-			//deu certo
-			$('.btn.primary-button').after('<span class="msg-form msg-sucesso">Formulário enviado!</span>');
-			$('.msg-erro').addClass('hide');
-			$('#form input').val('');
-			$('.msg-erro').addClass('hide');
-		}).fail(function () {
-			//deu errado
-			$('.btn.primary-button').after('<span class="msg-form msg-erro">Ocorreu um erro!</span>');
-			$('.msg-sucesso').addClass('hide');
-		}).always(function() {
-			//removeclass
-			$('.btn.primary-button').removeClass('loading');
-			$('.btn-enviar span').css('display', 'block');
-		});
-	}); 
-
 });
 
 
