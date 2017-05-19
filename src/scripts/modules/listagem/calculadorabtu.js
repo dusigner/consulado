01a589ab1fce815ddf207b43bea2f2db7bcfde1f
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2017-05-18 22:56:36 -0300

    calc

--- src/scripts/modules/listagem/calculadorabtu.js
+++ src/scripts/modules/listagem/calculadorabtu.js
@@ -5,7 +5,7 @@ Nitro.module('calculadorabtu', function () {
 		calcBtuMask = calcBtu.find('.page-calculadora-btu__mask'),
 		calcBtuIframe = $('#calculadora-btu');
 
-	calcBtuIframe.attr('src', 'http://consulqa.vtexcommercestable.com.br/landing/calculadora-btu');
+	// calcBtuIframe.attr('src', 'http://consulqa.vtexcommercestable.com.br/landing/calculadora-btu');
 
 	calcBtuMask.click(function(e) {
 		e.preventDefault();
