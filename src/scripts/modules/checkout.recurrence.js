5c95b06a6b1f8007d2f451f70c11ac246161afb1
gfpaiva <gf.paiva@yahoo.com.br>
2017-05-25 11:46:15 -0300

    bugfix hidepayments recorrencia

--- src/scripts/modules/checkout.recurrence.js
+++ src/scripts/modules/checkout.recurrence.js
@@ -261,7 +261,8 @@ Nitro.module('checkout.recurrence', function() {
 	this.hidePayments = function() {
 		$.each(self.orderForm.items, function(i, v) {
 			if(self.hasActiveRecurrence(v.attachments)){
-				$('.payment-group-item:not(#payment-group-creditCardPaymentGroup)').addClass('hide');
+				$('.payment-group-item:not(#payment-group-creditCardPaymentGroup)').addClass('hide').remove();
+				$('#payment-group-creditCardPaymentGroup').click();
 				return false;
 			}
 		});
