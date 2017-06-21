1aea310da43c3144e373d121a9668fffb8f6b2fb
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2017-06-21 04:53:50 -0300

    PLAT-3358 mudando checkbox para aparecer no firefox e adicionando tooltip

--- src/scripts/modules/store/validation.js
+++ src/scripts/modules/store/validation.js
@@ -13,12 +13,22 @@ function Validation() {
 Validation.prototype.setError = function(el, type, deferred) {
 	type = type || 'error';
 
-	el.addClass('error').data({
-		title: el.data('msg-' + type),
-		html: true,
-		placement: 'top',
-		trigger: 'manual'
-	}).tooltip('show');
+	if( type === 'checkbox' ) {
+		el.addClass('error').parents('.checkbox').data({
+			title: el.data('msg-' + type),
+			html: true,
+			placement: 'top',
+			trigger: 'manual'
+		}).tooltip('show');
+	} else {
+		el.addClass('error').data({
+			title: el.data('msg-' + type),
+			html: true,
+			placement: 'top',
+			trigger: 'manual'
+		}).tooltip('show');
+	}
+
 
 	deferred.reject(el, type);
 };
@@ -31,37 +41,37 @@ Validation.prototype.validate = function(inputs, submit) {
 
 	inputs.tooltip('destroy').closest('form').find('span.error').remove();
 
-	inputs
+	inputs.add($(inputs).parents('.checkbox'))
 		.removeClass('error')
 		.one('focus change', function() {
 			$(this).removeClass('error').tooltip('destroy').next('span.error').remove();
-		})
-		.each(function() {
-			var self = $(this),
-				validation = self.data('validation');
-
-			if (/required/.test(validation) && self.is(':blank')) {
-				_self.setError(self, 'required', deferred);
-			} else if (self.val().length < self.data('minlength')) {
-				_self.setError(self, 'minlength', deferred);
-			} else if (/email/.test(validation) && !self.validEmail()) {
-				_self.setError(self, 'email', deferred);
-			} else if (/cpf/.test(validation) && !self.validCpf()) {
-				_self.setError(self, 'cpf', deferred);
-			} else if (/cnpj/.test(validation) && !self.validCnpj()) {
-				_self.setError(self, 'cnpj', deferred);
-			} else if (/confirm/.test(validation) && self.val() !== inputs.filter('input[name="' + self.data('confirm') + '"]').val()) {
-				_self.setError(self, 'confirm', deferred);				
-			} else if (/checkbox/.test(validation) && !self.is(':checked')) {
-				_self.setError(self, 'checkbox', deferred);
-			} else if (/partner/.test(validation) && deferred.state() !== 'rejected') {
-				pending = true;
-
-				CRM.partnerSearch(self.val())
-					.done(deferred.resolve)
-					.fail(_self.setError.bind(_self, self, 'partner', deferred));
-			}
 		});
+	inputs.each(function() {
+		var self = $(this),
+			validation = self.data('validation');
+
+		if (/required/.test(validation) && self.is(':blank')) {
+			_self.setError(self, 'required', deferred);
+		} else if (self.val().length < self.data('minlength')) {
+			_self.setError(self, 'minlength', deferred);
+		} else if (/email/.test(validation) && !self.validEmail()) {
+			_self.setError(self, 'email', deferred);
+		} else if (/cpf/.test(validation) && !self.validCpf()) {
+			_self.setError(self, 'cpf', deferred);
+		} else if (/cnpj/.test(validation) && !self.validCnpj()) {
+			_self.setError(self, 'cnpj', deferred);
+		} else if (/confirm/.test(validation) && self.val() !== inputs.filter('input[name="' + self.data('confirm') + '"]').val()) {
+			_self.setError(self, 'confirm', deferred);				
+		} else if (/checkbox/.test(validation) && !self.is(':checked')) {
+			_self.setError(self, 'checkbox', deferred);
+		} else if (/partner/.test(validation) && deferred.state() !== 'rejected') {
+			pending = true;
+
+			CRM.partnerSearch(self.val())
+				.done(deferred.resolve)
+				.fail(_self.setError.bind(_self, self, 'partner', deferred));
+		}
+	});
 
 	if (deferred.state() !== 'rejected') {
 		submit.addClass('loading');
