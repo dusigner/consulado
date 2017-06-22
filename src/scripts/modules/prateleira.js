81af5829f631174765ae148f2f3df62ea8f07d74
Leonardo Lima <leonardotlima2@gmail.com>
2017-06-22 17:34:14 -0300

    PLAT-3319 - CNS | Excluir prateleiras vazias (Home - Categoria) (https://jussibr.atlassian.net/browse/PLAT-3319)

--- src/scripts/modules/prateleira.js
+++ src/scripts/modules/prateleira.js
@@ -3,8 +3,14 @@
 require('modules/percentoff');
 
 Nitro.module('prateleira', ['percentoff'], function() {
+	var $shelf = $('.prateleira-slider');
 
+	this.hiddenEmptyShelves = function() {
 
-	$('.helperComplement').remove();
-
+		$shelf.each(function() {
+			if( !($(this).innerHTML > 0) ) {
+				$(this).closest('section.slider').hide();
+			}
+		});
+	}();
 });
