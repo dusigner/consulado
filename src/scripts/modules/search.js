dc5142186574534994a695afdd7300341799bc81
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2017-12-04 17:10:37 -0200

    PLAT-5376 ajuste busca vtex

--- src/scripts/modules/search.js
+++ src/scripts/modules/search.js
@@ -32,98 +32,102 @@ Nitro.module('search', function() {
 	//     $(document).trigger('search', $searchContainer.is('.search-active') ? null : false);
 	// });
 
-	var loadContent = $.debounce(function(term) {
+	if( store.isCorp ) {
 
-		console.info('loadContent', term);
+		var loadContent = $.debounce(function(term) {
 
-		$.ajax({
-			url: API_ENDPOINT,
-			data: {
-				ft: term,
-				PS: total,
-				sl: layout
-			},
-			localCache: true,
-			cacheTTL: 1,
-			cacheKey: 'search:' + term,
-			dataType: 'html',
-			beforeSend: function() {
-				searchTerm = term;
+			console.info('loadContent', term);
 
-				$searchForm.addClass('loading').append($loader);
+			$.ajax({
+				url: API_ENDPOINT,
+				data: {
+					ft: term,
+					PS: total,
+					sl: layout
+				},
+				localCache: true,
+				cacheTTL: 1,
+				cacheKey: 'search:' + term,
+				dataType: 'html',
+				beforeSend: function() {
+					searchTerm = term;
+
+					$searchForm.addClass('loading').append($loader);
+				}
+			})
+			.done(function(data) {
+				if (data) {
+					$content.html(data).find('.helperComplement').remove();
+
+					$results = $content.find('a');
+
+					$(document).trigger('search', true);
+				}
+			})
+			.always(function() {
+				$searchForm.removeClass('loading');
+				$loader.remove();
+			});
+
+		}, 250);
+
+		$searchField.on('keyup focus', function() {
+
+			var value = $.trim($(this).val());
+
+			//console.info('input change', value);
+
+			if (searchTerm !== value) {
+				$document.trigger('search');
+
+				if (value.length >= 3) {
+					loadContent(value);
+				}
 			}
-		})
-		.done(function(data) {
-			if (data) {
-				$content.html(data).find('.helperComplement').remove();
 
-				$results = $content.find('a');
-
-				$(document).trigger('search', true);
-			}
-		})
-		.always(function() {
-			$searchForm.removeClass('loading');
-			$loader.remove();
 		});
 
-	}, 250);
 
-	$searchField.on('keyup focus', function() {
-
-		var value = $.trim($(this).val());
-
-		//console.info('input change', value);
-
-		if (searchTerm !== value) {
-			$document.trigger('search');
-
-			if (value.length >= 3) {
-				loadContent(value);
-			}
-		}
-
-	});
-
-
-	$searchForm.submit(function(e) {
-		e.preventDefault();
-		window.location.href = '/' + encodeURIComponent($searchField.val());
-	});
-
-	$document.on('search', function(e, status) {
-
-		if (status === false) {
-			$searchField.val('').trigger('keyup');
-		}
-
-		$searchBox.toggle(!!status); //force boolean
-
-		$searchActionText.text('"' + searchTerm + '"');
-
-		$searchAction.attr({
-			title: 'Buscar Por "' + searchTerm + '"',
-			href: '/' + encodeURIComponent(searchTerm)
+		$searchForm.submit(function(e) {
+			e.preventDefault();
+			window.location.href = '/' + encodeURIComponent($searchField.val());
 		});
 
-	}).keydown(function(e) {
+		$document.on('search', function(e, status) {
 
-		if ((e.keyCode === 40 || e.keyCode === 38) && $('body').is('.search-active')) {
-
-			var $focus = $searchContainer.find(':focus').removeClass('active');
-
-			if (e.keyCode === 40) { //down
-
-				$results.eq($results.index($focus) + 1).focus().addClass('active');
-
-			} else if (e.keyCode === 38) { //up
-
-				($results.index($focus) <= 0 ? $searchField : $results.eq($results.index($focus) - 1)).focus().addClass('active');
-
+			if (status === false) {
+				$searchField.val('').trigger('keyup');
 			}
 
-			return false;
-		}
-	});
+			$searchBox.toggle(!!status); //force boolean
+
+			$searchActionText.text('"' + searchTerm + '"');
+
+			$searchAction.attr({
+				title: 'Buscar Por "' + searchTerm + '"',
+				href: '/' + encodeURIComponent(searchTerm)
+			});
+
+		}).keydown(function(e) {
+
+			if ((e.keyCode === 40 || e.keyCode === 38) && $('body').is('.search-active')) {
+
+				var $focus = $searchContainer.find(':focus').removeClass('active');
+
+				if (e.keyCode === 40) { //down
+
+					$results.eq($results.index($focus) + 1).focus().addClass('active');
+
+				} else if (e.keyCode === 38) { //up
+
+					($results.index($focus) <= 0 ? $searchField : $results.eq($results.index($focus) - 1)).focus().addClass('active');
+
+				}
+
+				return false;
+			}
+		});
+	}
+
 
 });
