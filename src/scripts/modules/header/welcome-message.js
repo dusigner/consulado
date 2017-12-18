a6377ea002baa4e0ecab2b57d85decd89d5f97a2
Marcela Afonso <marcela.bafonso3@gmail.com>
2017-12-18 10:35:17 -0200

    PLAT-5431 Ajustes

--- src/scripts/modules/header/welcome-message.js
+++ src/scripts/modules/header/welcome-message.js
@@ -1,12 +1,13 @@
 'use strict';
 
 Nitro.module('welcome-message', function () {
+	console.log('uhuuuuu');
 	//Modificar mensagem de welcome
 	$(window).load(function () {
 		var welcomeMessage = $('.account p.welcome, .header-account p.welcome');
 
-		//verifica se é PJ para trocar o nome que aparece na msg de welcome
-		if (store.isCorp) {
+		//trocar mensagem de welcome
+		if (store.isCorp) { //verifica se é PJ para trocar pelo nome da empresa
 			//trata se não tiver nome da empresa pra colocar o e-mail
 			if (store.userData.corporateName === null) {
 				welcomeMessage.html('Olá <span class="email">' + store.userData.email + '</span>!');
