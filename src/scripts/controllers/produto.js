494f4feb9d925be3f68b982a55169c42adf8e9b6
Wandreus <Wandreus.pereira@outlook.com>
2017-12-27 17:20:17 -0200

    correcao warning

--- src/scripts/controllers/produto.js
+++ src/scripts/controllers/produto.js
@@ -14,9 +14,10 @@ require('modules/product/supermodel');
 require('modules/product/sku-select');
 require('modules/product/boleto');
 require('modules/product/notify-me');
+require('modules/product/share');
 // require('modules/product/special-content');
 
-Nitro.controller('produto', [ 'sku-fetch', 'gallery', 'product-nav', 'video', 'details', 'specifications', 'selos', 'supermodel', 'sku-select', 'boleto', 'notify-me' /*, 'special-content'*/ ], function() {
+Nitro.controller('produto', [ 'sku-fetch', 'gallery', 'product-nav', 'video', 'details', 'specifications', 'selos', 'supermodel', 'sku-select', 'boleto', 'notify-me', 'share' /*, 'special-content'*/ ], function() {
 	var self = this,
 		$body = $('body');
 
