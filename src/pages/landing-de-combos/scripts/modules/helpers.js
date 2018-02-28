1fce297be92528dbb62ad09c723ff3898b9cd027
Gabriel Henrique <gabhenrique0@gmail.com>
2018-02-28 10:02:13 -0300

    PLAT-5909 - Altera o arquivos JS da landing de combos com base na refatoração e ajustes de UX

--- src/pages/landing-de-combos/scripts/modules/helpers.js
+++ src/pages/landing-de-combos/scripts/modules/helpers.js
@@ -2,18 +2,23 @@
 
 var Helpers = {
 	self: this,
-	getInstallmentPrice: function (totalPrice) {
+
+	getInstallmentPrice: function(totalPrice) {
 		var result;
+
 		totalPrice = Helpers.formatFloat(totalPrice);
 		result = totalPrice / 10;
+
 		return _.formatCurrency(result);
 	},
-	sumPrice: function (prices) {
-		return prices.reduce(function (previousValue, currentValue) {
+
+	sumPrice: function(prices) {
+		return prices.reduce(function(previousValue, currentValue) {
 			return previousValue + currentValue;
 		});
 	},
-	formatFloat: function (valor) {
+
+	formatFloat: function(valor) {
 		return isNaN(valor) === false ? parseFloat(valor) : parseFloat(valor.replace('R$', '').replace('.', '').replace(',', '.'));
 	}
 };
