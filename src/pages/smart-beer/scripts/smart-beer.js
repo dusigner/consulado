f3321700aa834420f6a4314055a0227264077ea2
Fabio Haddad <fabiohpaes@gmail.com>
2018-03-21 18:54:51 -0300

    PLAT-6058 Finalização da Page Smart Beer Xhtml e SCSS

--- src/pages/smart-beer/scripts/smart-beer.js
+++ src/pages/smart-beer/scripts/smart-beer.js
@@ -1,1 +1,13 @@
-'use strict';
+// // 'use strict';
+
+
+// require('vendors/nitro');
+
+// Nitro.module('smart-beer', function () {
+//     this.init = function() {
+//     };
+    
+//     this.init();
+// });
+
+console.log('######################');
