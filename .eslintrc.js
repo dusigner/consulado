10b3cdef61ef7182903ca8e9f232686fdc2fa24b
Lucas Henrique <lucaa.henrique.carlos@gmail.com>
2018-07-05 17:02:27 -0300

    PLAT-6684 gulpfile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,58 @@
 
-{
+module.exports = {
   "extends": [
     "eslint:recommended",
     "plugin:import/errors",
     "plugin:import/warnings"
   ],
   "env": {
-    "es6": false,
+    "es6": true,
     "browser": true,
     "node": true,
     "jquery": true,
     "mocha": true
   },
+  "parserOptions": {
+		"ecmaVersion": 6,
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"modules": true,
+			"experimentalObjectRestSpread": true
+		}
+	},
   "globals": {
     "_": true,
+    "_jussi": true,
     "$": true,
     "dataLayer": true,
     "define": true,
     "dust": true,
     "localStore": true,
     "Nitro": true,
-    "store": true,
-    "vtxctx": true,
-    "vtexjs": true,
     "ScrollMagic": true,
+		"skuJson": true,
+    "store": true,
+    "TimelineMax": true,
     "TweenMax": true,
-    "TimelineMax": true
+    "userData": true,
+    "vtexjs": true,
+    "vtxctx": true
   },
   "rules": {
     "quotes": [2, "single"],
     "curly": 1,
     "strict": 1,
     "eqeqeq": ["error", "smart"],
-    "no-console": [1, { "allow": ["assert", "info", "error"] }],
+    "no-console": [1, {
+      "allow": ["assert", "info", "error"]
+    }],
     "no-debugger": 1,
-    "no-undef": ["error", { "typeof": true }],
+    "no-undef": ["error", 
+      { "typeof": true }
+    ],
     "no-mixed-spaces-and-tabs": 1,
     "no-var": 0,
+    "no-unresolved": 0,
     "indent": ["warn", "tab"],
     "semi": ["error", "always"],
     "no-trailing-spaces": 0,
@@ -45,6 +61,7 @@
     "no-underscore-dangle": 0,
     "no-alert": 1,
     "no-lone-blocks": 0,
-    "jsx-quotes": ["warn", "prefer-single"]
+    "jsx-quotes": ["warn", "prefer-single"],
+    "import/no-unresolved": 0
   }
-}
+};
