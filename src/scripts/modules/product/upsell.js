549c8c4a825d75fb86d7fff2ca36964f079e8d9d
wandreus <wandreus.pereira@outlook.com>
2018-07-22 03:29:47 -0300

    PLAT-6774 Desenvolvimento

--- src/scripts/modules/product/upsell.js
+++ src/scripts/modules/product/upsell.js
@@ -2,127 +2,198 @@
 
 'use strict';
 
-require('Dust/product/upsell.html');
+require('Dust/product/upgrade.html');
 require('Dust/product/downgrade.html');
-require('../bi/upselldataLayer');
 
 
-Nitro.module('upsell', 'upselldataLayer', function() {
+Nitro.module('upsell', function() {
 	console.clear();
-
+	
 	const self = this;
 	
 	let 
-		// pegando valores do produto atual	
 		urlUpgradetd       = $('td.value-field.Link-do-Upgrade'),
 		urlUpgrade         = $('td.value-field.Link-do-Upgrade').text(),
 		
 		uri   = window.location.href,
-		urlAtual = window.location.pathname,
-		verifyDowngrade = uri.split('downgrade=')[1],
-		// template = 'upsell',
+		currentUrl = window.location.pathname,
+		verifyDowngrade = uri.indexOf('downgrade'),
+		template = 'upgrade',
 		apiResponse,
 		apiResponseUpgrade,
 		apiResponseDowngrade;
 
-		// if (uri.indexOf('downgrade') !== -1) {
-		// 	console.log('🤞');
-		// 	template = 'downgrade';
-		// 	urlAtual = verifyDowngrade;
-		// }
-
-		// uri.indexOf('downgrade') !== -1 ? urlAtual = verifyDowngrade template = 'downgrade' : '';
-
-		console.log('urlAtual', urlAtual, 'template');
+	if (verifyDowngrade > 0) {
+		template = 'downgrade';
+		currentUrl = uri.split('downgrade=')[1];
+	}
+		
 
 	this.init = () => {
-		var settingsDowngrade = {
-			'url': `/api/catalog_system/pub/products/search/${urlAtual}`,
+		var getDowngrade = {
+			'url': `/api/catalog_system/pub/products/search/${currentUrl}`,
 			'method': 'GET'
 		};
-		$.ajax(settingsDowngrade).then(function (responseDowngrade) {
+		$.ajax(getDowngrade).then(function (responseDowngrade) {
 			apiResponseDowngrade = responseDowngrade[0];		
 			return responseDowngrade;
-		}).done(function(){
-			self.renderUpsell();
-		});
-		var settingsUpgrade = {
-			'url': `/api/catalog_system/pub/products/search/${urlUpgrade}`,
-			'method': 'GET'
-		};
-		$.ajax(settingsUpgrade).then(function (responseUpgrade) {
-			apiResponseUpgrade = responseUpgrade[0];
-			return responseUpgrade;
-		}).done(function(){
-			self.renderUpsell();
-		});
+		}).done(function(){		
+			var getUpgrade = {
+				'url': `/api/catalog_system/pub/products/search/${urlUpgrade}`,
+				'method': 'GET'
+			};
+			$.ajax(getUpgrade).then(function (responseUpgrade) {
+				apiResponseUpgrade = responseUpgrade[0];
+				return responseUpgrade;
+			}).done(function(){
+				self.renderUpsell();
+			});			
+		});		
 	};	
 
 	this.renderUpsell = () => {
+		
 		apiResponse = {
 			apiResponseDowngrade,
-			apiResponseUpgrade				
-		};	
-		dust.render('upsell', apiResponse, function(err, out) {
+			apiResponseUpgrade,
+			diferential01: apiResponseDowngrade['Diferencial 01'][0],				      
+			diferential02: apiResponseDowngrade['Diferencial 02'][0],
+			diferential03: apiResponseDowngrade['Diferencial 03'][0]		
+		};
+		
+			
+		dust.render(template, apiResponse, function(err, out) {
 			if (err) { 	throw new Error('upsell Dust error: ' + err); }
 			console.log('🙌', apiResponse);	
 			$('#upsell').html(out);
 		});	
-		self.valordiferenca();
-		self.openclose();
-		self.responsivo();
-		self.responsivo();
-		// upselldataLayer.setup();
+
+		self.opencloseAndMobile();
+		self.tag();
+		self.formatPrice();
 	};
 	
-	this.openclose = () => {
+	this.opencloseAndMobile = () => {
 		// Abre o modal de upgrade
 		$('.btn-interessado-upgrade').click(function() {			
 			$('#modal-produto-upgrade').vtexModal();			
 		});
 		// fecha barra fixa
 		$('.close-fixed, .icon-open-upgrade').click(function() {
-			$('#upsell').toggleClass('ativo');
-			$('.icon-open-upgrade').toggleClass('ativo');
-		});			
+			$('#upsell').toggleClass('active');
+			$('.icon-open-upgrade').toggleClass('active');
+		});
+		// verifica device
+		if ($(window).width() <= 768) {
+			$(document).ready(function(){
+				$('.product-upgrade').addClass('mobile');		
+				$('.upgrade-mobile').removeClass('hide');			
+				$('#upsell').addClass('mobile');
+			});	
+		}
 	};
 
-	this.valordiferenca = () => {
+	this.formatPrice = () => {
 		// pega e formata o preço do produto upgrade
 		let
-			priceBarra         = Number($('.title-price-upgrade span').text()),
-			priceBarraFormt    = _.formatCurrency( priceBarra );
-		$('.title-price-upgrade span, .corpo-produtos-modal .oportunidadePro span, .price-mobile-upgrade').html(`R$ ${priceBarraFormt}`);
+			priceBar         = Number(apiResponseUpgrade.items[0].sellers[0].commertialOffer.Price);
+		(priceBar < 1 && apiResponseUpgrade.items[1]) ? priceBar = Number(apiResponseUpgrade.items[1].sellers[0].commertialOffer.Price) : '';
+		(verifyDowngrade > 0) ? priceBar = Number(apiResponseDowngrade.items[0].sellers[0].commertialOffer.Price) : '';
+		(verifyDowngrade > 0 && priceBar < 1) ? priceBar = Number(apiResponseDowngrade.items[1].sellers[0].commertialOffer.Price) : '';
+		$('.title-price-upgrade span, .corpo-produtos-modal .oportunidadePro span, .price-mobile-upgrade').html(`R$ ${_.formatCurrency( priceBar )}`);
+
+		// Se nenhum preco estiver disponivel nao exibe o upsell
+		(priceBar < 1) && $('#upsell').remove();
 
 		// formata o preco do pruduto atual dentro do modal
 		let
-			priceatulmodal         = Number($('.voce-esta-vendo span').text()),
-			priceatulmodalFormat    = _.formatCurrency( priceatulmodal );
-		$('.voce-esta-vendo span').html(`R$ ${priceatulmodalFormat}`);
+			priceCurrentmodal         = Number($('.voce-esta-vendo span').text());
+		$('.voce-esta-vendo span').html(`R$ ${_.formatCurrency( priceCurrentmodal )}`);
 		
-		// Pega, formata e subtrai os valores dos produtos na barra fixa
+		// Pega, formata e subtrai os valores dos produtos na Barra fixa
 		let
-			valorProdatualcalc = $('.prod-preco .skuBestPrice').text().replace(/\D/gmi, ''),
-			valorProupgrade    = $('.title-price-upgrade span').text().replace(/\D/gmi, ''),
-			valordiferenca     = Number(valorProupgrade) - Number(valorProdatualcalc),
-			price              = `POR + R$ ${_.formatCurrency( valordiferenca / 100)}`;
-		$('.textupgrade span, .info-product-mobile > span').html( price );
+			priceCurrent = $('.prod-preco .skuBestPrice').text().replace(/\D/gmi, ''),
+			priceUpgrade    = $('.title-price-upgrade span').text().replace(/\D/gmi, ''),
+			priceDifference     = Number(priceUpgrade) - Number(priceCurrent);
+		$('.textupgrade span, .info-product-mobile > span').html(`POR + R$ ${_.formatCurrency( priceDifference / 100)}`);
+		
 	};
 
-	this.responsivo = () => {
-		if ($(window).width() <= 768) {
-			$(document).ready(function(){
-				$('.product-upgrade').removeClass('hide').addClass('mobile');			
-				$('.upgrade-mobile').removeClass('hide');			
-				$('#upsell').addClass('mobile');			
-			});	
-		} else {
-			$(document).ready(function(){				
-				$('.product-upgrade').removeClass('hide');		
-			});				
-		}
+	this.tag = () => {
+		//Tagueamento datalayer
+		$('.btn-interessado-upgrade').click(function() {
+			//Tagueamento datalayer
+			if( !$(this).hasClass('voltar') ){
+				dataLayer.push({
+					event: $(this).attr('data-event'),
+					category: $(this).attr('data-caegory'),
+					action: $(this).attr('data-action'),
+					label: $(this).attr('data-label')
+				});
+			}else{	
+				dataLayer.push({
+					event: $(this).attr('data-event'),
+					category: 'ver produto anterior',
+					action: $(this).attr('data-action'),
+					label: $(this).attr('data-label')
+				});
+			}	
+		});
+		
+
+		//Tagueamento dataLayer
+		$('.close-fixed').click(function(){
+			dataLayer.push({
+				event: 'visualTracking',
+				category: 'de um Upgrade',
+				action: 'barra',
+				label: 'Sair'
+			});
+		});
+
+		//Tagueamento dataLayer
+		$('.aceitarounao a').click(function(){
+			var action = $(this).text();
+			dataLayer.push({
+				event: 'visualTracking',
+				category: 'de um Upgrade',
+				action: 'Modal',
+				label: action
+			});
+		});
+
+		//Tagueamento dataLayer
+		$('.aceitarounao a').click(function(){
+			var action = $(this).text();
+			dataLayer.push({
+				event: 'visualTracking',
+				category: 'de um Upgrade',
+				action: 'Modal',
+				label: action
+			});
+		});
+
+		//Tagueamento datalayer
+		$('#vtex-modal-produto-upgrade .modal-header .close').click(function(){	
+			dataLayer.push({
+				event: 'visualTracking',
+				category: 'de um Upgrade',
+				action: 'Modal',
+				label: 'Sair'
+			});
+		});		
+
+		//Tagueamento datalayer
+		$('.btn-interessado-downgrade').click(function(){	
+			dataLayer.push({
+				event: 'visualTracking',
+				category: 'ver produto anterior',
+				action: 'barra',
+				label: 'Ver produto novamente'
+			});
+		});	
 	};
 	
-	this.init();
+	(urlUpgradetd.length >= 1 || verifyDowngrade > 0) && this.init();
 
 });
