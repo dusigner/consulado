dc2732336ec068518250bc6d006b9e6565b6631c
wandreus <wandreus.pereira@outlook.com>
2018-07-29 03:20:53 -0300

    PLAT-6530 AJUSTES

--- src/scripts/modules/checkout/reinput.js
+++ src/scripts/modules/checkout/reinput.js
@@ -2,65 +2,64 @@
 
 'use strict';
 
-require('../../../templates/reinput.html');
-
-// var CRM = require('modules/store/crm');
-
 require('expose-loader?store!modules/store/store');
+require('Dust/checkout/reinput.html');
 
-
-Nitro.module('reinput', function() {	
+Nitro.module('reinput', function() {
 	
-	var userType = $('#vtex-callcenter').length;		
+	var self = this,
+		userType = $('#vtex-callcenter').length;
 
-	self.renderHtml = function(){
+	this.renderHtml = function(){
 		dust.render('reinput', {}, function(err, out) {
 			if (err) {  throw new Error('Product Dust error: ' + err);	}
 			if ($('.fieldsReinput').length < 1) {
-				$(out).insertAfter('.orderform-template .summary-template-holder');         
+				$(out).insertAfter('.orderform-template .summary-template-holder');
 			}
 		});
 	};
 
-	self.lockpurchase = function () {
+	this.lockpurchase = function () {
 		$('input#isReinput:checked').each(function () {
 			$('.previouOrderId').show();
 			if (($('select#company').val() === 'Selecione') || ($('select#reason').val() === 'Selecione') || ($('select#alcada').val() === 'Selecione')) {
 				setTimeout(function(){
-					$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);						
+					(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);					
 				}, 200);					
 			} else{
 				setTimeout(function(){						
-					$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', false);												
+					(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', false);
 				}, 200);
 			}
 		});
 	};
 
-	self.searchOrderId = function (){
+	this.searchOrderId = function (){
 		$('body').on('keyup', 'input#previouOrderId', function () {
 			var pedidodigitado = $('input#previouOrderId').val();
-			if (pedidodigitado.length >= 8) {			
+			if (pedidodigitado.length > 7 && pedidodigitado.length < 11 ) {		
 				var getApi = {
 					'url': 'https://whirlpoolqa.hubinbeta.com/api/oms/pvt/findorder/' + pedidodigitado,
-					'method': 'GET'
+					'method': 'GET',
+					success: function (verify){
+						if(verify) {					
+							$('.company').show();
+							$('li.previouOrderId').addClass('load').removeClass('error');
+						}
+					}
 				};
-				$.ajax(getApi).then(function (verify) {					
-					if(verify) {								
-						$('.company').show();
-						$('li.previouOrderId').addClass('load').removeClass('error');
-					}else {
-						$('.company, .reason, .alcada, .comment').hide();
-						$('li.previouOrderId').removeClass('load').addClass('error');
-						$('.company, .reason, .alcada, .comment').removeClass('load');				
-						$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
-					}					
-				});
+				$.ajax(getApi);
+			}else {
+				$('.company, .reason, .alcada, .comment').hide();
+				$('li.previouOrderId').removeClass('load').addClass('error');
+				$('.company, .reason, .alcada, .comment').removeClass('load');
+				(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
 			}
 		});
 	};
 
-	self.checkboxReinput = function (){
+
+	this.checkboxReinput = function (){
 		$('input#isReinput').click(function () {				
 			$('.fieldsReinput form').submit(function (e) {
 				e.preventDefault();
@@ -68,16 +67,15 @@ Nitro.module('reinput', function() {
 			$('.previouOrderId, .company, .reason, .alcada, .comment').hide().removeClass('load');
 			$('#previouOrderId').val('');
 			$('li.previouOrderId').removeClass('load');
-			$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', false);
+			(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', false);
 			$('.fieldsReinput form').each(function () {
 				this.reset();
 			});
-			self.lockpurchase();
-			
+			self.lockpurchase();			
 		});
 	};
 
-	self.company = function () {
+	this.company = function () {
 		$('select#company').click(function () {
 			if ($('select#company').val() !== 'Selecione') {
 				$('.reason').show();
@@ -85,15 +83,16 @@ Nitro.module('reinput', function() {
 			} else {
 				$('.company').removeClass('load');
 				$('.reason, .alcada, .comment').hide();
-				$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
+				(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
 			}
 		});
+
 	};
 
-	self.reason = function () {
+	this.reason = function () {
 		$('select#reason').click(function () {
 			if ($('select#reason').val() === 'Selecione') {
-				$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
+				(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
 				$('.reason').removeClass('load');
 				$('.alcada').val('Selecione');
 				$('.alcada, .comment').hide();					
@@ -104,21 +103,21 @@ Nitro.module('reinput', function() {
 		});				
 	};
 
-	self.alcada = function () {
+	this.alcada = function () {
 		$('select#alcada').click(function () {
 			if ($('select#alcada').val() === 'Selecione') {
-				$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
+				(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', true);
 				$('.alcada').removeClass('load');
 				$('.comment').hide();
 			} else {
 				$('.alcada').addClass('load');
 				$('.comment').show();
-				$('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', false);						
+				(!$('#payment-data-submit, #payment-data-submit:last-child').hasClass('disabled')) && $('#payment-data-submit, #payment-data-submit:last-child').attr('disabled', false);
 			}
 		});				
 	};
 
-	self.storegeValues = function (){
+	this.storegeValues = function (){
 		$('#payment-data-submit, #payment-data-submit:last-child').on('click', function () {
 			$('input#isReinput:checked').each(function () {
 
@@ -138,16 +137,15 @@ Nitro.module('reinput', function() {
 				localStorage.setItem('orderR', pedidoreinputado);
 				localStorage.setItem('reason', motivoBoleto);
 				localStorage.setItem('alcada', motivoalcada);
-				localStorage.setItem('obsUser', obsreinpunt);
-				
+				localStorage.setItem('obsUser', obsreinpunt);				
 			});
 		});
 	};
 
-	self.comment = function (){
+	this.comment = function (){
 		$('body').on('keyup', 'textarea#obsreinpunt', function () {
 			var obsdigitado = $('textarea#obsreinpunt').val().length;
-			if (obsdigitado > 0) {					
+			if (obsdigitado > 0) {			
 				$('li.comment').addClass('load').removeClass('error');									
 			} else {
 				$('li.comment').removeClass('load');
@@ -156,11 +154,11 @@ Nitro.module('reinput', function() {
 	};
 
 	this.setup = function() {
-		if ( userType > 0 ) {
-			self.renderHtml();
-			self.searchOrderId();
-			self.checkboxReinput();
-			self.storegeValues();
+		if ( userType === 0 ) {
+			self.renderHtml();			
+			self.searchOrderId();			
+			self.checkboxReinput();			
+			self.storegeValues();	
 			self.company();
 			self.reason();
 			self.alcada();
