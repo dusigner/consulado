92c0a0e70d1baaec2132f47caff80f6adf6132f5
Fabio Haddad <fabiohpaes@gmail.com>
2018-09-27 19:26:37 -0300

    PLAT-7146 Implementação CTA teste

--- src/scripts/consul2-cta.js
+++ src/scripts/consul2-cta.js
@@ -9,24 +9,41 @@ require('modules/helpers');
 require('vendors/nitro');
 
 Nitro.setup('cta', function() {
-	console.log('teste');
 
 	var $body = $('body');
 	
 	var self = this,
 	urlParams = _.urlParams(), //parse params from url
-	skuid = 446;
+	skuid = 538,
+	emailUser = 'fabio.haddad@jussi.com.br';
+
+	
 	this.init = function() {
-		
-		setTimeout(() => {
-			vtexjs.checkout.addToCart([{
-				id: skuid,
-				quantity: 1,
-				seller: 1
-			}]).done(function(orderForm) {
-				window.location.replace('/checkout/#/payment');
-			});
-		}, 5000);
+		// setTimeout(() => {
+		// 	//avisar o VTEX ID que o email do cliente mudou
+		// 	if (window.vtexid) {
+		// 		window.vtexid.setEmail('guilherme.paiva+TESTETESTE@jussi.com.br');
+		// 	}
+	
+		// 	// levantar o evento para o script de navegação
+		// 	window.vtex.NavigationCapture && window.vtex.NavigationCapture.sendEvent('SendUserInfo', {
+		// 		visitorContactInfo: ['guilherme.paiva+TESTETESTE@jussi.com.br', '']
+		// 	});
+	
+		// 	// Avisar ao Checkout qual o email do cliente
+		// 	window.vtexjs.checkout.sendAttachment('clientProfileData', {email: 'guilherme.paiva+TESTETESTE@jussi.com.br'});
+			
+		// 	vtexjs.checkout.addToCart([{
+		// 		id: skuid,
+		// 		quantity: 1,
+		// 		seller: 1
+		// 	}]).done(function(orderForm) {
+	
+		// 		window.location.replace('/checkout/#/payment');
+	
+		// 	});
+
+		// }, 5000);
 		
 	};
 
