cd4059aa48dcef5722a90df010704587c5e6aa5b
Fabio Haddad <fabiohpaes@gmail.com>
2018-10-09 18:27:00 -0300

    PLAT-7146 Ajust Pull request - script no onload

--- src/scripts/consul2-cta.js
+++ src/scripts/consul2-cta.js
@@ -6,46 +6,58 @@ require('modules/helpers');
 require('vendors/nitro');
 
 Nitro.setup('cta', function() {
+	const self 		= this;
+	let urlParams 	= _.urlParams(); // Parse url parameters
+	let	skuid 		= urlParams.skuid ? urlParams.skuid : null;
+	let	emailUser 	= urlParams.email ? window.atob(urlParams.email) : null;
 
-	var $body = $('body');
-	
-	var self = this,
-		urlParams = _.urlParams(), //parse params from url
-		skuid = ( urlParams.skuid ? urlParams.skuid : null ),
-		emailUser = ( window.atob(urlParams.email) ? window.atob(urlParams.email) : null );
-	
-	this.init = function() {
-		vtexjs.checkout.getOrderForm().done(function(){
-			
+	/**
+	 * Default method to init modules
+	 */
+	this.init = () => $(window).load(() => self.mountOrderForm());
+
+	/**
+	 * Mount VTEX orderForm using url parameters
+	 * The two parameters that we need to put on url are the user email to register on orderForm
+	 * and the skuId to add the product on cart
+	 */
+	this.mountOrderForm = () => {
+		vtexjs.checkout.getOrderForm().done(() => {
 			//avisar o VTEX ID que o email do cliente mudou
+			/** 
+			 * Check if vtexid is available on page
+			 * In case of true we register the user email on VTEXID scope
+			 */
 			if (window.vtexid) {
 				window.vtexid.setEmail(emailUser);
 			}
-	
-			// levantar o evento para o script de navegação
+
+			/**
+			 * In case of the user is logged on store we remove his email
+			 */
 			window.vtex.NavigationCapture && window.vtex.NavigationCapture.sendEvent('SendUserInfo', {
 				visitorContactInfo: [emailUser, '']
 			});
 	
 			// Avisar ao Checkout qual o email do cliente
-			window.vtexjs.checkout.sendAttachment('clientProfileData', {email: emailUser});
+
+			/** Register the user email on orderForm using the url email parameter */
+			window.vtexjs.checkout.sendAttachment('clientProfileData', { email: emailUser });
 			
 			vtexjs.checkout.addToCart([{
 				id: skuid,
 				quantity: 1,
 				seller: 1
-			}]).done(function(orderForm) {
-	
-				// Redirecionando para o Checkout Payment
-				window.location.replace('/checkout/#/payment');
-	
-			});
+			}]).done(orderForm => {
+				console.info('orderForm', orderForm);
 
-		}).fail(function(){
-			window.location.replace('/');
-		});
+				/** In case of success, we redirect the user to the payment checkout step */
+				window.location.replace('/checkout/#/payment');
+			});
+		
+		/** In case of fail or error, we redirect the user to the root page of the store */
+		}).fail(() => window.location.replace('/'));
 	};
 
 	this.init();
-
 });
