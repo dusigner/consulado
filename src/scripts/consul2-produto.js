ca7306bd066d55c77026a1cebb7280f75cb741d0
kazuhirobruno <brunokazuhiro@gmail.com>
2018-10-21 13:45:14 -0300

    feat: counting working days until Christmas Create function to calculate number of days from today until Christmas and inform user if order will arrive before Chrismas WPLAT-7463

--- src/scripts/consul2-produto.js
+++ src/scripts/consul2-produto.js
@@ -374,6 +374,10 @@ Nitro.controller('produto', ['chaordic', 'sku-fetch', 'gallery', 'product-nav', 
 				});			
 				flag = 0;
 			}
+
+			$('.freight-values.active').find('tbody').find('td').each(function() {
+				self.setShippingMessage($(this));
+			});
 		});
 
 		$('body').on('click', '.freight-values .closed', function() {
@@ -389,5 +393,57 @@ Nitro.controller('produto', ['chaordic', 'sku-fetch', 'gallery', 'product-nav', 
 		};
 	});
 
+	/** 
+	 * Counts how many working days exists between today and Christmas
+	 * @returns number of working days between these dates
+	 */
+	this.countWorkingDays = function() {
+		let christmas = new Date('December 25, 2018 00:00:00'); // Christmas date
+		let now = new Date; // Actual date
+		let holidays = 0; // Set number of holidays
+		let weekendDays = 0; // Number of Saturdays and Sundays
+		
+		now.setHours(0, 0, 0, 0);
+
+		// Count how many days exists between these dates
+		let days = Math.round((christmas - now) / 1000 / 60 / 60 / 24) -1;
+
+		if (now <= new Date('November 2, 2018 23:59:59')) {
+			holidays = 2;	
+		} else if (now <= new Date('November 15, 2018 23:59:59')) {
+			holidays = 1;
+		}
+
+		// Count how many Saturdays and Sundays exists between these dates
+		for (let i = 0; i < days; i++) {
+			if (now.getDay() === 0 || now.getDay() === 6) {
+				weekendDays++;
+			} 
+
+			now.setDate(now.getDate()+1);
+		}
+
+		// Return total number of days, minus weekend days and holidays
+		return days - (weekendDays + holidays);
+	};
+
+	/**
+	 * Set text message if shipping can arrive before Christmas
+	 * @param {HTMLElement} $workingDaysElement
+	 */
+	this.setShippingMessage = function($workingDaysElement) {
+		// Rule to not consider schedule shipping.
+		if($workingDaysElement.text().indexOf('Agendada') > 0 ) {
+			return;
+		}
+		let workingDays = $workingDaysElement.text().match(/ (\d+) /g); // Get the number of days, if the string has this value
+		
+		// If the string has a day value, calculate how many days exists between today and Christmas and check if the order will arrive before Christmas
+		if (workingDays) {
+			let workingDaysUntilChristmas = self.countWorkingDays();
+			
+			(workingDaysUntilChristmas - parseInt(workingDays[0]) >= 0) ? $workingDaysElement.append(' <em>(Chega antes do Natal)</em>') : '';
+		}			
+	};
 
 });
