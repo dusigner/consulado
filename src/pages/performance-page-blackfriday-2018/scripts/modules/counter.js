e6618088bde32295e57e982ed3bb4b652734ab7f
Fabio Haddad <fabiohpaes@gmail.com>
2018-10-23 19:23:33 -0300

    WPLAT-7447 Implementation of Special Offer Counter

---
+++ src/pages/performance-page-blackfriday-2018/scripts/modules/counter.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * @fileOverview counter
+ *
+ */
+'use strict';
+
+require('vendors/nitro');
+
+Nitro.module('counter', function() {
+	const endDate = $('.counter__offer-prod .prateleira.default h2').text();
+
+	this.init = () => {
+		this.initCounter();
+	};
+
+	this.initCounter = () => {
+		let $counter = $('.counter'),
+			$days = $counter.find('.days'),
+			$hours = $counter.find('.hours'),
+			$minutes = $counter.find('.minutes'),
+			$seconds = $counter.find('.seconds'),
+			timeRemaining;
+
+		function getTimeRemaining(endDate) {
+			var today = Date.parse(new Date()),
+				finalDate = Date.parse(endDate),
+				total = finalDate - today,
+				seconds = Math.floor( ( total / 1000) % 60 ),
+				minutes = Math.floor( ( total / (1000 * 60 )) % 60 ),
+				hours = Math.floor( ( total / ( 1000 * 60 * 60 )) % 24 ),
+				days = Math.floor( ( total / ( 1000 * 60 * 60 * 24 )) ),
+				timeRemaining = {
+					'days': days >= 0 ? days : 0,
+					'hours': hours >= 0 ? hours : 0,
+					'minutes': minutes >= 0 ? minutes : 0,
+					'seconds': seconds >= 0 ? seconds : 0
+				};
+
+			return timeRemaining;
+		}
+
+		setInterval(function() {
+			timeRemaining = getTimeRemaining(endDate);
+
+			$days.text(timeRemaining.days > 9 ? timeRemaining.days : '0' + timeRemaining.days);
+			$hours.text(timeRemaining.hours > 9 ? timeRemaining.hours : '0' + timeRemaining.hours);
+			$minutes.text(timeRemaining.minutes > 9 ? timeRemaining.minutes : '0' + timeRemaining.minutes);
+			$seconds.text(timeRemaining.seconds > 9 ? timeRemaining.seconds : '0' + timeRemaining.seconds);
+		}, 1000);
+	};
+
+	// Start it
+	this.init();
+});
