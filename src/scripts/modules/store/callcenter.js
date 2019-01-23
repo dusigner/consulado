366b5c2c2d3505fe455426e7ef1a4ae68d2607f6
Mohamad Barakat <mohamad.barakat@jussi.local>
2019-01-23 18:14:50 -0200

    fix: login callcenter Automatic WPD-29

--- src/scripts/modules/store/callcenter.js
+++ src/scripts/modules/store/callcenter.js
@@ -3,18 +3,22 @@
 Nitro.module('callcenter', function () {
 	const self = this;
 	this.loginAutomaticCallcenter = function() {
+
 		(window.location.hostname.indexOf('empresa') !== -1) && 
 		$(document).on('click', '#impersonateButton.cc-btn-change', function() {
 			store.logout();
 			window.location.href = '/';
 		});
 		$(document).ajaxStop(function() {
-			var emailCallcenter = $('#vtex-callcenter__customer-email');
-			if( emailCallcenter.length > 0 && $('body').hasClass('pre-home')) {
-				$('.form-login.pre-home-form  .email').val(emailCallcenter.text());
-				$('.form-login.pre-home-form').trigger('submit');
-			}
+			let checkLoginCallcenter = setInterval(() => { 
+				var emailCallcenter = $('span#vtex-callcenter__customer-email');
+				if( emailCallcenter.length > 0 && $('body').hasClass('pre-home')) {
+					clearInterval(checkLoginCallcenter);
+					$('.form-login.pre-home-form  .email').val(emailCallcenter.text());
+					$('.pre-home #main fieldset button').click();
+				} 
+			}, 800);
 		});
-	};
+	};	
 	self.loginAutomaticCallcenter();
 });
