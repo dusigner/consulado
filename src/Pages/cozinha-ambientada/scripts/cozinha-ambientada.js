cf2b51f7a8faa544d2adffb6762ac07e9e3fba6e
Matheus Gomide <matheusggds@hotmail.com>
2019-02-13 15:51:07 -0200

    Feat: insert mosaic static content inside page
    WPD-18

--- src/Pages/cozinha-ambientada/scripts/cozinha-ambientada.js
+++ src/Pages/cozinha-ambientada/scripts/cozinha-ambientada.js
@@ -1,26 +1,33 @@
 /**
  *
- * @fileOverview 
+ * @fileOverview
  *
  */
 'use strict';
 
-Nitro.controller('cozinha-ambientada', [], function() {
-	/**
-	 * Função init bootstrap
-	 */
-	this.init = () => {
+Nitro.setup([], function() {
 
-		this.method();
+	this.init = () => {
+		this.setListeners();
 	};
 
-	/**
-	 * Apenas um exemplo arrow func com retorno
-	 * @param  {String} param um texto que será concatenado
-	 * @returns {String} texto example with concatenando param
-	 */
-	this.method = param => `example with ${param}`;
+	this.setListeners = () => {
 
-	// Start it
+		/* Button to toggle banner item modal */
+		$('.-js-toggle-active-item').on('click', function() {
+			handleActiveItem(this);
+		});
+
+		var handleActiveItem = (toggleActiveEl) => {
+			const	$activeButton 	=	$(toggleActiveEl);
+			const 	$itemModal		= 	$activeButton.parent();
+			const	$itemSlide		=	$activeButton.parent().parent();
+
+			$activeButton.hasClass('-is-active') ? $activeButton.removeClass('-is-active') : $activeButton.addClass('-is-active');
+			$itemModal.hasClass('-is-active') ? $itemModal.removeClass('-is-active') : $itemModal.addClass('-is-active');
+			$itemSlide.hasClass('-is-active') ? $itemSlide.removeClass('-is-active') : $itemSlide.addClass('-is-active');
+		};
+	};
+
 	this.init();
 });
