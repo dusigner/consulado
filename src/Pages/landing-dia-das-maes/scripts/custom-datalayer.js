5a833f5d7fddc828eceb7b75bb2d656ee036489e
Matheus Gomide <matheusggds@hotmail.com>
2019-05-02 17:24:15 -0300

    feat: tags landing dia das maes)

--- src/Pages/landing-dia-das-maes/scripts/custom-datalayer.js
+++ src/Pages/landing-dia-das-maes/scripts/custom-datalayer.js
@@ -9,14 +9,16 @@ Nitro.module('custom-datalayer', function() {
 
 	this.init = () => {
 		this.jeitinhoSection();
-
-		console.log('oi');
-
+		this.descontoSection();
 	};
 
 	this.jeitinhoSection = () => {
 		$('#seu-jeitinho .tab button').on('click', function() {
-			pushDataLayer(`Categoria ${$(this).text()}`, 'Clique na categoria', 'Ver conteúdo do botão');
+			pushDataLayer(
+				`Categoria ${$(this).text()}`,
+				'Clique na categoria',
+				'Ver conteúdo do botão'
+			);
 		});
 
 		$('#seu-jeitinho .box-produto').on('click', function(e) {
@@ -28,7 +30,80 @@ Nitro.module('custom-datalayer', function() {
 				idProd 	= $this.attr('data-idproduto'),
 				index	= $this.parent('li').index();
 
-			pushDataLayer(`Categoria ${currentCategory}`, 'Clique comprar produto', `${nome} + ${idProd} + Posicao ${index}`);
+			pushDataLayer(
+				`Categoria ${currentCategory}`,
+				'Clique comprar produto',
+				`${nome} + ${idProd} + Posicao ${index}`
+			);
+		});
+
+		$('#seu-jeitinho .slick-prev').on('click', function() {
+			const currentCategory = $('#seu-jeitinho .tab button.-active').text();
+
+			pushDataLayer(
+				`Categoria ${currentCategory}`,
+				'Clique seta esquerda',
+				`Ver produtos para esquerda`
+			);
+		});
+
+		$('#seu-jeitinho .slick-next').on('click', function() {
+			const currentCategory = $('#seu-jeitinho .tab button.-active').text();
+
+			pushDataLayer(
+				`Categoria ${currentCategory}`,
+				'Clique seta direita',
+				`Ver produtos para direita`
+			);
+		});
+	};
+
+	this.descontoSection = () => {
+		$('#descontos .btn-tab').on('click', function() {
+			const currentCategory = $(this).text();
+
+			pushDataLayer(
+				`${currentCategory}`,
+				'Clique na categoria',
+				`Ver conteúdo do botão`
+			);
+		});
+
+		$('#descontos .box-produto').on('click', function(e) {
+			e.preventDefault();
+
+			const currentCategory = $('#descontos .btn-tab.-active').text(),
+				$this 	= $(this),
+				nome 	= $this.find('.image').attr('title'),
+				idProd 	= $this.attr('data-idproduto'),
+				index	= $this.parent('li').index();
+
+			pushDataLayer(
+				`Categoria ${currentCategory}`,
+				'Clique comprar produto',
+				`${nome} + ${idProd} + Posicao ${index}`
+			);
+		});
+
+		$('#descontos .slick-prev').on('click', function() {
+			const currentCategory = $('#descontos .btn-tab.-active').text();
+
+			pushDataLayer(
+				`Categoria ${currentCategory}`,
+				'Clique seta esquerda',
+				`Ver produtos para esquerda`
+			);
+		});
+
+		$('#descontos .slick-next').on('click', function() {
+			console.log('oi');
+			const currentCategory = $('#descontos .btn-tab.-active').text();
+
+			pushDataLayer(
+				`Categoria ${currentCategory}`,
+				'Clique seta direita',
+				`Ver produtos para direita`
+			);
 		});
 	};
 
