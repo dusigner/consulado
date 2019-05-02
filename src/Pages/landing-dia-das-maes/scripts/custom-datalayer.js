3712dc8074d2bf4998849198652688d88ca4f91e
Matheus Gomide <matheusggds@hotmail.com>
2019-05-02 16:41:35 -0300

    feat: landing dia das maes tageamento

--- src/Pages/landing-dia-das-maes/scripts/custom-datalayer.js
+++ src/Pages/landing-dia-das-maes/scripts/custom-datalayer.js
@@ -3,7 +3,34 @@
 *
 *
 */
+import { pushDataLayer } from 'modules/_datalayer-inline';
 
 Nitro.module('custom-datalayer', function() {
-	console.log('teste');
+
+	this.init = () => {
+		this.jeitinhoSection();
+
+		console.log('oi');
+
+	};
+
+	this.jeitinhoSection = () => {
+		$('#seu-jeitinho .tab button').on('click', function() {
+			pushDataLayer(`Categoria ${$(this).text()}`, 'Clique na categoria', 'Ver conteúdo do botão');
+		});
+
+		$('#seu-jeitinho .box-produto').on('click', function(e) {
+			e.preventDefault();
+
+			const currentCategory = $('#seu-jeitinho .tab button.-active').text(),
+				$this 	= $(this),
+				nome 	= $this.find('.image').attr('title'),
+				idProd 	= $this.attr('data-idproduto'),
+				index	= $this.parent('li').index();
+
+			pushDataLayer(`Categoria ${currentCategory}`, 'Clique comprar produto', `${nome} + ${idProd} + Posicao ${index}`);
+		});
+	};
+
+	this.init();
 });
