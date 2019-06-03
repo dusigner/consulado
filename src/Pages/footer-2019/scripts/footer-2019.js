6805ab6e2da912cbce2632953e5134c4993aa57e
douglasamarelo <d.lopes.nascimento@gmail.com>
2019-06-03 13:06:59 -0300

    footer

--- src/Pages/footer-2019/scripts/footer-2019.js
+++ src/Pages/footer-2019/scripts/footer-2019.js
@@ -3,16 +3,25 @@
 
 (function(){
 	const $canaisAtendimentoTtitle = $('.canais-atendimento__title');
+	const $openMobileNewsletter = $('.open-mobile-newsletter');
+	const $leadNewsletter = $('.lead-newsletter');
 
 	const footer = {};
 
 	footer.init = () => {
 		footer.toogleAtendimento();
+		footer.toogleNewsletterMobile();
 	};
 
 	footer.toogleAtendimento = () => {
 		$canaisAtendimentoTtitle.click(function() {
 			$(this).parent().toggleClass('is--active');
+		});
+	};
+
+	footer.toogleNewsletterMobile = () => {
+		$openMobileNewsletter.click(function() {
+			$leadNewsletter.toggleClass('is--mobile-active');
 		});
 	};
 
