7ab41289e82403fd3a32a6bd84865c35460bd46f
kazuhirobruno <brunokazuhiro@gmail.com>
2019-06-19 15:06:14 -0300

    feat: new style on filters section ICD-519

--- src/Scripts/modules/listagem/filters.js
+++ src/Scripts/modules/listagem/filters.js
@@ -164,15 +164,22 @@ Nitro.module('filters', ['order-by'], function (orderBy) {
 		$('.priceRange').remove();
 		self.priceRange();
 		self.specificationRange();
+		self.clearFilterRel();
 	};
 
 	this.mobileClearFilter = function() {
-		var $button = $('<button class="clear-filter">Limpar filtros</div>');
+		var $button = $(`<div class="mobile-buttons">
+							<button class="clear-filter">Limpar filtros</button>
+							<button class="close-filter">Voltar</button>
+						</div>`);
 
-		$filterWrapper.append($button);
+		$filterWrapper.prepend($button);
 
 		$('.clear-filter').click(function() {
 			self.clearFilter();
+		});
+
+		$('.close-filter').on('click', function() {
 			self.closeFilter();
 		});
 
@@ -433,23 +440,22 @@ Nitro.module('filters', ['order-by'], function (orderBy) {
 
 	this.openFilter = function() {
 		$('.open-filter').click(function() {
-			//console.log('teste');
-			if($('.overlay-filter').length === 0) {
-				$('body').prepend('<div class="overlay-filter"></div>');
-			} else {
-				$('.overlay-filter').removeClass('hide');
-			}
 			$filterWrapper.addClass('opened');
+			//console.log('teste');
+			 if($('.overlay-filter').length === 0) {
+			 	$('body').prepend('<div class="overlay-filter"></div>');
+			} else {
+			 	$('.overlay-filter').removeClass('hide');
+			}
 
 			$('.overlay-filter').unbind('click').click(function() {
-				self.closeFilter();
+			 	self.closeFilter();
 			});
 		});
 	};
 
 	this.closeFilter = function() {
 		$filterWrapper.removeClass('opened');
-		$('.overlay-filter').addClass('hide');
 	};
 
 
