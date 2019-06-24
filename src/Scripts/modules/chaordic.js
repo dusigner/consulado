24c3a360408d76057337d5ae64792e7ef3613be7
kazuhirobruno <brunokazuhiro@gmail.com>
2019-06-24 16:33:54 -0300

    feat: new styleguide to showcases ICD-528

--- src/Scripts/modules/chaordic.js
+++ src/Scripts/modules/chaordic.js
@@ -535,6 +535,16 @@ Nitro.module('chaordic', function() {
 		var placeholderDust;
 		$('body').hasClass('body-cart') ? placeholderDust = 'shelf-content-placeholder-cart' : placeholderDust = 'shelf-content-placeholder';
 
+		if(renderData[0] && renderData[0].name === 'Visualizados') {
+			renderData[0].title = 'Você <span class="shelf--visited">visitou</span>';
+			renderData[0].subtitle = 'Talvez se <span class="shelf--visited">interesse por</span>';
+		}
+
+		if(renderData[1] && renderData[1].name === 'Histórico') {
+			renderData[1].title = 'Últimos <span class="shelf--visited">vistos</span>';
+			renderData[1].subtitle = 'Relacionados aos <span class="shelf--visited">seus interesses</span>';
+		}
+
 		dust.render(placeholderDust, renderData, function(err, out) {
 			if (err) {
 				throw new Error('Chaordic Placeholder Dust error: ' + err);
