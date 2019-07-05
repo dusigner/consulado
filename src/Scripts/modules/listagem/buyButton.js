2cfeedce0864c9f94110882715aa385f8da1bdbf
kazuhirobruno <brunokazuhiro@gmail.com>
2019-07-05 15:30:12 -0300

    fix: fix labels ICD-571

--- src/Scripts/modules/listagem/buyButton.js
+++ src/Scripts/modules/listagem/buyButton.js
@@ -1,25 +1,25 @@
 'use strict';
 
 Nitro.module('buy-button', function () {
-
-	var _self = this;
-
 	// PREPARE DATA TO OBJECT -> RENDER
 	this.setup = function(){
 		let productBox = $('.list-content article');
 		let skus = productBox.find('.product-insertsku');
 
 		skus.each(function(index) {
-			let item = skus.eq(index).find('.from-shelf');
+			let item = skus.eq(index).find('.from-shelf'),
+				skuName = skus.eq(index).find('input[type=checkbox]').attr('name');
 
 			if(skus.find('.prod__selectSKU').children().length === 0) {
 				for (var i = 0; i < item.length; i++) {
-					let title = item.eq(i).find('input[type=text]').attr('title');
-					let skuId = item.eq(i).find('input[type=checkbox]').attr('rel');
-					skus.eq(index).parents('article').find('.prod__selectSKU').append(`
+					let title = item.eq(i).find('input[type=text]').attr('title'),
+						skuId = item.eq(i).find('input[type=checkbox]').attr('rel'),
+						objectClass = title.replace(/\s/g, '') + '_' + skuId;
+
+					skus.eq(index).parents('article').find('.prod__selectSKU').attr('name', skuName).append(`
 												<div class="sku__selector" data-title="${title}">
-													<input class="sku_radio" type="radio" id="${title}_${skuId}" name=${skuId}>
-													<label class="sku_title" for=${title}_${skuId}>${title}</label>
+													<input class="sku_radio" type="radio" id="${objectClass}" name=${skuName}>
+													<label class="sku_title" for="${objectClass}" name=${skuName}>${title}</label>
 												</div>`);
 				}
 			}
