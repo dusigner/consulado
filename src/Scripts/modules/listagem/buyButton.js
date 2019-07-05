3bcbaf33eb4c8261bc6dc8618f3de2bf9990da7c
kazuhirobruno <brunokazuhiro@gmail.com>
2019-07-05 16:54:20 -0300

    feat: set checked when there's only 1 sku available ICD-571

--- src/Scripts/modules/listagem/buyButton.js
+++ src/Scripts/modules/listagem/buyButton.js
@@ -8,22 +8,32 @@ Nitro.module('buy-button', function () {
 
 		skus.each(function(index) {
 			let item = skus.eq(index).find('.from-shelf'),
-				skuName = skus.eq(index).find('input[type=checkbox]').attr('name');
+				skuName = skus.eq(index).find('input[type=checkbox]').attr('name'),
+				sku = skus.eq(index).parents('.detalhes').find('.prod__selectSKU');
 
-			if(skus.find('.prod__selectSKU').children().length === 0) {
+
+			if(sku.children().length === 0) {
 				for (var i = 0; i < item.length; i++) {
 					let title = item.eq(i).find('input[type=text]').attr('title'),
 						skuId = item.eq(i).find('input[type=checkbox]').attr('rel'),
-						objectClass = title.replace(/\s/g, '') + '_' + skuId;
+						objectClass = title.replace(/\s/g, '') + '_' + skuId,
+						isAvailable = (item.eq(i).hasClass('unavailable')) ? 'unavailable' : 'available';
 
 					skus.eq(index).parents('article').find('.prod__selectSKU').attr('name', skuName).append(`
 												<div class="sku__selector" data-title="${title}">
-													<input class="sku_radio" type="radio" id="${objectClass}" name=${skuName}>
-													<label class="sku_title" for="${objectClass}" name=${skuName}>${title}</label>
+													<input class="sku_radio ${isAvailable}" type="radio" id="${objectClass}" name=${skuName}>
+													<label class="sku_title ${isAvailable}" for="${objectClass}" name=${skuName}>${title}</label>
 												</div>`);
+				}
+
+				sku.find('.unavailable').attr('disabled', true);
+				if(sku.find('.available').length === 2) {
+					sku.find('.available').attr('checked', 'checked');
 				}
 			}
 		});
+
+		//$('.prod__selectSKU').find('.unavailable').attr('disabled', true);
 	};
 
 	this.setup();
