3506956c1418a62f439969e413b5ba529938d3bd
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2019-07-11 12:04:30 -0300

    ICD-569 ICD-573 banner de cupom de desconto

--- src/Scripts/modules/_pagina-cervejeiras.js
+++ src/Scripts/modules/_pagina-cervejeiras.js
@@ -1,3 +1,5 @@
+const toastr = require('vendors/toastr');
+
 (() => {
 	'use strict';
 
@@ -8,6 +10,7 @@
 	cervejeiras.init = () => {
 		cervejeiras.slider();
 		cervejeiras.selectColor();
+		cervejeiras.copyCupom();
 	};
 
 	cervejeiras.slider = () => {
@@ -29,6 +32,57 @@
 			slidesToScroll: 1,
 			slidesToShow: 1,
 			initialSlide: 1
+		});
+	};
+
+	/**
+	 * Show a toastr alert with the coupon code
+	 * @param {string} code Coupon code
+	 */
+	cervejeiras.couponToastr = code => {
+		jQuery($ => {
+			const windowWidth = $(window).width();
+			const position = windowWidth <= 320 ? 'toast-bottom-center' : 'toast-top-center';
+
+			toastr.options = {
+				closeButton: true,
+				debug: false,
+				newestOnTop: true,
+				progressBar: false,
+				positionClass: position,
+				preventDuplicates: true,
+				onclick: null,
+				showDuration: '300',
+				hideDuration: '1000',
+				timeOut: '5000',
+				extendedTimeOut: '1000',
+				showEasing: 'swing',
+				hideEasing: 'linear',
+				showMethod: 'fadeIn',
+				hideMethod: 'fadeOut',
+			};
+			toastr.info(`Código ${code} copiado`);
+		});
+	};
+
+	cervejeiras.copyCupom = () => {
+		const cupom = $('.cupom').text();
+		$('.category-page-top-banner, .copiar').click((e) => {
+			e.preventDefault();
+
+			const temp = $('<input>');
+
+			$('body').append(temp);
+
+			temp.val(cupom).select();
+
+			document.execCommand('copy');
+
+			cervejeiras.couponToastr(cupom);
+
+			temp.remove();
+
+
 		});
 	};
 
