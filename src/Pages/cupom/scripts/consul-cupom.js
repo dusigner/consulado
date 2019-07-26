523ab5e6d85baae260bca63054566d2a2517e89b
Dayane Alves <dayane.alves@jussi.com.br>
2019-07-26 18:12:54 -0300

    Feat: Fazendo a página de cupom ICD-338

--- src/Pages/cupom/scripts/consul-cupom.js
+++ src/Pages/cupom/scripts/consul-cupom.js
@@ -9,10 +9,18 @@ Nitro.setup([], function() {
 		slidesToScroll: 4,
 		responsive: [
 			{
+				breakpoint: 1800,
+				settings: {
+					variableWidth: true,
+					slidesToShow: 4,
+					slidesToScroll: 4
+				}
+			},
+			{
 				breakpoint: 1024,
 				settings: {
 					slidesToShow: 3,
-					slidesToScroll: 3,
+					slidesToScroll: 2,
 					dots: true
 				}
 			},
