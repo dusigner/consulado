cf8996d271f621ce036bfa570b31f9ab51d9c6a5
unknown <Dayane@J00637.jussi.local>
2019-07-29 18:24:11 -0300

    Feat: Página de cupons ICD-338

--- src/Pages/cupom/scripts/consul-cupom.js
+++ src/Pages/cupom/scripts/consul-cupom.js
@@ -2,40 +2,29 @@ import 'vendors/nitro';
 import 'vendors/slick';
 
 Nitro.setup([], function() {
-	$('.slick').slick({
-		dots: true,
-		speed: 300,
+	$('.slider').slick({
 		slidesToShow: 4,
-		slidesToScroll: 4,
+		infinite: false,
 		responsive: [
-			{
-				breakpoint: 1800,
-				settings: {
-					variableWidth: true,
-					slidesToShow: 4,
-					slidesToScroll: 4
-				}
-			},
 			{
 				breakpoint: 1024,
 				settings: {
 					slidesToShow: 3,
-					slidesToScroll: 2,
 					dots: true
 				}
 			},
 			{
-				breakpoint: 600,
+				breakpoint: 768,
 				settings: {
 					slidesToShow: 3,
-					slidesToScroll: 2
+					dots: true
 				}
 			},
 			{
 				breakpoint: 480,
 				settings: {
 					slidesToShow: 1,
-					slidesToScroll: 1
+					dots: true
 				}
 			}
 		]
