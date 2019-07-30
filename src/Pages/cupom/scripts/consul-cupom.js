e23e83db2fe3ac8afaafe19459c1a2d24a59fc1f
Dayane Siqueira <dayane.alves@jussi.com.br>
2019-07-30 18:24:08 -0300

    Fix: Fazendo mobile página cupons ICD-338

--- src/Pages/cupom/scripts/consul-cupom.js
+++ src/Pages/cupom/scripts/consul-cupom.js
@@ -2,31 +2,57 @@ import 'vendors/nitro';
 import 'vendors/slick';
 
 Nitro.setup([], function() {
-	$('.slider').slick({
-		slidesToShow: 4,
-		infinite: false,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 3,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					dots: true
-				}
-			}
-		]
-	});
+    $('.slider').slick({
+        slidesToShow: 4,
+        infinite: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+    $('.slider-prod').slick({
+        arrows: true,
+        slidesToShow: 3,
+        infinite: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
 });
