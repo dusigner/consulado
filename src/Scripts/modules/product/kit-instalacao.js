0ff6729c481b800f6989b0fb6127966cbe712f95
douglasamarelo <d.lopes.nascimento@gmail.com>
2019-08-07 18:28:35 -0300

    feat: Kit de instalação

--- src/Scripts/modules/product/kit-instalacao.js
+++ src/Scripts/modules/product/kit-instalacao.js
@@ -4,81 +4,106 @@
 Nitro.module('kit-instalacao', function() {
 	const self = this;
 	const $selectVoltage = $('.select-voltage');
+
 	const template = `
-		<!-- Kit instalação -->
-		<div class="kit-instalacao">
-			<strong class="kit-instalacao__title specification">Selecione o tipo de gás</strong>
-			<div class="skuList">
-				<span>
-					<input type="radio" name="botijao" class="kit-instalacao__input" id="botijao" value="Botijão">
-					<label for="botijao" class="kit-instalacao__label">Botijão</label>
+	<!-- Kit instalação -->
+	<div class="kit-instalacao">
+		<strong class="kit-instalacao__title specification">Selecione o tipo de gás</strong>
+		<div class="skuList">
+			<span>
+				<input type="radio" name="gas-type" class="kit-instalacao__input" id="botijao" value="Botijão">
+				<label for="botijao" class="kit-instalacao__label">Botijão</label>
 
-					<input type="radio" name="gas-natural" class="kit-instalacao__input" id="gas-natural" value="Gás natural">
-					<label for="gas-natural" class="kit-instalacao__label">Gás natural</label>
-				</span>
-			</div>
-
-			<div class="kit-instalacao-container">
-				<p class="kit-instalacao__description">
-					Aproveite e leve seu kit de instalação
-				</p>
-
-				<!-- Product Kit -->
-				<div class="kit-product">
-					<div class="kit-product__item kit-product__item-image">
-						<img src="https://consul.vteximg.com.br/arquivos/kit-botijao.png" alt="">
-					</div>
-
-					<div class="kit-product__item kit-product__item-title">
-						<h2>Kit de Instalação para Gás Encanado</h2>
-						<span>W10866791</span>
-					</div>
-
-					<div class="kit-product__item kit-product__item-price">
-						<div class="de">De R$ 139,99</div>
-						<div class="por">Por R$ 136,90</div>
-					</div>
-
-					<div class="kit-product__item kit-product__item-select">
-						<input type="checkbox" name="" id="kit-product-item-select-1" />
-						<label for="kit-product-item-select-1"></label>
-					</div>
-				</div>
-				<!-- /Product Kit -->
-
-				<!-- Product Kit -->
-				<div class="kit-product type-gas-natural">
-					<div class="kit-product__item kit-product__item-image">
-						<img src="https://consul.vteximg.com.br/arquivos/kit-botijao.png" alt="">
-					</div>
-
-					<div class="kit-product__item kit-product__item-title">
-						<h2>Kit de Instalação para Gás Encanado</h2>
-						<span>W10866791</span>
-					</div>
-
-					<div class="kit-product__item kit-product__item-price">
-						<div class="de">De R$ 139,99</div>
-						<div class="por">Por R$ 136,90</div>
-					</div>
-
-					<div class="kit-product__item kit-product__item-select">
-						<input type="checkbox" name="" id="kit-product-item-select" />
-						<label for="kit-product-item-select"></label>
-					</div>
-				</div>
-				<!-- /Product Kit -->
-			</div>
+				<input type="radio" name="gas-type" class="kit-instalacao__input" id="gas-natural" value="Gás natural">
+				<label for="gas-natural" class="kit-instalacao__label">Gás natural</label>
+			</span>
 		</div>
-		<!-- /Kit instalação -->
+
+		<div class="kit-instalacao-container">
+			<p class="kit-instalacao__description">
+				Aproveite e leve seu kit de instalação
+			</p>
+
+			<!-- Product Kit -->
+			<div class="kit-product">
+				<div class="kit-product__item kit-product__item-image">
+					<img src="https://consul.vteximg.com.br/arquivos/kit-botijao.png" alt="">
+				</div>
+
+				<div class="kit-product__item kit-product__item-title">
+					<h2>Kit de Instalação para Gás Encanado</h2>
+					<span>W10866791</span>
+				</div>
+
+				<div class="kit-product__item kit-product__item-price">
+					<div class="de">De R$ 139,99</div>
+					<div class="por">Por R$ 136,90</div>
+				</div>
+
+				<div class="kit-product__item kit-product__item-select">
+					<input type="checkbox" name="" id="kit-product-item-select" />
+					<label for="kit-product-item-select"></label>
+				</div>
+
+				<div class="kit-product__info">
+					Conversão gratuita
+					<i class="icon icon-question"></i>
+				</div>
+			</div>
+			<!-- /Product Kit -->
+
+			<!-- Product Kit -->
+			<div class="kit-product type-gas-natural">
+				<div class="kit-product__item kit-product__item-image">
+					<img src="https://consul.vteximg.com.br/arquivos/kit-botijao.png" alt="">
+				</div>
+
+				<div class="kit-product__item kit-product__item-title">
+					<h2>Kit de Instalação para Gás Encanado</h2>
+					<span>W10866791</span>
+				</div>
+
+				<div class="kit-product__item kit-product__item-price">
+					<div class="de">De R$ 139,99</div>
+					<div class="por">Por R$ 136,90</div>
+				</div>
+
+				<div class="kit-product__item kit-product__item-select">
+					<input type="checkbox" name="" id="kit-product-item-select-1" />
+					<label for="kit-product-item-select-1"></label>
+				</div>
+
+				<div class="kit-product__info">
+					Conversão gratuita
+					<i class="icon icon-question"></i>
+				</div>
+			</div>
+			<!-- /Product Kit -->
+		</div>
+	</div>
+	<!-- /Kit instalação -->
 	`;
 
 	this.init = () => {
 		console.log('Init kit-instalacao...');
-
 		$selectVoltage.attr('class', 'select-voltage');
-
 		$selectVoltage.after(template);
+
+		this.selectGasType();
+	};
+
+	this.selectGasType = () => {
+		const $kitInstalacao = $('.kit-instalacao');
+		const $gasType = $('.kit-instalacao__input');
+
+		$gasType.on('change', (e) => {
+			let gasClass = e.target.value;
+			gasClass = gasClass.toLowerCase();
+			gasClass = gasClass.replace(/\s/, '-');
+			gasClass = gasClass.replace(/[ãàáâ]/, 'a');
+
+			$kitInstalacao.attr('class', `kit-instalacao ${gasClass}`);
+		});
 	};
 
 
