6f931320e8ff89c05bf293ce49c3df3fcf9288c4
douglasamarelo <d.lopes.nascimento@gmail.com>
2019-08-09 18:10:10 -0300

    feat: Kit instalação

--- src/Scripts/modules/product/kit-instalacao.js
+++ src/Scripts/modules/product/kit-instalacao.js
@@ -2,111 +2,126 @@
 'use strict';
 
 Nitro.module('kit-instalacao', function() {
+	// Variables
 	const self = this;
-	const $selectVoltage = $('.select-voltage');
+	const $kitInstalacao = $('.kit-instalacao');
+	const $gasType = $('.kit-instalacao__input');
+	const $kitInstalacaoContainer = $('.kit-instalacao-container');
 
-	const template = `
-	<!-- Kit instalação -->
-	<div class="kit-instalacao">
-		<strong class="kit-instalacao__title specification">Selecione o tipo de gás</strong>
-		<div class="skuList">
-			<span>
-				<input type="radio" name="gas-type" class="kit-instalacao__input" id="botijao" value="Botijão">
-				<label for="botijao" class="kit-instalacao__label">Botijão</label>
-
-				<input type="radio" name="gas-type" class="kit-instalacao__input" id="gas-natural" value="Gás natural">
-				<label for="gas-natural" class="kit-instalacao__label">Gás natural</label>
-			</span>
-		</div>
-
-		<div class="kit-instalacao-container">
-			<p class="kit-instalacao__description">
-				Aproveite e leve seu kit de instalação
-			</p>
-
-			<!-- Product Kit -->
-			<div class="kit-product">
-				<div class="kit-product__item kit-product__item-image">
-					<img src="https://consul.vteximg.com.br/arquivos/kit-botijao.png" alt="">
-				</div>
-
-				<div class="kit-product__item kit-product__item-title">
-					<h2>Kit de Instalação para Gás Encanado</h2>
-					<span>W10866791</span>
-				</div>
-
-				<div class="kit-product__item kit-product__item-price">
-					<div class="de">De R$ 139,99</div>
-					<div class="por">Por R$ 136,90</div>
-				</div>
-
-				<div class="kit-product__item kit-product__item-select">
-					<input type="checkbox" name="" id="kit-product-item-select" />
-					<label for="kit-product-item-select"></label>
-				</div>
-
-				<div class="kit-product__info">
-					Conversão gratuita
-					<i class="icon icon-question"></i>
-				</div>
-			</div>
-			<!-- /Product Kit -->
-
-			<!-- Product Kit -->
-			<div class="kit-product type-gas-natural">
-				<div class="kit-product__item kit-product__item-image">
-					<img src="https://consul.vteximg.com.br/arquivos/kit-botijao.png" alt="">
-				</div>
-
-				<div class="kit-product__item kit-product__item-title">
-					<h2>Kit de Instalação para Gás Encanado</h2>
-					<span>W10866791</span>
-				</div>
-
-				<div class="kit-product__item kit-product__item-price">
-					<div class="de">De R$ 139,99</div>
-					<div class="por">Por R$ 136,90</div>
-				</div>
-
-				<div class="kit-product__item kit-product__item-select">
-					<input type="checkbox" name="" id="kit-product-item-select-1" />
-					<label for="kit-product-item-select-1"></label>
-				</div>
-
-				<div class="kit-product__info">
-					Conversão gratuita
-					<i class="icon icon-question"></i>
-				</div>
-			</div>
-			<!-- /Product Kit -->
-		</div>
-	</div>
-	<!-- /Kit instalação -->
-	`;
-
-	this.init = () => {
-		console.log('Init kit-instalacao...');
-		$selectVoltage.attr('class', 'select-voltage');
-		$selectVoltage.after(template);
-
-		this.selectGasType();
-	};
-
+	// Selecione o tipo de gás
 	this.selectGasType = () => {
-		const $kitInstalacao = $('.kit-instalacao');
-		const $gasType = $('.kit-instalacao__input');
-		let gasClass = '';
-
 		$gasType.on('change', (e) => {
-			gasClass = e.target.value;
-			gasClass = gasClass.toLowerCase();
-			gasClass = gasClass.replace(/\s/, '-');
-			gasClass = gasClass.replace(/[ãàáâ]/, 'a');
+			const cssClass = this.textToCssClass(e.target.value);
 
-			$kitInstalacao.attr('class', `kit-instalacao ${gasClass}`);
+			if (!$kitInstalacao.hasClass('kit-loaded')) {
+				this.loadingAnimation();
+				this.getProducts();
+
+				$kitInstalacao.addClass(`kit-loaded`);
+			}
+
+			$kitInstalacao.attr('data-gastype', `${cssClass}`);
 		});
 	};
 
+	// Nomalizar o texto e retornalo com um padrao de css
+	this.textToCssClass = (text) => {
+		let gasClass = '';
+		gasClass = text;
+		gasClass = gasClass.toLowerCase();
+		gasClass = gasClass.replace(/\s/, '-');
+		gasClass = gasClass.replace(/[ãàáâ]/, 'a');
 
-	self.init();
+		return gasClass;
+	};
+
+	// Animação de loading...
+	this.loadingAnimation = () => {
+		$kitInstalacao.toggleClass('kit-is-loading');
+	};
+
+	// Busca todos os produtos da coleção
+	this.getProducts = () => {
+		fetch('/api/catalog_system/pub/products/search?fq=productClusterIds:1765')
+			.then(resp => resp.json())
+			.then(data => this.printProducts(data))
+			.then(() => this.loadingAnimation())
+			.catch(error => {
+				this.printError();
+				console.error('###Error', error);
+			});
+	};
+
+	// Exibe os produtos na tela de produto
+	this.printProducts = (data) => {
+		const products = data;
+
+		products.map(product => {
+			$kitInstalacaoContainer.after(this.productKitTemplate(product));
+		});
+	};
+
+	// Template a ser exibido na tela de produto
+	this.productKitTemplate = (product) => {
+		const { productId, productTitle, productReference } = product;
+		const productImage = product.items[0].images[0].imageUrl;
+		let { ListPrice, Price } = product.items[0].sellers[0].commertialOffer;
+
+		// Formatar o preço antes de exibir na tela
+		ListPrice = _.formatCurrency(ListPrice);
+		Price     = _.formatCurrency(Price);
+
+		// Product Kit
+		const kitTemplate = `
+			<div class="kit-product product-${productId}">
+				<div class="kit-product__item kit-product__item-image">
+					<img src="${productImage}" alt="${productTitle}">
+				</div>
+
+				<div class="kit-product__item kit-product__item-title">
+					<h2>${productTitle}</h2>
+					<span>${productReference}</span>
+				</div>
+
+				<div class="kit-product__item kit-product__item-price">
+					<div class="de">De R$ ${ListPrice}</div>
+					<div class="por">Por R$ ${Price}</div>
+				</div>
+
+				<div class="kit-product__item kit-product__item-select">
+					<input type="checkbox" name="product-${productId}" id="product-${productId}" />
+					<label for="product-${productId}"></label>
+				</div>
+
+				<div class="kit-product__info">
+					Conversão gratuita <i class="icon icon-question"></i>
+				</div>
+			</div>
+		`;
+
+		return kitTemplate;
+	};
+
+	// Template de erro
+	this.printError = () => {
+		const errorTemplate = `
+			<div class="kit-instalacao__error">
+				<p>Ops! Tivemos algum problema para carregar os produtos.</p>
+				<button class="secondary-button">Tentar novamente</button>
+			</div>
+		`;
+
+		$kitInstalacaoContainer.after(errorTemplate);
+		this.handleError();
+	};
+
+	this.handleError = () => {
+		const $kitError = $('.kit-instalacao__error');
+
+		$kitError.find('button').click(() => {
+			this.getProducts();
+		});
+	};
+
+	self.selectGasType();
 });
