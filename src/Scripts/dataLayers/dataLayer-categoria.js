7584dd14c456a003d400d71694808e872f4f9318
kazuhirobruno <brunokazuhiro@gmail.com>
2019-08-15 13:15:06 -0300

    feat: tracking on category banner ICD-690

--- src/Scripts/dataLayers/dataLayer-categoria.js
+++ src/Scripts/dataLayers/dataLayer-categoria.js
@@ -1,6 +1,15 @@
 import { checkInlineDatalayers, pushDataLayer } from 'modules/_datalayer-inline';
 
 Nitro.module('dataLayer-categoria', function() {
+
+	this.init = () => {
+		checkInlineDatalayers();
+
+		this.breadCrumb();
+		this.shelfSelectSku();
+		this.bannerDataLayer();
+	},
+
 	this.breadCrumb = () => {
 		$('.breadcrumb a').click(function() {
 			const category = $(this).text();
@@ -23,8 +32,17 @@ Nitro.module('dataLayer-categoria', function() {
 		});
 	};
 
-	checkInlineDatalayers();
+	this.bannerDataLayer = () => {
+		const category = dataLayer[0].categoryName;
+		$('.category-page-top-banner a').on('click', function() {
+			dataLayer.push({
+				event: 'generic',
+				category: `[SQUAD] Banner Categoria - ${category}`,
+				action: 'Clique no banner',
+				label: 'Banner Promocional'
+			});
+		});
+	},
 
-	this.breadCrumb();
-	this.shelfSelectSku();
+	this.init();
 });
