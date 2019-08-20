4a04fa655fda1e281b16195d77ba78746fb75189
Dayane Siqueira <dayane.alves@jussi.com.br>
2019-08-20 16:03:25 -0300

    Dinamizando box cupom e vitrine com as coleções com desconto ICD--338

--- src/Scripts/modules/prateleira.js
+++ src/Scripts/modules/prateleira.js
@@ -3,17 +3,22 @@
 require('modules/listagem/percentoff');
 
 Nitro.module('prateleira', ['percentoff'], function() {
-	$('.helperComplement').remove();
 
-	var $shelf = $('.prateleira-slider');
+	this.init = () =>{
+		$('.helperComplement').remove();
 
-	this.hiddenEmptyShelves = (function() {
-		$shelf.each(function() {
-			if ($(this).is(':empty')) {
-				$(this)
-					.closest('section.slider')
-					.hide();
-			}
-		});
-	})();
+		var $shelf = $('.prateleira-slider');
+
+		this.hiddenEmptyShelves = (function() {
+			$shelf.each(function() {
+				if ($(this).is(':empty')) {
+					$(this)
+						.closest('section.slider')
+						.hide();
+				}
+			});
+		})();
+	}
+
+	this.init();
 });
