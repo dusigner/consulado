4323e7cc30b7e4a6a365f45047633d1ee81589e2
GAC-GOC <gabriel_afonso_da_costa_corebiz@whirlpool.com>
2019-09-25 16:14:23 -0300

    link button

--- src/Scripts/modules/embutidos/eventos.embutidos.js
+++ src/Scripts/modules/embutidos/eventos.embutidos.js
@@ -252,9 +252,8 @@ const Eventos = {
 		totalPor = formatReal(totalPor);
 		totalDe = formatReal(totalDe);
 		$('.vitrine ul').append(
-			`<li><h1>Comprando Junto</h1> <p class="de-final"> De: R$ ${totalDe}</p> <p class="por-final">Por: R$ ${totalPor}</p> <a class="btn-primary-button" >Ir para Carrinho</a></li>`
+			`<li><h1>Comprando Junto</h1> <p class="de-final"> De: R$ ${totalDe}</p> <p class="por-final">Por: R$ ${totalPor}</p> <a class="btn-primary-button go" href="/checkout/cart/add?sku=429&qty=1&seller=1&redirect=true&sc=3&sku=565&qty=1&seller=1&redirect=true&sc=3&sku=490&qty=1&seller=1&redirect=true&sc=3" >Ir para Carrinho</a></li>`
 		);
-
 		//Açoes
 		$('.remove-item').click(x => {
 			x.currentTarget.classList = 'remove-item';
