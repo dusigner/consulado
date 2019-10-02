d7deee45b7c6c909fda167320812e3516b1363ff
brunoannunciato <brunobannunciato@gmail.com>
2019-10-02 18:00:03 -0300

    WPD-652 | adaptação de depoimentos para mobile

--- src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
+++ src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
@@ -2,5 +2,27 @@
 
 require('vendors/slick');
 
+$( document ).ready(function() {
+	initSlider();
+});
 
-$('.depoiments__slider').slick()
+
+const adaptHtmlToSliderMobile = () => {
+	if ($(window).width() < 920) {
+		$('.depoiments__content, .card').unwrap()
+		$('.card').wrap('<div class="mobile-slide"></div>')
+	}
+}
+
+const initSlider = () => {
+	adaptHtmlToSliderMobile();
+
+	$('.depoiments__slider').slick({
+		adaptiveHeight: true,
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		dots: true,
+		arrows: true
+	});
+}
+
