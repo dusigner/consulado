62474eeb96c64e253d33e811278c43d1a9ecfdf3
brunoannunciato <brunobannunciato@gmail.com>
2019-10-03 18:17:06 -0300

    WPD-652 | botando o contador pra funcionar

--- src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
+++ src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
@@ -39,3 +39,62 @@ const initCountdownSlider = () => {
 	}
 }
 
+// -------------------------------------------
+//       CÓDIGO ROUBADO DA LP DO ANO PASSADO
+// -------------------------------------------
+
+var contador;
+
+var Index = {
+
+	init: function(){
+		contador = setInterval(this.countdown, 1000);
+	},
+
+	calculateTimeRemaining: function( endDate ) {
+		var total   = Date.parse( endDate ) - Date.parse( new Date(), 'mm-dd-yyyy' ),
+			seconds = Math.floor( ( total / 1000) % 60 ),
+			minutes = Math.floor( ( ( total / 1000 ) / 60 ) % 60 ),
+			hours   = Math.floor( ( total / ( ( 1000 * 60 ) * 60 ) ) % 24 ),
+			days    = Math.floor( total / ( ( ( 1000 * 60 ) * 60 ) * 24 ) ),
+			timeRemaining = {
+				'total'   : total,
+				'days'    : days,
+				'hours'   : hours,
+				'minutes' : minutes,
+				'seconds' : seconds
+			};
+
+		return timeRemaining;
+	},
+
+	countdown: function() {
+		var endDate       = '2019/11/29',
+			$countdown    = $('.countdown'),
+			$days         = $countdown.find('.days.indicators__number'),
+			$hours        = $countdown.find('.hours.indicators__number'),
+			$minutes      = $countdown.find('.minutes.indicators__number'),
+			timeRemaining = Index.calculateTimeRemaining(endDate),
+			days          = timeRemaining.days,
+			hours         = timeRemaining.hours,
+			minutes       = timeRemaining.minutes,
+			seconds       = timeRemaining.seconds;
+
+		$days.text(days < 10 ? '0' + days : days);
+		$hours.text(hours < 10 ? '0' + hours : hours);
+		$minutes.text(minutes < 10 ? '0' + minutes : minutes);
+
+		if ( days + hours + minutes + seconds <= 0 ) {
+			clearInterval(contador);
+
+			$('.lpbf-contador').addClass('is--end');
+		}
+	}
+};
+
+Index.init();
+
+// -------------------------------------------
+//       FIM DO CÓDIGO ROUBADO
+// -------------------------------------------
+
