bc3151d94f543ccaae89bde5df0e3f91a316fcbc
Fabio Haddad <fabiohpaes@gmail.com>
2019-10-08 03:43:05 -0300

    WPD-652 Implementation Steeps and validate Jquery Form

--- src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
+++ src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
@@ -3,12 +3,103 @@
 require('vendors/slick');
 
 $( document ).ready(function() {
+	initLeadsBlackfriday();
 	initDepoimentsSlider();
 	initCountdownSlider();
 	createAccordeon($('.category__title'))
 	createAccordeon($('.faq__header'))
+
+	jQuery.extend(jQuery.validator.messages, {
+		required: 'Este campo &eacute; requerido.',
+		remote: 'Por favor, corrija este campo.',
+		email: 'Por favor, forne&ccedil;a um endere&ccedil;o eletr&ocirc;nico v&aacute;lido.',
+		url: 'Por favor, forne&ccedil;a uma URL v&aacute;lida.',
+		date: 'Por favor, forne&ccedil;a uma data v&aacute;lida.',
+		dateISO: 'Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).',
+		number: 'Por favor, forne&ccedil;a um n&uacute;mero v&aacute;lido.',
+		digits: 'Por favor, forne&ccedil;a somente d&iacute;gitos.',
+		creditcard: 'Por favor, forne&ccedil;a um cart&atilde;o de cr&eacute;dito v&aacute;lido.',
+		equalTo: 'Por favor, forne&ccedil;a o mesmo valor novamente.',
+		accept: 'Por favor, forne&ccedil;a um valor com uma extens&atilde;o v&aacute;lida.',
+		maxlength: jQuery.validator.format('Por favor, forne&ccedil;a n&atilde;o mais que {0} caracteres.'),
+		minlength: jQuery.validator.format('Por favor, forne&ccedil;a ao menos {0} caracteres.'),
+		rangelength: jQuery.validator.format('Por favor, forne&ccedil;a um valor entre {0} e {1} caracteres de comprimento.'),
+		range: jQuery.validator.format('Por favor, forne&ccedil;a um valor entre {0} e {1}.'),
+		max: jQuery.validator.format('Por favor, forne&ccedil;a um valor menor ou igual a {0}.'),
+		min: jQuery.validator.format('Por favor, forne&ccedil;a um valor maior ou igual a {0}.')
+	});
 });
 
+const initLeadsBlackfriday = () => {
+
+	var form = $('#contact-lbf2019');
+
+
+	form.validate({
+		debug: true,
+		rules: {
+			firstName: {
+				required: true,
+
+			},
+			email: {
+				required: true,
+				email: true,
+
+			}
+		}
+	});
+
+	$('.btn1').on('click', function(){
+		// console.log('#### CLICOU NO BTN 1 ###');
+		var steep1 = false;
+		if ($('#firstName').val() !== '' && $('#email').val() !== '') {
+			steep1 = true;
+		}
+		if($('#email-error').text() !==''){
+			steep1 = false;
+		}
+		if ($('#term').attr('checked') === undefined){
+			steep1 = false;
+		}
+		if(steep1 === true) {
+			$('.steep1').fadeOut();
+			$('.steep2').delay(500).fadeIn();
+
+			// console.log($('#firstName').val());
+			// console.log($('#email').val());
+			// console.log($('#term').val());
+		}
+	});
+
+	$('.btn2').on('click', function(){
+		// console.log('#### CLICOU NO BTN 2 ###');
+		$('.steep2').fadeOut();
+		$('.steep3').delay(500).fadeIn();
+
+		// console.log($('#geladeiras').val());
+		// console.log($('#fogoes').val());
+		// console.log($('#arcondicionado').val());
+		// console.log($('#freezers').val());
+		// console.log($('#lavadoras').val());
+		// console.log($('#cervejeiras').val());
+		// console.log($('#coifas').val());
+	});
+
+	$('.btn3').on('click', function(){
+		// console.log('#### CLICOU NO BTN 3 ###');
+		$('.steep3').fadeOut();
+		$('.steep4').delay(500).fadeIn();
+
+		// console.log($('#geladeiras').val());
+		// console.log($('#fogoes').val());
+		// console.log($('#arcondicionado').val());
+		// console.log($('#freezers').val());
+		// console.log($('#lavadoras').val());
+		// console.log($('#cervejeiras').val());
+		// console.log($('#coifas').val());
+	});
+}
 
 const adaptHtmlToSliderMobile = () => {
 	if ($(window).width() < 920) {
