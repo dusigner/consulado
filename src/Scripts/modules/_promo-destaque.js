b7cec2964aea5e270bf19dc132f6e45923e46f4d
douglasamarelo <d.lopes.nascimento@gmail.com>
2019-10-14 19:18:29 -0300

    Promoções em destaque - com automatização de cadastro

--- src/Scripts/modules/_promo-destaque.js
+++ src/Scripts/modules/_promo-destaque.js
@@ -1,5 +1,5 @@
 // Transforma o texto do front em números para o cálculo do desconto.
-const valueFormat = text => {
+const formatValue = text => {
 	let value = text
 		.replace('R$ ', '')
 		.replace('.', '')
@@ -24,48 +24,80 @@ const discountValidate = (discount, productPrice) => {
 	return !!(prodDiscount >= minPrice && prodDiscount <= maxPrice && prodDiscount < prodPrice);
 };
 
-// Promo destaque - Dia das mães
+const clearText = str => {
+	let text = str.replace(/\s+/gmi, '-');
+	text = text.toLowerCase();
+
+	return text;
+};
+
+// Promoção para as prateleiras
 const promoDestaque = produto => {
-	const hasPromo = produto.find('.FlagsHightLight .flag[class*="_promo-destaque_"]');
+	const hasPromo = produto.find('.FlagsHightLight .flag[class*="__promo__"]');
 	const $prodInfo = produto.find('.prod-info, .shelf-item__info');
-	const precoDe = valueFormat(produto.find('.de .val').text());
-	const precoPor = valueFormat(produto.find('.por .val').text());
+	const precoDe = formatValue(produto.find('.de .val').text());
+	const precoPor = formatValue(produto.find('.por .val').text());
 	const desconto = discountCalculate(precoDe, precoPor);
 
-	if (hasPromo.length && precoDe.length && discountValidate(desconto, precoPor)) {
+	// if (hasPromo.length && precoDe.length && discountValidate(desconto, precoPor)) {
+	if (hasPromo.length) {
+		const promoText = hasPromo.text().split('__');
+		const promoPreTitle = promoText[2];
+		const promoTitle = promoText[3];
+		const promoColor = promoText[4];
+
 		const $promoDestaque = `
 			<div class="promo-destaque">
-				<div class="promo-destaque__text">
-					Saldão<br />
-					DIA DAS MÃES
+				<div class="promo-destaque__text" style="color: ${promoColor}">
+					<div class="promo-destaque__icon" style="background-image: url('//loja.consul.com.br/arquivos/promo__${clearText(promoPreTitle + '-' + promoTitle)}.png')"></div>
+					<div class="promo-destaque__icon" style="background-image: url('//loja.consul.com.br/arquivos/promo__${clearText(promoPreTitle + '-' + promoTitle)}__hover.png')"></div>
+
+					${promoPreTitle}<br />
+					${promoTitle}
 				</div>
 				<div class="promo-destaque__price">
 					R$-${discountFormat(desconto)}
 				</div>
+
+				<div class="promo-destaque__bg" style="background: ${promoColor}"></div>
 			</div>
 		`;
 
 		$prodInfo.prepend($promoDestaque);
+
+		// $.each(hasPromo, function(idx, $el) {
+
+		// });
 	}
 };
 
+// Promoção para a página de produto
 const prodPromoDestaque = () => {
-	const hasPromo = $('.prod-selos .flag[class*="_promo-destaque_"]');
+	const hasPromo = $('.prod-selos .flag[class*="__promo__"]');
 	const $prodPreco = $('.prod-preco');
-	const precoDe = valueFormat($prodPreco.find('.skuListPrice').text());
-	const precoPor = valueFormat($prodPreco.find('.skuBestPrice').text());
+	const precoDe = formatValue($prodPreco.find('.skuListPrice').text());
+	const precoPor = formatValue($prodPreco.find('.skuBestPrice').text());
 	const desconto = discountCalculate(precoDe, precoPor);
 
-	if (hasPromo.length && precoDe.length && discountValidate(desconto, precoPor)) {
+	// if (hasPromo.length && precoDe.length && discountValidate(desconto, precoPor)) {
+	if (hasPromo.length) {
+		const promoText = hasPromo.text().split('__');
+		const promoPreTitle = promoText[2];
+		const promoTitle = promoText[3];
+		const promoColor = promoText[4];
+
 		const $promoDestaque = `
 			<div class="promo-destaque promo-produto">
 				<div class="promo-destaque__text">
-					Saldão<br />
-					DIA DAS MÃES
+					<div class="promo-destaque__icon" style="background-image: url('//loja.consul.com.br/arquivos/promo__${clearText(promoPreTitle + '-' + promoTitle)}__hover.png')"></div>
+					${promoPreTitle}<br />
+					${promoTitle}
 				</div>
-				<div class="promo-destaque__price">
+				<div class="promo-destaque__price" style="color: ${promoColor}">
 					Produto com <span>R$${discountFormat(desconto)} de desconto</span>
 				</div>
+
+				<div class="promo-destaque__bg" style="background: ${promoColor}"></div>
 			</div>
 		`;
 
