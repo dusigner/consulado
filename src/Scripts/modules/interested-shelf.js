166ab212b8a75f93e19feb4bab341ccc6b0c84c8
kazuhirobruno <brunokazuhiro@gmail.com>
2019-10-21 12:52:11 -0300

    feat: create shelf ICD-802

--- src/Scripts/modules/interested-shelf.js
+++ src/Scripts/modules/interested-shelf.js
@@ -1,24 +1,23 @@
 Nitro.module('interested-shelf', function() {
+	const $allShelves = $('.vitrines-interesses'),
+		$shelfButton = $('.discount-item');
 
 	this.prepareShelf = () => {
-
+		$(`.vitrines-interesses:not([data-value=${$('.discount-item.active').attr('data-value')}])`).addClass('not-show');
 	};
 
 	this.activateShelf = () => {
-		let $allShelves = $('.vitrines-interesses'),
-			$shelfButton = $('.discount-item');
-
 		$shelfButton.on('click', function() {
 			$shelfButton.removeClass('active');
-			$allShelves.addClass('hide');
+			$allShelves.addClass('not-show');
 			$(this).addClass('active');
-			$(`.vitrines-interesses[data-value=${$(this).attr('data-value')}]`).removeClass('hide');
+			$(`.vitrines-interesses[data-value=${$(this).attr('data-value')}]`).removeClass('not-show');
 		});
 	};
 
 	this.init = () => {
-		this.prepareShelf();
 		this.activateShelf();
+		this.prepareShelf();
 	};
 
 	this.init();
