8e330ae13d31e749d78be7987325d244f885173d
kazuhirobruno <brunokazuhiro@gmail.com>
2019-10-24 19:04:41 -0300

    feat: create active tab ICD-801

--- src/Scripts/modules/subCategoryList.js
+++ src/Scripts/modules/subCategoryList.js
@@ -12,8 +12,21 @@ Nitro.module('subCategoryList', function() {
 		});
 	};
 
+	this.setClickFunction = () => {
+		const tabList = $('.categories-list li a'),
+			subCategoryList = $('.subcategories-list ul');
+
+		tabList.on('click', function() {
+			tabList.removeClass('active');
+			$(this).addClass('active');
+			subCategoryList.removeClass('showSubCategory')
+			$(`.subcategories-list ul[data-category=${$(this).attr('data-category')}]`).addClass('showSubCategory');
+		});
+	};
+
 	this.init = () => {
 		this.slickSubCategories();
+		this.setClickFunction();
 	};
 
 	this.init();
