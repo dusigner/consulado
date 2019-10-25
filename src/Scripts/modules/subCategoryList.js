972796b8cc41fbc1fdf84de464b20e6ec152e03b
kazuhirobruno <brunokazuhiro@gmail.com>
2019-10-25 11:26:51 -0300

    feat: subcategory list ICD-801

--- src/Scripts/modules/subCategoryList.js
+++ src/Scripts/modules/subCategoryList.js
@@ -8,19 +8,44 @@ Nitro.module('subCategoryList', function() {
 			dots: false,
 			slidesToShow: 6,
 			slidesToScroll: 2,
-			infinite: true
+			responsive: [
+				{
+					breakpoint: 1000,
+					settings: {
+						slidesToShow: 4,
+						slidesToScroll: 2,
+						swipeToSlide: true
+					}
+				},
+				{
+					breakpoint: 500,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 2,
+						swipeToSlide: true
+					}
+				},
+				{
+					breakpoint: 375,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						swipeToSlide: true
+					}
+				}
+			]
 		});
 	};
 
 	this.setClickFunction = () => {
 		const tabList = $('.categories-list li a'),
-			subCategoryList = $('.subcategories-list ul');
+			subCategoryList = $('.subcategories-list div');
 
 		tabList.on('click', function() {
 			tabList.removeClass('active');
 			$(this).addClass('active');
 			subCategoryList.removeClass('showSubCategory')
-			$(`.subcategories-list ul[data-category=${$(this).attr('data-category')}]`).addClass('showSubCategory');
+			$(`.subcategories-list div[data-category=${$(this).attr('data-category')}]`).addClass('showSubCategory');
 		});
 	};
 
