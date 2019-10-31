7b821a2e971863ac4c587275fe74322099d58444
RafaelGrande <rafaelgrande_@hotmail.com>
2019-10-31 17:20:42 -0300

    WPD-654 | [Beta] CNS | Landing Page Black Friday  - 3° Módulo (#244)
    * WPD-712 | troca de titulo por H1
    
    * WPD-654 | Widget de prateleira LP BF
    
    * Inserindo tagueamento no step 2 e 3 do lead
    
    * Alterando texto e icones do cronometo da LP de Black Friday.
    
    * WPD-543 | ajuste slider vitrine
    
    * WPD-654 | Ajuste de bug na prateleira de consul
    
    * WPD-654 | Prateleira mobile

--- src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
+++ src/Pages/landing-blackfriday-2019/scripts/consul-landing-blackfriday-2019.js
@@ -2,14 +2,15 @@
 
 require('vendors/slick');
 
-$( document ).ready(function() {
+$(document).ready(function () {
 	// console.log('###################### PROXY ######################');
 	initTags();
 	initLeadsBlackfriday();
 	initDepoimentsSlider();
 	initCountdownSlider();
-	createAccordeon($('.category__title'))
-	createAccordeon($('.faq__header'))
+	createAccordeon($('.category__title'));
+	createAccordeon($('.faq__header'));
+	initSlider();
 
 	jQuery.extend(jQuery.validator.messages, {
 		required: 'Este campo &eacute; requerido.',
@@ -33,7 +34,7 @@ $( document ).ready(function() {
 });
 
 const shootDataLayer = (event, category, action, label) => {
-	if( typeof(dataLayer) !== "undefined" ) {
+	if (typeof (dataLayer) !== "undefined") {
 		dataLayer.push({
 			event,
 			category,
@@ -48,6 +49,41 @@ const initTags = () => {
 		const title = $(e.target).parents('.card').find('.card__title').text().trim();
 		shootDataLayer('generic', 'lp_black_friday_2019', 'nossas_dicas', `veja_mais_${title}`);
 	})
+}
+
+const initSlider = () => {
+	const $prateleiraHolder = $('.prateleira-holder__list .prateleira > ul');
+	$prateleiraHolder.children('li.helperComplement').remove();
+
+	const $items = $prateleiraHolder.children('li');
+
+	if( $items.length > 4 ) {
+		$prateleiraHolder.slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			infinite: false,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 2
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1
+					}
+				}
+			]
+		});
+	}
 }
 
 const initLeadsBlackfriday = () => {
@@ -74,21 +110,21 @@ const initLeadsBlackfriday = () => {
 		}
 	});
 
-	$('.btn1').on('click', function(){
+	$('.btn1').on('click', function () {
 		// console.log('#### CLICOU NO BTN 1 ###');
 		var steep1 = false;
 		if ($('#firstName').val() !== '' && $('#email').val() !== '') {
 			steep1 = true;
 		}
-		if($('#email-error').text() !==''){
+		if ($('#email-error').text() !== '') {
 			steep1 = false;
 		}
-		if ($('#term').attr('checked') === undefined){
+		if ($('#term').attr('checked') === undefined) {
 			steep1 = false;
 
 			//exibir msg de erro termo
 		}
-		if(steep1 === true) {
+		if (steep1 === true) {
 			shootDataLayer('generic', 'lp_black_friday_2019', 'cadastro', 'quero_agora');
 
 			$('.steep1').fadeOut();
@@ -105,51 +141,60 @@ const initLeadsBlackfriday = () => {
 		}
 	});
 
-	$('.btn2').on('click', function(){
+	$('.btn2').on('click', function () {
 		// console.log('#### CLICOU NO BTN 2 ###');
+		shootDataLayer('generic', 'lp_black_friday_2019', 'quais_produtos_ira_aproveitar', 'continuar');
 		$('.steep2').fadeOut();
 		$('.steep3').delay(500).fadeIn();
 
 		var categorias = [];
 
-		($('#geladeiras').is(':checked'))? categorias.push('geladeiras'):'';
-		($('#fogoes').is(':checked')) ? categorias.push('fogoes'):'';
-		($('#arcondicionado').is(':checked')) ? categorias.push('arcondicionado'):'';
-		($('#freezers').is(':checked')) ? categorias.push('freezers'):'';
-		($('#lavadoras').is(':checked')) ? categorias.push('lavadoras'):'';
-		($('#cervejeiras').is(':checked')) ? categorias.push('cervejeiras'):'';
-		($('#coifas').is(':checked')) ? categorias.push('coifas'):'';
+		($('#geladeiras').is(':checked')) ? categorias.push('geladeiras') : '';
+		($('#fogoes').is(':checked')) ? categorias.push('fogoes') : '';
+		($('#arcondicionado').is(':checked')) ? categorias.push('arcondicionado') : '';
+		($('#freezers').is(':checked')) ? categorias.push('freezers') : '';
+		($('#lavadoras').is(':checked')) ? categorias.push('lavadoras') : '';
+		($('#cervejeiras').is(':checked')) ? categorias.push('cervejeiras') : '';
+		($('#coifas').is(':checked')) ? categorias.push('coifas') : '';
 
 		categoriasLista = categorias.join(', ');
 		// console.log(categoriasLista);
 		// console.log('################ steep2 OK ################');
 	});
 
-	$('.btn3').on('click', function(e){
+	$('.steep2 .btn-finished').on('click', function (e) {
+		shootDataLayer('generic', 'lp_black_friday_2019', 'quais_produtos_ira_aproveitar', 'finalizar');
+		e.preventDefault();
+		generateLead();
+	});
+
+	$('.btn3').on('click', function (e) {
+		shootDataLayer('generic', 'lp_black_friday_2019', 'qual_beneficio_mais_te_atrai', 'continuar');
 		e.preventDefault();
 
 		generateLead();
 	});
 
-	$('.btn-finished').on('click', function(e){
+	$('.steep3 .btn-finished').on('click', function (e) {
+		shootDataLayer('generic', 'lp_black_friday_2019', 'qual_beneficio_mais_te_atrai', 'finalizar');
 		e.preventDefault();
 		generateLead();
 	});
 
-	$('.steep4 .lead__btn').on('click', function(){
+	$('.steep4 .lead__btn').on('click', function () {
 		window.location.replace('http://loja.consul.com.br/landing/blackfriday');
 	});
 
-	function generateLead(){
+	function generateLead() {
 		var benefits = $('input[name=benefits]:checked').attr('id');
-		benefits = $('label[for='+benefits+']').text();
+		benefits = $('label[for=' + benefits + ']').text();
 
 		// console.log(benefits);
 
 		var dataToPatch = {
 			firstName: firstName,
 			email: email,
-			cadastroBlackFriday: (term===1)?true:false,
+			cadastroBlackFriday: (term === 1) ? true : false,
 			receberOfertasBlackFriday: categoriasLista,
 			beneficiosBlackFriday: benefits,
 			isNewsletterOptIn: true,
@@ -167,7 +212,7 @@ const initLeadsBlackfriday = () => {
 				'Accept': 'application/vnd.vtex.ds.v10+json'
 			}
 
-		}).done(function() {
+		}).done(function () {
 			$('.steep2').fadeOut();
 			$('.steep3').fadeOut();
 			$('.steep4').delay(500).fadeIn();
@@ -232,44 +277,44 @@ var contador;
 
 var Index = {
 
-	init: function(){
+	init: function () {
 		contador = setInterval(this.countdown, 1000);
 	},
 
-	calculateTimeRemaining: function( endDate ) {
-		var total   = Date.parse( endDate ) - Date.parse( new Date(), 'mm-dd-yyyy' ),
-			seconds = Math.floor( ( total / 1000) % 60 ),
-			minutes = Math.floor( ( ( total / 1000 ) / 60 ) % 60 ),
-			hours   = Math.floor( ( total / ( ( 1000 * 60 ) * 60 ) ) % 24 ),
-			days    = Math.floor( total / ( ( ( 1000 * 60 ) * 60 ) * 24 ) ),
+	calculateTimeRemaining: function (endDate) {
+		var total = Date.parse(endDate) - Date.parse(new Date(), 'mm-dd-yyyy'),
+			seconds = Math.floor((total / 1000) % 60),
+			minutes = Math.floor(((total / 1000) / 60) % 60),
+			hours = Math.floor((total / ((1000 * 60) * 60)) % 24),
+			days = Math.floor(total / (((1000 * 60) * 60) * 24)),
 			timeRemaining = {
-				'total'   : total,
-				'days'    : days,
-				'hours'   : hours,
-				'minutes' : minutes,
-				'seconds' : seconds
+				'total': total,
+				'days': days,
+				'hours': hours,
+				'minutes': minutes,
+				'seconds': seconds
 			};
 
 		return timeRemaining;
 	},
 
-	countdown: function() {
-		var endDate       = '2019/11/29',
-			$countdown    = $('.countdown'),
-			$days         = $countdown.find('.days.indicators__number'),
-			$hours        = $countdown.find('.hours.indicators__number'),
-			$minutes      = $countdown.find('.minutes.indicators__number'),
+	countdown: function () {
+		var endDate = '2019/11/29',
+			$countdown = $('.countdown'),
+			$days = $countdown.find('.days.indicators__number'),
+			$hours = $countdown.find('.hours.indicators__number'),
+			$minutes = $countdown.find('.minutes.indicators__number'),
 			timeRemaining = Index.calculateTimeRemaining(endDate),
-			days          = timeRemaining.days,
-			hours         = timeRemaining.hours,
-			minutes       = timeRemaining.minutes,
-			seconds       = timeRemaining.seconds;
+			days = timeRemaining.days,
+			hours = timeRemaining.hours,
+			minutes = timeRemaining.minutes,
+			seconds = timeRemaining.seconds;
 
 		$days.text(days < 10 ? '0' + days : days);
 		$hours.text(hours < 10 ? '0' + hours : hours);
 		$minutes.text(minutes < 10 ? '0' + minutes : minutes);
 
-		if ( days + hours + minutes + seconds <= 0 ) {
+		if (days + hours + minutes + seconds <= 0) {
 			clearInterval(contador);
 
 			$('.lpbf-contador').addClass('is--end');
