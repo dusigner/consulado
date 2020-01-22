77b2d95be56a40b08b24395b0b90be5b1b9fbf23
Kevin Bueno <kevin.bueno@jussi.local>
2020-01-22 16:52:50 -0300

    Ajustes

--- src/Scripts/components/WishList/wishlist-main.js
+++ src/Scripts/components/WishList/wishlist-main.js
@@ -13,14 +13,10 @@ class wishList {
             const userInfos = await (await fetch(`/no-cache/profileSystem/getProfile`)).json();
 
             if (userInfos.IsUserDefined) {
-                const variantResponse = await (await fetch(`/api/catalog_system/pvt/products/ProductGet/${this.productId}`)).json(),
-                    dataBaseResponse = await (await fetch(`/api/dataentities/WL/search?email=${userInfos.Email}&_fields=id,productId`)).json(),
+                const dataBaseResponse = await (await fetch(`/api/dataentities/WL/search?email=${userInfos.Email}&_fields=id,productId`)).json(),
+                    variantResponse = await (await fetch(`/api/catalog_system/pvt/products/ProductGet/${this.productId}`)).json(),
                     referenceId = variantResponse.RefId,
                     productId = dataBaseResponse.map(item => item.productId);
-
-                console.log(dataBaseResponse);
-                console.log(referenceId);
-                console.log(productId);
             }
         } catch(error) {
             console.log(error);
