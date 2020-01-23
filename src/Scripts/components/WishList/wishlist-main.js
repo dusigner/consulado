1da05cdbf607d076071f13efa316f8a07deeecb5
Kevin Bueno <kevin.bueno@jussi.local>
2020-01-23 11:36:58 -0300

    Wishlist

--- src/Scripts/components/WishList/wishlist-main.js
+++ src/Scripts/components/WishList/wishlist-main.js
@@ -15,18 +15,24 @@ class wishList {
             const dataBaseResponse = await (await fetch(`/api/dataentities/WL/search?email=${this.userEmail}&_fields=id,productId`)).json(),
                 variantResponse = await (await fetch(`/api/catalog_system/pvt/products/ProductGet/${this.productId}`)).json(),
                 listId = dataBaseResponse.map(item => item.id),
-                productCode = dataBaseResponse.map(item => item.productId),
-                referenceCode = variantResponse.RefId;
+                productCode = dataBaseResponse.map(item => item.productReference),
+                referenceCode = variantResponse.RefId,
                 wishData = {
                     id: listId,
                     email: this.userEmail,
                     productId: newArray.push(productCode, referenceCode)
-                };
+                },
+                request = new Request(url, {
+                    method: 'PATCH',
+                    credentials: 'include',
+                    headers: {
+                        'Accept': 'application/vnd.vtex.ds.v10+json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(wishData)
+                });
 
-            // newArray.push(productCode, referenceId);
-
-
-
+                fetch(request);
         } catch(error) {
             console.log(error);
         }
