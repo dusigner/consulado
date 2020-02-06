0e68e004ca83b02866b1fd450adc1786593a1f73
Kevin Bueno <kevin.bueno@jussi.local>
2020-02-06 11:04:32 -0300

    Ajustes Pratilheira

--- src/Scripts/modules/listagem/wish-pratileira.js
+++ src/Scripts/modules/listagem/wish-pratileira.js
@@ -5,26 +5,32 @@ import wishList from './../../components/WishList/wishlist-main.js';
 import { dataBaseFetch, changingEvent } from './../../components/WishList/wishlist-utils.js';
 import cacheSelector from './cache-selector.js';
 
-const El = cacheSelector.auxiliars, { Document, userApi, wishAddButton, wishContainer, wishButton } = El;
+const El = cacheSelector.auxiliars, { $document, userApi, wishContainer, wishButton } = El;
 
 Nitro.module('wish-pratileira', function() {
 	this.init = () => {
 		const windowHash = window.location.hash;
 
-		fetch(userApi).then(res => res.json().then((res) => {
-			setTimeout(() => {
-				this.wishInit(res, windowHash);
-			}, 1000);
-		}));
+		$(window).on('load', () => {
+			fetch(userApi).then(res => res.json().then((res) => {
+				window.location.pathname.indexOf('login') === -1 &&
+					this.wishInit(res, windowHash);
+			}));
+		});
 	};
 
 	this.wishInit = (res, windowHash = null) => {
-		if (windowHash) {
-			const productID = windowHash.split('#productID')[1],
-				$element = $(`.wishlist__button[data-idproduto=${productID}]`),
-				wishListStart = new wishList(productID, res.Email, $element);
+		if (windowHash && windowHash.indexOf('productID' ) !== -1) {
+			const productID = windowHash.split('#productID')[1];
 
-			wishListStart.favoritesEvents();
+			$(`.wishlist__button[data-idproduto=${productID}]`).each((i, el) => {
+				const $element = $(el),
+					wishListStart = new wishList(productID, res.Email, $element);
+
+				wishListStart.favoritesEvents();
+			});
+
+			window.location.hash = '';
 			this.setFavoriteds(res);
 		} else {
 			this.setFavoriteds(res);
@@ -65,8 +71,8 @@ Nitro.module('wish-pratileira', function() {
 	}
 
 	this.handleFavorites = (res) => {
-		Document.on('click', wishAddButton, ({target}) => {
-			const $element = $(target),
+		$document.on('click', wishButton, ({currentTarget}) => {
+			const $element = $(currentTarget),
 				productID = $element.parents(wishContainer).find(wishButton).attr('data-idproduto');
 
 			if (res.IsUserDefined) {
