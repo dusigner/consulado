58b12998c5fe14cd80ad19c7975cc5975455e19f
Kevin Bueno <kevin.bueno@jussi.local>
2020-02-11 18:06:58 -0300

    WishList Adjust

--- src/Pages/shared/scripts/shared-wishlist.js
+++ src/Pages/shared/scripts/shared-wishlist.js
@@ -1,22 +1,46 @@
 const UtilShared = {
-    fetchSharedList(wishListID) {
-        return fetch(`/api/dataentities/WL/search?id=${wishListID}&_fields=productReference`);
+    async fetchSharedList(wishListID) {
+        try {
+            const res = await fetch(`/api/dataentities/WL/search?id=${wishListID}&_fields=productReference`);
+            return await res.json();
+        } catch (err) {
+            UtilShared.errorReturn();
+            console.log('SharedPage Error: ', err)
+        }
     },
-    fetchProductVariations(ids) {
-        return fetch(`/api/catalog_system/pvt/products/ProductGet/${ids}`);
+
+    async fetchProductVariations(ids) {
+        try {
+            const res = await fetch(`/api/catalog_system/pvt/products/ProductGet/${ids}`);
+            return await res.json();
+        } catch (err) {
+            UtilShared.errorReturn();
+            console.log('SharedPage Error: ', err)
+        }
     },
-    fetchProductSearch(refID) {
-        return fetch(`/api/catalog_system/pub/products/search/${refID}`);
+
+    async fetchProductSearch(refID) {
+        try {
+            const res = await fetch(`/api/catalog_system/pub/products/search/${refID}`);
+            return await res.json();
+        } catch (err) {
+            UtilShared.errorReturn();
+            console.log('SharedPage Error: ', err)
+        }
     },
+
     formatPrice(p) {
         return p && p !== 0 ? p.toLocaleString('pt-br',{ style: 'currency', currency: 'BRL'}) : undefined;
     },
+
     encrypt(s) {
         return s.replace(/\-/gm, 'U2FsdGVkX186oi');
     },
+
     decrypt(s) {
         return s.replace(/\U2FsdGVkX186oi/gm, '-');
     },
+
     copyToClipBoard(t) {
         const textArea = document.createElement("textarea");
 
@@ -27,6 +51,14 @@ const UtilShared = {
         document.execCommand("copy");
         document.body.removeChild(textArea);
     },
+
+    errorReturn() {
+        console.log('executei');
+        $('.shared__ws-container')
+            .append('<h1 style="text-align:center;">Não há produtos favoritados nessa lista :(</h1>')
+            .removeClass('hide');
+        $('.shared__ws-loader-container').hide();
+    }
 }
 
 module.exports = UtilShared;
