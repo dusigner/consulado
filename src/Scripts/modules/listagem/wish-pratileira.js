c1e1548e1de5e4928e68aa94ae7b0ec5cfb300a8
Kevin Bueno <kevin.bueno@jussi.local>
2020-02-12 16:31:57 -0300

    resolving problems

--- src/Scripts/modules/listagem/wish-pratileira.js
+++ src/Scripts/modules/listagem/wish-pratileira.js
@@ -61,6 +61,7 @@ Nitro.module('wish-pratileira', function() {
 
 			wishListStart.favoritesEvents();
 		});
+		$('.box-produto .product-infos-wrap a').on('click', ev => ev.stopPropagation());
 	};
 
 	this.init();
