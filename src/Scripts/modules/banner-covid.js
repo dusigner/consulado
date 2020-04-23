fd93ecf039ca2ce91cb9c96940522364400ac524
Felipe Rocha <felipe_rocha_corebiz@whirlpool.com>
2020-04-22 23:52:09 -0300

    Ajuste disparo tag e init slick

--- src/Scripts/modules/banner-covid.js
+++ src/Scripts/modules/banner-covid.js
@@ -1,5 +1,9 @@
 'use strict';
 
+require('../vendors/slick');
+
+var alreadySendEvent = false
+
 window.addBannerCovid = function() {
     const bannerDesk = `
         <div class="hide-small hide-extra-small">
@@ -35,23 +39,106 @@ window.addBannerCovid = function() {
         })
     })
 
-    var vitrine = document.querySelector('.container.vitrines.covid ul')
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
 
-    if (vitrine) {
-        dataLayer.push({
-            category: 'lp_garantia_de_entrega_covid',
-            action: 'viability_sugestão_de_compra',
-            label: 'o_que_voce_precisa_na_sua_casa',
-            event: 'generic'
-        })
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
 
-        vitrine.addEventListener('click', function() {
+        return ((elemBottom - 100 <= docViewBottom) && (elemTop - 100 >= docViewTop));
+    }
+
+    document.addEventListener('scroll', function() {
+        if (isScrolledIntoView(document.querySelector('.container.vitrines.covid ul li')) && !alreadySendEvent) {
             dataLayer.push({
                 category: 'lp_garantia_de_entrega_covid',
-                action: 'clique_produto',
-                label: 'comprar',
+                action: 'viability_sugestão_de_compra',
+                label: 'o_que_voce_precisa_na_sua_casa',
                 event: 'generic'
             })
+            alreadySendEvent = true
+        }
+    })
+
+    var ul = document.querySelector('.container.vitrines.covid ul')
+
+    ul.addEventListener('click', function() {
+        dataLayer.push({
+            category: 'lp_garantia_de_entrega_covid',
+            action: 'clique_produto',
+            label: 'comprar',
+            event: 'generic'
         })
-    }
+    })
+
+    $('.container.vitrines.covid').find('.prateleira>ul').not('.slick-initialized').slick({
+        mobileFirst: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerPadding: '10px',
+        arrows: true,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2.1,
+                    slidesToScroll: 2.1,
+                    centerPadding: '30px',
+                    initialSlide: 1,
+                    arrows: true,
+                    centerMode: true,
+                    index: 0
+                }
+            },
+            {
+                breakpoint: 998,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerPadding: '30px',
+                    arrows: true,
+                    initialSlide: 0,
+                    index: 0
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 1.3,
+                    slidesToScroll: 1,
+                    centerPadding: '20px',
+                    initialSlide: 0,
+                    arrows: true,
+                    centerMode: true,
+                    index: 0
+                }
+            },
+            {
+                breakpoint: 414,
+                settings: {
+                    slidesToShow: 1.1,
+                    slidesToScroll: 1,
+                    centerPadding: '24px',
+                    initialSlide: 0,
+                    centerMode: true,
+                    arrows: true,
+                    index: 0
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1.1,
+                    slidesToScroll: 1,
+                    centerPadding: '20px',
+                    initialSlide: 0,
+                    centerMode: true,
+                    arrows: true,
+                    index: 0
+                }
+            }
+        ]
+    })
 }
