4e3ce56b6284e70d5a19c62f9a015b46887c75a0
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-05-22 16:27:14 -0300

    Making tag

--- src/Scripts/dataLayers/dataLayer-new-header-menu.js
+++ src/Scripts/dataLayers/dataLayer-new-header-menu.js
@@ -9,18 +9,44 @@ Nitro.module('dataLayer-new-header-menu', function () {
 		checkInlineDatalayers();
 
 		this.newHeaderMenu();
+		this.newMenuDepartament();
 	};
 
 	this.newHeaderMenu = () => {
 
-		$('.menu .item').first().on('click', function () {
+		// $('.menu .item').first().on('click', function () {
+		// 	pushDataLayer(
+		// 		'Novo-Header',
+		// 		`click_menu-departamentos`,
+		// 		`novo-menu`
+		// 	);
+		// });
+
+		//click header
+		let acao = $('.menu').find('.menu-header-category a').attr('title');
+
+		$('.menu').find('.menu-header-category a').on('click', function () {
+			acao = $(this).attr('title');
 			pushDataLayer(
 				'Novo-Header',
-				`click_menu-departamentos`,
+				`click_menu-${acao}`,
 				`novo-menu`
 			);
 		});
 
+	};
+	this.newMenuDepartament = () => {
+		//click inside menu department
+		let action = $('.menu .item-cozinhar').find('.menu-subcategory a').attr('title');
+
+		$('.menu .item-cozinhar').find('.menu-subcategory a').on('click', function () {
+			action = $(this).attr('title');
+			pushDataLayer(
+				'Novo-Header',
+				`click_hover-${action}`,
+				`hover-cozinhar`
+			);
+		});
 	};
 
 	this.init();
