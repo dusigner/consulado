cd3cf6fe70915193be59a18a05d00d84a580e01b
Caue henrique <caue.rosa@jussi.com.br>
2020-05-29 13:25:50 -0300

    init

--- src/Scripts/modules/checkout/checkout-login.js
+++ src/Scripts/modules/checkout/checkout-login.js
@@ -2,6 +2,8 @@
 
 Nitro.module('checkout-login', function(){
 	var self = this,
+		returnUrl = false,
+		$modalBody = $('.checkout-modal-custom-login--body'),
 		routes = {
 			authenticateByEmailKey: '/pub/authentication/accesskey/validate',
 			authenticateLogin: '/pub/authentication/classic/validate',
@@ -20,12 +22,103 @@ Nitro.module('checkout-login', function(){
 			faceLogin : false,
 			googleLogin : false
 		},
-		templates = {}
+		templates = {
+			templatesLogin:
+				'<div class="checkout-modal-custom-login--forms">' +
+					'<div class="checkout-checkout-modal-custom-login--body"></div>' +
+					'<div class="checkout-modal-custom-login--forms-options">' +
+						'<div class="checkout-modal-custom-login--forms-options-items">' +
+							'<div class="checkout-modal-custom-login--forms-options-items-top">' +
+								'<h2 id="checkout-login-modal-user"></h2>' +
+								'<h3 class="checkout-modal-custom-login--forms-options-items-top--text">Use uma das opções para confirmar sua identidade</h3>' +
+								'<button type="button" class="checkout-modal-custom-login--forms-options-items-top-close">×</button>' +
+							'</div>' +
+							'<div class="checkout-modal-custom-login--forms-options-items-bottom">' +
+								'<button id="checkout-login-modal-key" type="button" class="checkout-modal-custom-login--forms-options-items-bottom-buttons">Receber chave de acesso rápido por email</button>' +
+								'<button id="checkout-login-modal-email" type="button" class="checkout-modal-custom-login--forms-options-items-bottom-buttons">Receber chave de acesso rápido por email</button>' +
+							'</div>' +
+						'</div>' +
+					'</div>' +
+					'<div id="checkout-mail_pass_login">' +
+						'<p class="checkout-modal-custom-login--sectitles">Entre com um email e senha:</p>' +
+						'<form id="checkout-modal-custom-login--form">' +
+							'<div class="checkout-modal-custom-login-mail_pass_login">' +
+								'<div class="checkout-modal-custom-email--inputbox">' +
+									'<input type="text" id="login" placeholder="seu@email.com.br" class="custom-label" name="login" autocomplete="off">' +
+								'</div>' +
+								'<div class="checkout-modal-custom-password--inputbox">' +
+									'<input type="password" id="password" placeholder="Senha" class="custom-label" name="password">' +
+								'</div>' +
+								'<div class="checkout-modal-custom-login--formOptions">' +
+									'<span class="forget_pass">Esqueci minha senha</span>' +
+									'<span class="no_pass">Não tenho uma senha</span>' +
+								'</div>' +
+								'<div class="checkout-custom-login-buttons--action--box">' +
+								'<input type="submit" class="checkout-custom-login-btn checkout-btn-custom-secondary" value="Entrar">' +
+								'</div>' +
+							'</div>' +
+						'</form>' +
+					'</div>' +
+				'</div>' +
+				'<div class="checkout-modal-custom-login--forms__overlay"></div>',
+		},
+		helpers = {
+			formtoobj : function(form) {
+				var unindexed_array = form.serializeArray();
+				var indexed_array = {};
+
+				$.map(unindexed_array, function(n){
+					indexed_array[n['name']] = n['value'];
+				});
+
+				return indexed_array;
+			},
+			authenticateUser : function(cookies, expire) {
+				for (var i = 0, len = cookies.length; i < len; i++) {
+					var cookie = cookies[i];
+
+					if (cookie) {
+						helpers.setCookie(cookie.Name, cookie.Value, '.' + location.hostname.split('.').reverse()[1] + '.' + location.hostname.split('.').reverse()[0], expire);
+					}
+				}
+			},
+			setCookie: function(name, value, domain, expire) {
+				if (!name || !value) {return false;}
+
+				document.cookie = name + '=' + value + ';expire=' + expire + ';path=/;';
+			},
+			urlToObject: function(url){
+				var obj = JSON.parse('{"' + decodeURI(url).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+				return obj;
+			},
+			printError: function(errorText) {
+				if ($('.modal-custom-login--errorBox').length > 0) {
+					$('.modal-custom-login--errorBox').remove();
+					$modalBody.prepend('<h4 class="modal-custom-login--errorBox">' + errorText + '</h4>');
+				} else {
+					$modalBody.prepend('<h4 class="modal-custom-login--errorBox">' + errorText + '</h4>');
+				}
+			},
+			removeError: function() {
+				$('.modal-custom-login--errorBox').remove();
+			},
+			isEmail: function(email) {
+				if (!email) {return;}
+				var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+				if (filtro.test(email)) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+		}
 
 	this.init = () => {
 		self.accessModalLogin();
 		self.htmlErrorValidadeEmail();
 		self.validateEmail();
+		self.verifyLogged();
 
 		// init functions
 		setEnviroment();
@@ -61,6 +154,7 @@ Nitro.module('checkout-login', function(){
 					self.htmlLoginFacebookAndGoogle();
 				});
 		}
+		self.setListeners();
 	};
 
 	this.request = function(route, params) {
@@ -172,6 +266,50 @@ Nitro.module('checkout-login', function(){
 
 		$('body').on('click', '.modal-custom-login .modal-custom-login-btn.facebook_access', () => {
 			self.socialModalLogin('Facebook');
+		})
+	}
+
+	this.verifyLogged = () => {
+		vtexjs.checkout.getOrderForm().done(function (_user_infos) {
+			if (_user_infos.loggedIn !== true) {
+				$('body').append(templates.templatesLogin);
+				$('body').addClass('is--checkout-disconnected');
+			} else {
+				$('body').removeClass('is--checkout-disconnected');
+			}
+		});
+	};
+
+	function reload() {
+		console.log('deu certo')
+		// if (returnUrl) {
+		// 	window.location = window.location.origin + returnUrl;
+		// } else {
+		// 	location.reload();
+		// }
+	}
+
+	this.setListeners = () => {
+		$('body').on('submit', '#checkout-modal-custom-login--form', function(ev) {
+			ev.preventDefault();
+			var params = $.extend(helpers.formtoobj($(this)), {
+				recaptcha: '',
+				authenticationToken: userInfos.authenticationToken
+			});
+			// console.log(params);
+			self.request(routes.authenticateLogin, params)
+				.then(function(data) {
+					if (data.authStatus === 'Success') {
+						helpers.authenticateUser([data.authCookie, data.accountAuthCookie], data.expiresIn);
+						reload();
+					} else if (data.authStatus === 'WrongCredentials') {
+						helpers.printError('E-mail ou senha inválidos.');
+					} else {
+						helpers.printError(data.authStatus);
+					}
+				}, function(err) {
+					helpers.printError(err);
+				});
 		})
 	}
 
