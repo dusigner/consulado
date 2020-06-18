03d7f37608b8eaf771fe3e2952891118a391f8c9
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-06-18 17:50:55 -0300

    add placeholer

--- src/Scripts/components/filterFamilySize.js
+++ src/Scripts/components/filterFamilySize.js
@@ -1,14 +1,14 @@
 Nitro.module('filterFamilySize', function() {
 
 	this.filterSize = () => {
-		$('#parent > div').hide();
-		$('#parent > div:first-of-type').show();
+		$('#vitrines-family > div').hide();
+		$('#vitrines-family > div:first-of-type').show();
 
 
 		let $buttons = $('.btn').click(function() {
-			$('#parent > div').fadeIn(450);
+			$('#vitrines-family > div').fadeIn(450);
 			let $card = $('.' + this.id).fadeIn(450);
-			$('#parent > div').not($card).hide();
+			$('#vitrines-family > div').not($card).hide();
 
 			$buttons.removeClass('active');
 			$(this).addClass('active');
