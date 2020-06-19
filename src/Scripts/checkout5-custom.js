6ee0f1211e2afff8e989cd3cf614e7022d8d2b21
cauejussi <caue.rosa@jussi.com.br>
2020-06-19 12:33:37 -0300

    changes

--- src/Scripts/checkout5-custom.js
+++ src/Scripts/checkout5-custom.js
@@ -127,6 +127,7 @@ $(document).on('ready', function() {
 				self.shippingSelector();
 				self.shippingSelectorInformation();
 				self.limitQuantityCart();
+				self.descountCheckout();
 
 				this.orderFormUpdated(null, window.vtexjs && window.vtexjs.checkout.orderForm);
 
@@ -145,6 +146,24 @@ $(document).on('ready', function() {
 					})
 				);
 			};
+
+			this.descountCheckout = () => {
+                $('#remove-gift-card, #btn-add-gift-card').on('click', () => {
+                    if ( $('.payment-discounts-list').length >= 1 ) {
+                        $('body.body-order-form .box-step-content').addClass('is--descount');
+                    }
+                })
+                if ( $('.payment-discounts-list').length >= 1 ) {
+                    $('body.body-order-form .box-step-content').addClass('is--descount')
+                }
+                // descount
+                setInterval(function (){
+                    if ( $('.payment-discoints-table .icon-ok').length ) {
+                        var $priceDescountTotal = $('.box-payment-group2 .sight span').eq(1).text();
+                        $('.checkout-container .mini-cart .cart-totalizers tfoot .monetary[data-bind]').html(`${$priceDescountTotal}`);
+                    }
+                }, 100)
+            };
 
 			this.shippingSelector = function() {
 				const $btnShippingSelector = $('.shipping-sla-selector .btn-mini');
@@ -1357,7 +1376,10 @@ $(document).on('ready', function() {
 
     setCheckoutEvents();
   }
-  init();
+
+  window.onload  = (function() {
+    init();
+  });
 
 })(window, document);
 
