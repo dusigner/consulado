afa737441e029194bfbd92cd8806c59f7ac6fe92
cauejussi <caue.rosa@jussi.com.br>
2020-06-22 18:29:44 -0300

    clipboard

--- src/Scripts/modules/checkout/checkout-billet.js
+++ src/Scripts/modules/checkout/checkout-billet.js
@@ -8,29 +8,41 @@ Nitro.module('checkout-billet', function() {
 	};
 
 	this.htmtBillet = () => {
-		$('body[class*=consul].body-checkout-confirmation #app-top .ph3-ns .cconf-alert .fr').before(`
-			<div class="checkout-billet">
-				<div class="checkout-billet-top">
-					<p class="checkout-billet-top--text">Para realizar o pagamento, copie<br />o código do boleto ou imprima.</p>
-				</div>
-				<div class="checkout-billet-bottom">
-					<button class="checkout-billet-bottom--button">Copiar Código</button>
-				</div>
-			</div>
-		`);
+		setTimeout(function() {
+
+			const $bankInvoice = $('#print-bank-invoice')
+			if ($bankInvoice.length > 0) {
+				var $orderID = $('#order-id').text().split('-')[0];
+				$.getJSON('/api/checkout/pub/orders/order-group/' + $orderID).then((res) => {
+
+					var $codBillet = res[0].paymentData.transactions[0].payments[0].bankIssuedInvoiceIdentificationNumberFormatted;
+
+					$('body[class*=consul].body-checkout-confirmation #app-top .ph3-ns .cconf-alert .fr').before(`
+					<div class="checkout-billet">
+						<div class="checkout-billet-top">
+							<p class="checkout-billet-top--text">Para realizar o pagamento, copie<br />o código do boleto ou imprima.</p>
+						</div>
+						<input id="copyBillet" value="${$codBillet}" />
+						<div class="checkout-billet-bottom">
+							<button class="checkout-billet-bottom--button">Copiar Código</button>
+						</div>
+					</div>
+				`);
+				});
+			}
+		}, 1000);
 	};
 
-	this.clipToBoard = () => {
+	this.copyClipToBoard = () => {
 		$('body').on('click', '.checkout-billet-bottom--button', function() {
-			var $orderID = $('#order-id').text().split('-')[0];
-			  $.getJSON('/api/checkout/pub/orders/order-group/' + $orderID).then((res) => {
-
-				console.log(res)
-
-			 });
-			// $(this).select();
-			document.execCommand('copy');
-		  })
+			$('#copyBillet').select()
+			try {
+				var ok = document.execCommand('copy');
+				if (ok) { console.log('Texto copiado para a área de transferência'); }
+			} catch (e) {
+				console.log(e)
+			}
+		})
 	}
 
 	this.init();
