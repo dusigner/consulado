fd1b927e46850d4b0b5b5f3b01b948e3d484c740
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-06-25 16:14:40 -0300

    filter family size completed

--- src/Scripts/consul2-home.js
+++ src/Scripts/consul2-home.js
@@ -365,35 +365,61 @@ Nitro.controller(
 			});
 			//\ vitrines padrões vtex
 		}
-
-		this.slickVitrineFamilia = () => {
-			var $vitrineFamilia = $('#vitrines-family').find('.prateleira > ul').not('.slick-initialized');
-			$vitrineFamilia.slick({
-				infinite: false,
-				slidesToShow: 4,
-				dots: true,
-				arrows: true,
-			});
-		};
-
 		this.filterSize = () => {
-			$('#vitrines-family > div').hide();
-			$('#vitrines-family > div:first-of-type').show();
-
+			$('#vitrines-family .box.pequena').show();
+			$('#vitrines-family .box.pequena .prateleira > ul').not('.slick-initialized').slick({
+				infinite: false,
+				dots: false,
+				mobileFirst: true,
+				responsive: [
+					{
+						breakpoint: 992,
+						settings: {
+							arrows: true,
+							slidesToShow: 4
+						}
+					}
+				]
+			});
 
 			const $buttons = $('.btn').click(function() {
 				$('#vitrines-family > div').fadeIn(450);
+
 				const $card = $('.' + this.id).fadeIn(450);
 				$('#vitrines-family > div').not($card).hide();
+				$card.find('.prateleira > ul').not('.slick-initialized').slick({
+					infinite: false,
+					arrows: true,
+					dots: false,
+					mobileFirst: true,
+					responsive: [
+						{
+							breakpoint: 992,
+							settings: {
+								arrows: true,
+								slidesToShow: 4
+							}
+						},
+						{
+							breakpoint: 768,
+							settings: {
+								arrows: false,
+								dots: true,
+								infinite: false,
+								slidesToScroll: 1,
+								slidesToShow: 1,
+								swipeToSlide: true
+							}
+						}
+					]
+				});
 
 				$buttons.removeClass('active');
 				$(this).addClass('active');
 			})
 		};
 
-
 		this.init = () => {
-			this.slickVitrineFamilia();
 			this.filterSize();
 		};
 
