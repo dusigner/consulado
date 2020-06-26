148ebac7cb498aea72dfb896a15e85cb4d3325a5
cauejussi <caue.rosa@jussi.com.br>
2020-06-26 10:38:46 -0300

    changes deploy

--- src/Scripts/dataLayers/dataLayer-checkout.js
+++ src/Scripts/dataLayers/dataLayer-checkout.js
@@ -10,16 +10,9 @@ Nitro.module('dataLayer-checkout', function() {
 		this.connectGmail();
 		this.backToCart();
 		this.validateEmail();
-
-		// copy billet
-		this.buttonCopy();
-		this.printTicket();
-		this.newBuy();
-		this.buttonRequests();
 	};
 
 	var $category = '[SQUAD] Login Facebook-Google';
-	var $categoryCopyBillet = '[SQUAD] Pagamento-Boleto';
 
 	this.emailContinue = () => {
 		$('.client-email #btn-client-pre-email').on('click', function() {
@@ -86,67 +79,6 @@ Nitro.module('dataLayer-checkout', function() {
 					`${$label}`
 				);
 			}
-		});
-	};
-
-	// copy billet
-	this.buttonCopy = () => {
-		$('body').on('click', '.checkout-billet-bottom--button', function() {
-			pushDataLayer(
-				`${$categoryCopyBillet}`,
-				`click copiar cod`,
-				`botão cop`
-			);
-
-			$('#copyBillet').select()
-			try {
-				var ok = document.execCommand('copy');
-				if (ok) {
-					pushDataLayer(
-						`${$categoryCopyBillet}`,
-						`exbicao cod copiado`,
-						`${ok}`
-					);
-				}
-			} catch (e) {
-				console.log(e)
-			}
-		});
-	};
-
-	this.printTicket = () => {
-		$('#print-bank-invoice').on('click', function() {
-			const $label = $(this).text();
-
-			pushDataLayer(
-				`${$category}`,
-				`click imprimir boleto`,
-				`${$label}`
-			);
-		});
-	};
-
-	this.newBuy = () => {
-		$('body.body-checkout-confirmation #app-container .ph3-ns .cconf-continue-button').on('click', function() {
-			const $label = $(this).text();
-
-			pushDataLayer(
-				`${$category}`,
-				`click nova compra`,
-				`${$label}`
-			);
-		});
-	};
-
-	this.buttonRequests = () => {
-		$('body.body-checkout-confirmation #app-container .ph3-ns .cconf-myorders-button').on('click', function() {
-			const $label = $(this).text();
-
-			pushDataLayer(
-				`${$category}`,
-				`click pedidos`,
-				`${$label}`
-			);
 		});
 	};
 
