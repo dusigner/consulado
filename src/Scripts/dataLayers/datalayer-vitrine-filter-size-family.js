b1d8be8ac719a216d6e8ec1440b15a369b9d16c5
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-07-01 20:03:31 -0300

    tag

--- src/Scripts/dataLayers/datalayer-vitrine-filter-size-family.js
+++ src/Scripts/dataLayers/datalayer-vitrine-filter-size-family.js
@@ -13,77 +13,27 @@ Nitro.module('datalayer-vitrine-filter-size-family', function () {
 
 	this.vitrineFilterFamily = () => {
 
-		let acao = $('.vitrine-subcategorias').find('.categories-list a.active').attr('data-category');
+		let acao = $('#vitrines-family .pequena .prateleira').find('ul li > .box-produto .detalhes .prod-info').attr('title');
 
-		$('.vitrine-subcategorias').find('.categories-list a').on('click', function () {
-			acao = $(this).attr('data-category');
+		$('#vitrines-family .pequena .prateleira').find('ul li > .box-produto').on('click', function () {
+			acao = $(this).attr('title');
 
 			pushDataLayer(
-				'[SQUAD] Vitirine Categorizacao',
-				`${acao}`,
-				`Menu Categoria`
+				'Vitrines_Tamanho-familia',
+				`click_${acao}`,
+				`card_${label}`
 			);
 		});
 
-		$('.vitrine-subcategorias .subCategorySection a.subcategory-link').on('click', function () {
-			const label = $(this).find('img').attr('alt');
+		// $('.cont-linha').on('click', function () {
+		// 	const label = $(this).find('button');
 
-			pushDataLayer(
-				'[SQUAD] Vitirine Categorizacao',
-				`${acao}`,
-				`${label}`
-			);
-		});
-
-		$('.vitrine-subcategorias').find('.subCategorySection ul button.slick-arrow').on('click', function () {
-			const data = $(this).parents('.subCategorySection').attr('data-category');
-			const seta = $(this).attr('aria-label');
-			pushDataLayer(
-				'[SQUAD] Vitirine Categorizacao',
-				`${data}`,
-				`Seta ${seta === 'Previous' ? 'Esquerda' : 'Direita'}`
-			);
-		});
-
-		$('.vitrine-subcategorias .subCategorySection > a.categoryLink').on('click', function () {
-			const data = $(this).parents('.subCategorySection').attr('data-category');
-			const link = $(this).text();
-
-			pushDataLayer(
-				'[SQUAD] Vitirine Categorizacao',
-				`${data}`,
-				`${link}`
-			);
-		});
-
-	};
-	this.taggingSelo = () => {
-        var $categoryVitrine = 'Vitrines_Tamanho-familia';
-        $('body').on('click', '.container .list-container .main .vitrine .prateleira ul li .box-produto', function() {
-            var $label = '';
-            var $nameProduct = '';
-            $label = $(this).parents('li').find('.promo-destaque__icon').attr('style')
-            $nameProduct = $(this).parents('li').find('.nome').text()
-            if ( $label === `background-image: url('/arquivos/cns__promo__famílias-pequenas.png?v=dln')` ) {
-                pushDataLayer(
-                    `[SQUAD] ${$categoryVitrine}`,
-                    `${$nameProduct}`,
-                    `Familias pequenas`
-                );
-            } else if ( $label === `background-image: url('/arquivos/cns__promo__famílias-médias.png?v=dln')` ) {
-                pushDataLayer(
-                    `[SQUAD] ${$categoryVitrine}`,
-                    `${$nameProduct}`,
-                    `Familias médias`
-                );
-            } else if ( $label === `background-image: url('/arquivos/cns__promo__famílias-grandes.png?v=dln')` ) {
-                pushDataLayer(
-                    `[SQUAD] ${$categoryVitrine}`,
-                    `${$nameProduct}`,
-                    `Familias grandes`
-                );
-            }
-        });
-
+		// 	pushDataLayer(
+		// 		'Vitrines_Tamanho-familia',
+		// 		`click`,
+		// 		`card_${label}`
+		// 	);
+		// });
+	}
 	this.init();
 });
