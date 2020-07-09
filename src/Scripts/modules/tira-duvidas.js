e27c7b3b2b12475e23b775339efa36a58c4db34a
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-07-09 19:19:15 -0300

    html e js do tira dúvidas

--- src/Scripts/modules/tira-duvidas.js
+++ src/Scripts/modules/tira-duvidas.js
@@ -8,3 +8,15 @@ $('#btn-modal').click(function(){
 	$('#cover, #modal').fadeTo(200,0).hide();
 	$('html').css({"overflow": "inherit", "height": "auto"});
   });
+
+  $("ul.content").hide();
+  $("ul.menu-main").delegate("li.toggle", "click", function() {
+      $(this).next().toggle("fast").siblings(".content").hide("fast");
+  });
+
+  $("h2").click(function() {
+	var target = $(this).next('div.resposta')
+	$("div.resposta:visible").not(target).slideUp();
+	target.slideToggle();
+
+});
