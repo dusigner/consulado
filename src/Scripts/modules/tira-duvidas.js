369cc6776e586052fa7d301b4916bf782c3ce182
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-07-10 20:27:40 -0300

    tira duvidas

--- src/Scripts/modules/tira-duvidas.js
+++ src/Scripts/modules/tira-duvidas.js
@@ -9,10 +9,14 @@ $('#btn-modal').click(function(){
 	$('html').css({"overflow": "inherit", "height": "auto"});
   });
 
-  $("ul.content").hide();
+//   $("ul.content").hide();
   $("ul.menu-main").delegate("li.toggle", "click", function() {
       $(this).next().toggle("slideLeft").siblings(".content").hide("slideLeft");
   });
+  $("li.toggle").click(function() {
+	$("li.toggle").removeClass("active");
+	$(this).addClass("active");
+ });
 
   $("h2").click(function() {
 	var target = $(this).next('div.resposta')
