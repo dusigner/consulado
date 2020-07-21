fdce8b35b10c684cfa09e3623297816508f643c0
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-07-21 17:46:29 -0300

    modal pronto

--- src/Scripts/modules/tira-duvidas.js
+++ src/Scripts/modules/tira-duvidas.js
@@ -1,43 +1,52 @@
-const { $body } = require("./orders/order.modal.gae");
+'use strict';
 
+Nitro.module('tiraduvidas', function() {
+		this.init = () => {
+			this.modal();
+			this.acoordeon();
+		};
 
-
-$('#btn-modal').click(function(){
-	$('#cover, #modal').fadeTo(200,1);
-	$('html').css({"overflow": "hidden", "height": "100vh"});
-  });
-
-  $('#close, #cover').click(function(){
-	$('#cover, #modal').fadeTo(200,0).hide();
-	$('html').css({"overflow": "inherit", "height": "auto"});
-  });
-
- $(".pergunta h2").click(function() {
-
-	$("h2").removeClass("active");
-	$(this).addClass("active");
-
-	var target = $(this).next('div.resposta');
-	$("div.resposta:visible").not(target).slideUp();
-	target.slideToggle();
-});
-
-
-$(".toggle").click(function() {
-	$(".toggle").removeClass("active");
-	$(this).addClass("active");
-	var target = $(this).next('ul.content');
-	$("ul.content:visible").not(target).hide("fast");
-	target.show("fast");
-});
-
-if ($('body').width() < 768){
-	$('.btn-voltar').hide();
-	$(".toggle").click(function() {
-		$('.btn-voltar').show();
-		$('.btn-voltar, .title-question').click(function() {
-			$('.content').hide();
+	this.modal = () => {
+		$('#btn-modal').click(function() {
+			$('#cover, #modal').fadeTo(200,1);
+			$('html').css({"overflow": "hidden", "height": "100vh"});
 		});
-	});
-}
 
+		$('#close, #cover').click(function() {
+			$('#cover, #modal').fadeTo(200,0).hide();
+			$('html').css({"overflow": "inherit", "height": "auto"});
+		});
+	}
+
+	this.acoordeon = () => {
+		$(".pergunta h2").click(function() {
+
+			$(this).toggleClass("active");
+			$(".pergunta h2").removeClass("active");
+
+			var target = $(this).next('div.resposta');
+			$("div.resposta:visible").not(target).slideUp();
+			target.slideToggle();
+		});
+
+
+		$(".toggle").click(function() {
+			$(".toggle").removeClass("active");
+			$(this).addClass("active");
+			var target = $(this).next('ul.content');
+			$("ul.content:visible").not(target).hide("fast");
+			target.show("fast");
+		});
+
+		if ($('body').width() < 768){
+			$('.btn-voltar').hide();
+			$(".toggle").click(function() {
+				$('.btn-voltar').show();
+				$('.btn-voltar, .title-question').click(function() {
+					$('.content').hide();
+				});
+			});
+		}
+	}
+	this.init();
+});
