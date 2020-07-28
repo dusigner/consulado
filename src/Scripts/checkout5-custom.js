4548422f565f775d268f8927296d876165ce8191
Rafael Lourenço Marques <rafael.marques@jussi.com.br>
2020-07-28 10:43:33 -0300

    insert function tira-duvidas in checkout scripts.

--- src/Scripts/checkout5-custom.js
+++ src/Scripts/checkout5-custom.js
@@ -130,6 +130,7 @@ $(document).on('ready', function() {
 				self.descountCheckout();
 				self.searchCheckout();
 				self.textDescriptionPayment();
+				self.tiraDuvidas();
 
 				this.orderFormUpdated(null, window.vtexjs && window.vtexjs.checkout.orderForm);
 
@@ -1051,6 +1052,52 @@ $(document).on('ready', function() {
 			this.cookieFormat = (cookie, str) => {
 				//Funcao auxiliar para trazer somente o valor desejado dos cookies de userinfo e shippingdata.
 				return decodeURIComponent(cookie.split(str)[1].split('/')[0]);
+			}
+
+			this.tiraDuvidas = () => {
+				$('#btn-modal').length === 0 ? $('<button id="btn-modal"><div class="icone-questions"><img src="/arquivos/cns-selo-tira-duvidas.png"/></div>Está com alguma dúvida em relação ao pagamento?</button>').appendTo('.cart-links') : console.log('Button already exist!');
+
+				$('#btn-modal').click(function () {
+					$('#cover, #modal').fadeTo(200, 1);
+					$('html').css({ 'overflow': 'hidden', 'height': '100vh' });
+				});
+
+				$('#close, #cover').click(function () {
+					$('#cover, #modal').fadeTo(200, 0).hide();
+					$('html').css({ 'overflow': 'inherit', 'height': 'auto' });
+				});
+
+				$('.pergunta h2').click(function () {
+					var target = $(this).next('div.resposta');
+					$('div.resposta:visible').not(target).slideUp();
+					target.slideToggle();
+
+					if ($(this).hasClass('is--active')) {
+						$(this).removeClass('is--active');
+					} else {
+						$('.pergunta h2').removeClass('is--active');
+						$(this).addClass('is--active');
+					}
+				});
+
+
+				$('.toggle').click(function () {
+					$('.toggle').removeClass('active');
+					$(this).addClass('active');
+					var target = $(this).next('ul.content');
+					$('ul.content:visible').not(target).hide('fast');
+					target.show('fast');
+				});
+
+				if ($('body').width() < 768) {
+					$('.btn-voltar').hide();
+					$('.toggle').click(function () {
+						$('.btn-voltar').show();
+						$('.btn-voltar, .title-question').click(function () {
+							$('.content').hide();
+						});
+					});
+				}
 			}
 
 			this.init();
