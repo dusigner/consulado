7ec3c36149de1e8aee222164615a296ff841f072
Brendon Guedes <brendonguedes@github.com>
2020-08-24 15:18:59 -0300

    Merge branch 'correcao banner consent

--- src/Scripts/consul2-home.js
+++ src/Scripts/consul2-home.js
@@ -26,7 +26,6 @@ import 'modules/counter_consumer_week_2020';
 // import 'modules/datalayer_track';
 import 'dataLayers/datalayer-vitrine-categorizacao';
 import 'dataLayers/datalayer-vitrine-filter-size-family';
-import 'consentCookie';
 
 Nitro.controller(
 	'home',
