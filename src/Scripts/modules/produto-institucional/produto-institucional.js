07a8bacc3edf768e250838af7cdc294189bf72a5
brendon guedes <brendon.guedes@corebiz.ag>
2020-09-23 15:49:08 -0300

    slide wip

--- src/Scripts/modules/produto-institucional/produto-institucional.js
+++ src/Scripts/modules/produto-institucional/produto-institucional.js
@@ -1,35 +1,20 @@
 'use strict';
-Nitro.module('produto-institucional', function () {
-    this.init = function () {
-        this.sliderFacilita()
-    }
-    this.sliderFacilita = function () {
-        console.info('batat')
-        // $('.container-cards').slick({
-        //     dots: true,
-        //     infinite: true,
-        //     speed: 1000,
-        //     slidesToShow: 1,
-        //     adaptiveHeight: true,
-        //     responsive: [
-        //         {
-        //             breakpoint: 667,
-        //             settings: {
-        //                 slidesToShow: 1,
-        //                 slidesToScroll: 3,
-        //                 arrows: true
-        //             }
-        //         },
-        //         {
-        //             breakpoint: 480,
-        //             settings: {
-        //                 slidesToShow: 1,
-        //                 slidesToScroll: 3,
-        //                 arrows: true
-        //             }
-        //         }
-        //     ]
-        // });
-    }
-    this.init()
+
+$('.container-cards').addClass('carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }
+    ]
 });
