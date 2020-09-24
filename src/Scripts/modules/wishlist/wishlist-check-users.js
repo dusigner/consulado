42c324ac2528ec6438f1fc6375b8e2b9f7a29ed5
Guilherme Sennes <guilherme.domingues@jussi.com.br>
2020-09-24 12:55:32 -0300

    ajustes no botão favoritar

--- src/Scripts/modules/wishlist/wishlist-check-users.js
+++ src/Scripts/modules/wishlist/wishlist-check-users.js
@@ -8,25 +8,27 @@ Nitro.module('favourite-user-not-logged', function() {
 
     this.checkIfUserIsLogged = () =>{
 
-        vtexjs.checkout.getOrderForm()
-        .done(function(orderForm) {
-            var checkUser = orderForm.loggedIn;
+        window.onload = function(){
+            vtexjs.checkout.getOrderForm()
+            .done(function(orderForm) {
+                var checkUser = orderForm.loggedIn;
 
-                if(!checkUser) {
-                    $('.wishlist__button-pdp').on('click', function(){
-                        localStorage.setItem("wishlist", skuJson.productId);
-                    });
-                } else {
-                    if(localStorage.getItem('wishlist') && !$('#wishlist-product').hasClass('wished')) {
-                        console.log('Product Id = ', localStorage.getItem('wishlist'));
+                    if(!checkUser) {
+                        $('.wishlist__button-pdp').on('click', function(){
+                            localStorage.setItem("wishlist", skuJson.productId);
+                        });
+                    } else {
+                        if(localStorage.getItem('wishlist') && !$('#wishlist-product').hasClass('wished')) {
+                            console.log('Product Id = ', localStorage.getItem('wishlist'));
 
-                        setTimeout(function(){
-                            $(".wishlist__container").find(".wishlist__button-pdp").click();
-                        }, 3000);
+                            setTimeout(function(){
+                                $(".wishlist__container").find(".wishlist__button-pdp").click();
+                            }, 1500);
+                        }
+                        localStorage.removeItem('wishlist');
                     }
-                    localStorage.removeItem('wishlist');
-                }
-        });
+            });
+        }
 
     }
 
