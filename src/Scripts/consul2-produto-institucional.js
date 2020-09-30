7c1d42e8cf4507f4a9350d3697125a1d7c587509
Rafael Lourenço Marques <rafael.marques@jussi.com.br>
2020-09-30 15:27:44 -0300

    end branch

--- src/Scripts/consul2-produto-institucional.js
+++ src/Scripts/consul2-produto-institucional.js
@@ -3,17 +3,41 @@
 
 import 'modules/produto-institucional/produto-institucional';
 import 'modules/produto-institucional/benefits';
-import 'modules/produto-institucional/specifications-institutional';
 
 Nitro.controller(
 	'produto-institucional',
 	[
 		'produto-institucional',
 		'beneficios',
-		'specifications-institutional'
 	],
 
-	function () {}
+	function () {
+		// Chave seletora para a exibição das especificações do produto com caixa ou sem caixa.
+		const elementSelector = $('.specs__measure-selector'),
+			elementBox = $('.specs__measure-box h4');
+
+		elementSelector.find('a:first').addClass('active');
+		$(`.specs__measure-box h4[data-selector=${elementSelector.find('a:first').attr('data-selector')}]`).addClass('active');
+
+		elementSelector.find('a').on('click', function () {
+			elementSelector.find('a').removeClass('active');
+			elementBox.removeClass('active');
+
+			$(this).addClass('active');
+			$(`.specs__measure-box h4[data-selector=${$(this).attr('data-selector')}]`).addClass('active');
+		});
+
+		// Chave seletora para a exibição dos setores de especificações nos dispositivos móveis.
+		$('.specs__measure, .specs__items, .specs__specs, .specs__additionalInfo, .specs__links').find('h4').on('click', function() {
+			let $this = $(this);
+			$this.toggleClass('specActive');
+			$this.parents('.specs__section').toggleClass('inactive');
+		});
+
+		$('.btn-ver-mais-especs').click(function (){
+			$('.especs-toggle').toggleClass('active-toggle');
+		});
+	}
 )
 
 let sbSlick = function () {
