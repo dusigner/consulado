9428c512adfc6912098342a4aab70550a2344e8d
Felipe Rocha <felipecortesrocha@gmail.com>
2020-10-02 10:59:13 -0300

    Inclusao classe is--active e ajuste media query

--- src/Scripts/consul2-app.js
+++ src/Scripts/consul2-app.js
@@ -192,8 +192,10 @@ window.showBannerPromotor = function(mobile, desk) {
 
     if (device === 'whats') {
 		// Container PDP
-		if(containerWhatsClass){
+		const containerWhats = document.querySelector(containerWhatsClass)
+		if(containerWhats){
 			document.querySelector(containerWhatsClass).style.display = 'block';
+			if (window.mobileCheck()) document.querySelector('.content_botoes_televendas-whats').classList.add('is--active')
 		}
 	}
 
@@ -208,20 +210,25 @@ window.showBannerPromotor = function(mobile, desk) {
 		}
 
 		// Tempo carregamento lip live chat
+		let runnedChat = false;
+		let timeoutChat;
 
-		let runned = false;
-		let timeout;
-
-		function tryToRun() {
-			if (!runned) {
+		function tryToRunChat() {
+			if (!runnedChat) {
 				if (window.liveChat) {
 					window.liveChat('init', liveChatId)
-					runned = true;
-				}else timeout = setTimeout(tryToRun, 1000);
+					runnedChat = true;
+				}else timeoutChat = setTimeout(tryToRunChat, 1000);
 			}
 		}
-		tryToRun();
+		tryToRunChat();
 	}
+}
+
+// Controle do container do promotor e liveChat para controle via Optimize
+if(document.querySelector('.container-whats')){
+	document.querySelector('.container-whats').style.display = 'none';
+	document.querySelector('.content_botoes_televendas').style.display = 'none';
 }
 
 let bannerPromotor = localStorage.getItem('bannerPromotorTestAB')
