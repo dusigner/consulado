3a0dcd35d5fa263d246b956f804de922acf0cc12
Guilherme Sennes <guilherme.domingues@jussi.com.br>
2020-10-04 04:30:54 -0300

    feature[seção de caracteristicas pdp]

--- src/Scripts/modules/produto-institucional/characteristics-block.js
+++ src/Scripts/modules/produto-institucional/characteristics-block.js
@@ -8,15 +8,31 @@ Nitro.module('characteristics', function () {
 
 	this.handleChangeImage = () => {
         let pathImage;
-        let button = $('.characteristics-block__image .image-buttons .image-buttons__item')
+        let button = $('.characteristics-block__image .image-buttons .image-buttons__item');
+        let textDescription = $('.characteristics-block__text .product-description');
+        console.log(textDescription)
         $(button).on('click', function(){
+            let buttonClicked = $(this);
             if ($(button).hasClass('is-active')) {
                 $(button).removeClass('is-active')
             }
-            $(this).addClass('is-active')
-            pathImage = $(this).attr('data-image');
+
+            $(buttonClicked).addClass('is-active')
+            pathImage = $(buttonClicked).attr('data-image');
             $('.characteristics-block__image img').attr('src', `/arquivos/${pathImage}`);
-        })
+
+            if($(textDescription).hasClass('is-active')) {
+                $(textDescription).removeClass('is-active')
+            }
+
+            $.map(textDescription, function(item){
+                let itemId = item.getAttribute('id')
+                if($(buttonClicked).attr('data-option') === itemId) {
+                    $(item).toggleClass('is-active');
+                }
+            })
+
+        });
     }
 
 	this.init();
