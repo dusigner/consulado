fb77d2e5ddfb529168f5fd3b04ef4a1df8693f8b
Dayane Siqueira <dayane.siqueira.alves@gmail.com>
2020-10-05 18:59:07 -0300

    estilização preço

--- src/Scripts/consul2-produto-institucional.js
+++ src/Scripts/consul2-produto-institucional.js
@@ -1,66 +1,59 @@
 /* global $: true, Nitro: true */
 'use strict';
-
+import 'modules/produto-institucional/produto-institucional';
 // import 'modules/product/video';
-import 'modules/product/sku-fetch';
-// import 'modules/product/gallery-v2';
- import 'modules/product/product-nav-v2';
-// import 'modules/product/details';
-// import 'modules/product/specifications-v2';
-// import 'modules/product/selos';
-// import 'modules/product/sku-select';
+// import 'modules/product/sku-fetch';
+// import 'modules/product/gallery-v3';
+// import 'modules/product/product-nav-v2';
+// import 'modules/product/details-v3';
+// import 'modules/product/specifications-v3';
+import 'modules/product/selos';
+// import 'modules/product/sku-select-v3';
 // import 'modules/product/produtos-adicionais';
 // import 'modules/product/boleto';
 // import 'modules/product/notify-me';
 // import 'modules/product/share';
 // import 'modules/product/upsell';
 // import 'modules/product/recurrence';
-// import 'modules/product/deliveryTime';
+ import 'modules/product/deliveryTime';
 // import 'modules/product/color-selector';
 // import 'modules/product/product-tags';
 // import 'modules/product/outline-products';
 // import 'modules/chaordic';
 // import 'dataLayers/dataLayer-product';
-import 'modules/caracteristicas';
-import 'modules/produto-institucional/produto-institucional';
+// import 'dataLayers/dataLayer-main-tabs';
+// import 'dataLayers/dataLayer-topbar-nav';
+// import 'modules/listagem/comparebar';
+
+// import 'modules/product/compare';
 
 Nitro.controller(
-    'produto-institucional',
-    [
-        'produto-institucional',
-        // 'chaordic',
-        // 'color-selector',
-        'sku-fetch',
-        // 'galleryv2',
-        'product-nav',
-        // 'video',
-        // 'details',
-        // 'specifications-v2',
-        // 'selos',
-        // 'sku-select',
-        // 'produtos-adicionais',
-        // 'boleto',
-        // 'share',
-        // 'upsell',
-        // 'deliveryTime',
-        // 'recurrence',
-        // 'notify-me',
-        // 'product-tags',
-        // 'dataLayer-product',
-        // 'outline-products',
-    ]
-)
-
-let sbSlick = function () {
-    var screenWidth = $(window).width();
-    //Slick slider intro mobile
-    if (screenWidth <= '660') {
-        $('.container-cards').slick({
-            autoplay: true,
-            autoplaySpeed: 1000,
-            arrows: false,
-            dots: true
-        });
-    }
-    this.sbSlick()
-};
+	'produto-institucional',
+	[	'produto-institucional',
+		// 'chaordic',
+		// 'color-selector',
+		// 'sku-fetch',
+		// 'gallery-v3',
+		// 'product-nav',
+		// 'video',
+		// 'details-v3',
+		// 'specifications-v3',
+		'selos',
+		// 'sku-select-v3',
+		// 'produtos-adicionais',
+		// 'boleto',
+		// 'share',
+		// 'upsell',
+		'deliveryTime',
+		// 'recurrence',
+		// 'notify-me',
+		// 'product-tags',
+		// 'dataLayer-product',
+		// 'outline-products',
+		// 'dataLayer-main-tabs',
+		// 'dataLayer-topbar-nav',
+		// 'topBarV2',
+		// 'comparebar',
+		// 'compare'
+	]
+);
--- src/Scripts/modules/produto-institucional/produto-institucional.js
+++ src/Scripts/modules/produto-institucional/produto-institucional.js
@@ -1,3 +1,30 @@
+var self = this,
+$body = $('body');
+var $reference = $('.reference'),
+			$productSku = $('.productSku');
+
+		//TROCA DE NOMES PRODUCT / SKUREF
+		$(document)
+			.on('skuSelected.vtex', function () {
+				$reference.addClass('hide');
+				$productSku.removeClass('hide');
+			})
+			.on('skuUnselected.vtex', function () {
+				$productSku.addClass('hide');
+				$reference.removeClass('hide');
+			});
+
+		$(document).ajaxComplete(function (e, xhr, settings) {
+			if (/outrasformasparcelamento/.test(settings.url)) {
+				self.valoresParcelas();
+			}
+		});
+
+		$('input:radio').click(function () {
+			$('#showVoltage').text($(this).val());
+		}); // Mostra a a voltagem selecionada
+
+
 $('.container-cards').slick({
     dots: false,
     infinite: false,
@@ -16,29 +43,89 @@ $('.container-cards').slick({
     }]
 });
 
-	this.init = () => {
-		this.caracteristicas();
-	};
+//Opções de parcelamento
+self.valoresParcelas = function () {
+	console.log('deu');
+	var $valoresParcelas = $('.valores-parcelas'),
+		$showParcelas = $valoresParcelas.find('.titulo-parcelamento'),
+		$opcoesParcelamento = $valoresParcelas.find('.other-payment-method-ul'),
+		installmentQuantity = skuJson.skus[0].installments;
 
-	this.caracteristicas = () => {
-		$('.caracteristicas-lp').append(`<h1 id='caracteristicas-nome'></h1>
-		<p id='caracteristicas-preco-de'></p>
-		<p id='caracteristicas-preco-para'></p>
-		<p id='caracteristicas-comprar'></p>`);
+	if (installmentQuantity === 1) {
+		$('.formas-pagamento-container').css('display', 'none');
+	}
 
-		fetch('/api/catalog_system/pub/products/search/geladeira-consul-frost-free-duplex-397-litros-evox-com-freezer-embaixo-cre44ak/p')
-			.then(response =>  response.json())
-			.then(result => {
-				$("#caracteristicas-nome").html(result[0].items[0].nameComplete);
-				$("#caracteristicas-preco-de").html(result[0].items[0].sellers[0].commertialOffer.ListPrice);
-				$("#caracteristicas-preco-para").html(result[0].items[0].sellers[0].commertialOffer.Price);
-				const link = result[0].items[0].sellers[0].addToCartLink;
-				$("#caracteristicas-comprar").attr('href',`${link}`);
-				})
-			.catch(function(error) {
-				console.log('Error: ' + error.message);
-			});
+	//title parcelas
+	$(".titulo-parcelamento").html("Veja todas as formas de parcelamento");
 
-		}
-	this.init();
+	$opcoesParcelamento.find('li').each(function () {
+		var $numeroParcelas = $(this).find('span:first-child'),
+			numeroParcelas = $numeroParcelas.text().split('X')[0],
+			$valorParcela = $(this).find('strong'),
+			valorParcela = parseFloat(
+				$valorParcela
+					.text()
+					.replace('.', '')
+					.replace(',', '.')
+					.split('R$')[1]
+			),
+			text = $numeroParcelas.text().replace('de', ''),
+			precoTotal = parseFloat(numeroParcelas * valorParcela).toFixed(2);
 
+		$(this).append(
+			'<span class="valor-total">Total: R$ ' + precoTotal.toString().replace('.', ',') + '</span>'
+		);
+		$numeroParcelas.text(text);
+		$valorParcela.text('de ' + $valorParcela.text());
+	});
+
+	// Exibe as opções de parcelamento
+	$showParcelas.click(function () {
+		$(this).parents('.formas-pagamento-container').toggleClass('is--active');
+	});
+
+
+	$('.select-voltage .select.skuList label').click(function () {
+		$valoresParcelas.find('>p').slideUp();
+		$opcoesParcelamento.slideUp();
+	});
+};
+
+self.valoresParcelas();
+
+
+
+//Cep
+const $document = $(document),
+	$cepInput = '#calculoFrete .prefixo input';
+	$document.on('change', $cepInput, ({ currentTarget }) => {
+	const $element = $(currentTarget);
+		$element.val() ? $element.parent().addClass('has--cep') : $element.parent().removeClass('has--cep');
+	});
+
+
+	// this.init = () => {
+	// 	this.caracteristicas();
+	// };
+
+	// this.caracteristicas = () => {
+	// 	$('.caracteristicas-lp').append(`<h1 id='caracteristicas-nome'></h1>
+	// 	<p id='caracteristicas-preco-de'></p>
+	// 	<p id='caracteristicas-preco-para'></p>
+	// 	<a id='caracteristicas-comprar'></a>`);
+
+	// 	fetch('/api/catalog_system/pub/products/search/geladeira-consul-frost-free-duplex-397-litros-evox-com-freezer-embaixo-cre44ak/p')
+	// 		.then(response =>  response.json())
+	// 		.then(result => {
+	// 			$("#caracteristicas-nome").html(result[0].items[0].nameComplete);
+	// 			$("#caracteristicas-preco-de").html(result[0].items[0].sellers[0].commertialOffer.ListPrice);
+	// 			$("#caracteristicas-preco-para").html(result[0].items[0].sellers[0].commertialOffer.Price);
+	// 			const link = result[0].items[0].sellers[0].addToCartLink;
+	// 			$("#caracteristicas-comprar").attr('href',`${link}`);
+	// 			})
+	// 		.catch(function(error) {
+	// 			console.log('Error: ' + error.message);
+	// 		});
+
+	// 	}
+	// this.init();
