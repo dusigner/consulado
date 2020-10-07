b6577557de26d85ba546865c3d3602e1f0d0832e
Guilherme Sennes <guilherme.domingues@jussi.com.br>
2020-10-07 17:59:42 -0300

    feature[tagueamento main banner]

--- src/Scripts/consul2-produto-institucional.js
+++ src/Scripts/consul2-produto-institucional.js
@@ -22,11 +22,13 @@
 // import 'modules/chaordic';
 // import 'dataLayers/dataLayer-product';
 import 'modules/produto-institucional/produto-institucional';
+import 'modules/produto-institucional/main-banner-datalayer';
 
 Nitro.controller(
     'produto-institucional',
     [
-        'produto-institucional'
+        'produto-institucional',
+        'main-banner-datalayer',
         // 'chaordic',
         // 'color-selector',
         // 'sku-fetch',
@@ -47,8 +49,60 @@ Nitro.controller(
         // 'product-tags',
         // 'dataLayer-product',
         // 'outline-products',
-    ]
+    ],
+    function(){}
 )
+
+$(document).ready(function(){
+    // Check if element is on the screen
+    // Necessário para checagem de viability
+	$.fn.isOnScreen = function (x, y) {
+		if (x == null || typeof x == 'undefined') x = 1;
+		if (y == null || typeof y == 'undefined') y = 1;
+
+		var win = $(window);
+
+		var viewport = {
+			top: win.scrollTop(),
+			left: win.scrollLeft()
+		};
+		viewport.right = viewport.left + win.width();
+		viewport.bottom = viewport.top + win.height();
+
+		var height = this.outerHeight();
+		var width = this.outerWidth();
+
+		if (!width || !height) {
+			return false;
+		}
+
+		var bounds = this.offset();
+		bounds.right = bounds.left + width;
+		bounds.bottom = bounds.top + height;
+
+		var visible = !(
+			viewport.right < bounds.left ||
+			viewport.left > bounds.right ||
+			viewport.bottom < bounds.top ||
+			viewport.top > bounds.bottom
+		);
+
+		if (!visible) {
+			return false;
+		}
+
+		var deltas = {
+			top: Math.min(1, (bounds.bottom - viewport.top) / height),
+			bottom: Math.min(1, (viewport.bottom - bounds.top) / height),
+			left: Math.min(1, (bounds.right - viewport.left) / width),
+			right: Math.min(1, (viewport.right - bounds.left) / width)
+		};
+
+		return (
+			deltas.left * deltas.right >= x && deltas.top * deltas.bottom >= y
+		);
+	};
+})
 
 let sbSlick = function () {
     var screenWidth = $(window).width();
