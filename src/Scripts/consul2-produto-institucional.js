f0973e64622cce14150fc42f5974206b4f3157d7
Rafael Lourenço Marques <rafael.marques@jussi.com.br>
2020-10-07 17:25:51 -0300

    aplicado tagueamento na barra fixa.

--- src/Scripts/consul2-produto-institucional.js
+++ src/Scripts/consul2-produto-institucional.js
@@ -2,6 +2,7 @@
 'use strict';
 
 import 'modules/produto-institucional/benefits';
+import { pushDataLayer } from 'modules/_datalayer-inline';
 import 'modules/produto-institucional/produto-institucional';
 import 'modules/produto-institucional/specifications';
 
@@ -14,42 +15,82 @@ Nitro.controller(
 	],
 
 	function () {
-		$('.navigation-sticked-link[href^="#"]').on('click', function(e) {
-			e.preventDefault();
-			$('.navigation-sticked-link').removeClass('active');
-			$(this).addClass('active');
+		const fixedBar = {
+			init: function() {
+				fixedBar.carouselMobile();
+				fixedBar.anchor();
+			},
 
-			let hash = $(this).attr('href'),
-				targetOffset = $(hash).offset().top;
+			carouselMobile: function() {
+				if($(window).width() < 700) {
+					$('.navigation-sticked-list').slick({
+						arrows: false,
+						dots: false,
+						infinite: false,
+						mobileFirst: true,
+						slidesToScroll: 1,
+						slidesToShow: 1,
+						variableWidth: true,
+						responsive: [
+							{
+								breakpoint: 300,
+								settings: {
+									slidesToScroll: 1,
+									slidesToShow: 2,
+								}
+							}
+						]
+					});
+				}
+			},
 
-			$('html, body').animate({
-				scrollTop: targetOffset - 56
-			}, 500);
-		});
+			anchor: function() {
+				$('.navigation-sticked-link[href^="#"]').on('click', function(e) {
+					e.preventDefault();
 
-		/**
-		 * fixed top bar with carousel in mobile;
-		 */
-		if($(window).width() < 700) {
-			$('.navigation-sticked-list').slick({
-				arrows: false,
-				dots: false,
-				infinite: false,
-				mobileFirst: true,
-				slidesToScroll: 1,
-				slidesToShow: 1,
-				variableWidth: true,
-				responsive: [
-					{
-						breakpoint: 300,
-						settings: {
-							slidesToScroll: 1,
-							slidesToShow: 2,
-						}
-					}
-				]
+					const label = $(this).text().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^\w\-]+/g, '_').toLowerCase();
+
+					$('.navigation-sticked-link').removeClass('active');
+					$(this).addClass('active');
+
+					let hash = $(this).attr('href'),
+						targetOffset = $(hash).offset().top;
+
+					$('html, body').animate({
+						scrollTop: targetOffset - 56
+					}, 500);
+
+					pushDataLayer(
+						'PDP_institucional',
+						'barra_fixada',
+						`${label}`
+					);
+				});
+
+				$('.navigation-sticked-cta[href^="#"]').on('click', function(e) {
+					e.preventDefault();
+
+					let hash = $(this).attr('href'),
+						targetOffset = $(hash).offset().top;
+
+					$('html, body').animate({
+						scrollTop: targetOffset - 56
+					}, 500);
+
+					pushDataLayer(
+						'PDP_institucional',
+						'barra_fixada',
+						'clique_botao_eu_quero'
+					);
+				});
+			},
+		};
+
+		(function (window, document, $) {
+			$(function () {
+				fixedBar.init();
 			});
-		}
+		})(window, document, jQuery);
 	}
 )
 
