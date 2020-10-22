892b37b3c61161dc324507bfcf2618b46c13af97
Guilherme Sennes <guilherme.domingues@jussi.com.br>
2020-10-22 19:59:39 -0300

    feature[cupons vitrine bf 2020]

--- src/Scripts/modules/coupons-blackfriday-2020.js
+++ src/Scripts/modules/coupons-blackfriday-2020.js
@@ -7,28 +7,51 @@ Nitro.module('coupons-blackfriday-2020', function() {
 	let self = this;
 
 	self.init = () => {
+
 		self.getCouponsBf().then(coupons => {
 			self.formatCouponsBlackFridayVitrine(coupons);
 		});
+
+		self.copyCouponToClioBoard();
 	}
 
 	self.formatCouponsBlackFridayVitrine = (coupon, index) => {
-		let productBoxes = $('.box-produto .FlagsHightLight p');
+		let productBoxes
+		if($('body').hasClass('home') || $('body').hasClass('categoria') || $('body').hasClass('listagem')){
+			productBoxes = $('.box-produto .FlagsHightLight p');
+		} else {
+			productBoxes = $('.prod-image .prod-selos p');
+		}
+
 		$(productBoxes).map(function(product, index){
 			let produto = $(this);
 			if($(produto).attr('class').indexOf('blackfriday-2020') > -1) {
 				let collectionId = $(produto).attr('class').split('-');
 				$(coupon).map(function(index, c){
-					console.info(c);
 					if($(c).idCollection === collectionId[-1]) {
 						$(produto).parent().find('.cns__promo__ganhe__de-volta-__-702f8f__500').text($(c)[0].coupon)
 					}
 				})
-				// console.info(collectionId);
 			}
 		})
 
-		// console.info(coupon);
+
+	}
+
+	self.copyCouponToClioBoard = () => {
+		const copyCouponButton = $('.cns__promo__ganhe__de-volta-__-702f8f__500');
+
+		$(copyCouponButton).on('click', function(e){
+			e.preventDefault();
+
+			let copiedCoupon = $(this).text();
+			$(this).addClass('copied');
+			var $temp = $("<input>");
+			$("body").append($temp);
+			$temp.val($(this).text()).select();
+			document.execCommand("copy");
+			$temp.remove();
+		})
 	}
 
 	self.getCouponsBf = () => {
@@ -44,6 +67,19 @@ Nitro.module('coupons-blackfriday-2020', function() {
 
 		return deferred.promise();
 	};
-	// Start it
-	self.init();
+
+	if($('body').hasClass('categoria') || $('body').hasClass('listagem')) {
+		$(document).ajaxStop(function () {
+
+			self.init();
+
+			if ($('.see-more').length && $('.see-more button').hasClass('hide')) {
+				console.info('batata');
+				$(this).unbind("ajaxStop");
+			}
+
+		});
+	} else {
+		self.init();
+	}
 });
