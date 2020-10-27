b55428c76158896d785b4272818f53a41d2a7b0f
brendon guedes <brendon.guedes@corebiz.ag>
2020-10-27 15:53:01 -0300

    WIP css

--- src/Scripts/modules/product/_slick-module-top.js
+++ src/Scripts/modules/product/_slick-module-top.js
@@ -1,86 +1,86 @@
-Nitro.module('slider-module-prateleira', function () {
+// Nitro.module('slider-module-prateleira', function () {
 
-    this.slickStartModule = () => {
-        const $sliderPrateleira = $('.module-top10');
+//     this.slickStartModule = () => {
+//         const $sliderPrateleira = $('.module-top10');
 
-        $sliderPrateleira
-            .slick({
-                arrows: true,
-                dots: true,
-                infinite: true,
-                speed: 300,
-                slidesToShow: 5,
-                slidesToScroll: 5,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
-                            infinite: true,
-                            dots: true
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-    };
+//         $sliderPrateleira
+//             .slick({
+//                 arrows: true,
+//                 dots: true,
+//                 infinite: true,
+//                 speed: 300,
+//                 slidesToShow: 5,
+//                 slidesToScroll: 5,
+//                 responsive: [
+//                     {
+//                         breakpoint: 1024,
+//                         settings: {
+//                             slidesToShow: 3,
+//                             slidesToScroll: 3,
+//                             infinite: true,
+//                             dots: true
+//                         }
+//                     },
+//                     {
+//                         breakpoint: 600,
+//                         settings: {
+//                             slidesToShow: 2,
+//                             slidesToScroll: 2
+//                         }
+//                     },
+//                     {
+//                         breakpoint: 480,
+//                         settings: {
+//                             slidesToShow: 1,
+//                             slidesToScroll: 1
+//                         }
+//                     }
+//                     // You can unslick at a given breakpoint now by adding:
+//                     // settings: "unslick"
+//                     // instead of a settings object
+//                 ]
+//             });
+//     };
 
-    this.init = () => {
-        this.slickStartModule();
-    };
+//     this.init = () => {
+//         this.slickStartModule();
+//     };
 
-    this.init();
-});
+//     this.init();
+// });
 
 
-var self = this,
-    $slider = $('section.vitrines:not(.vitrine-ofertas-interesses, .vitrine-ofertas-alavancas)').find('.prateleira-slider .prateleira>ul').not('.slick-initialized');
+// var self = this,
+//     $slider = $('section.vitrines:not(.vitrine-ofertas-interesses, .vitrine-ofertas-alavancas)').find('.prateleira-slider .prateleira>ul').not('.slick-initialized');
 
-//INICIA CHAMADA DAS VITRINES CHAORDIC
-//chaordic.init('home');
+// //INICIA CHAMADA DAS VITRINES CHAORDIC
+// //chaordic.init('home');
 
-this.setupSlider = function ($currentSlider) {
-    $currentSlider.not('.slick-initialized').slick({
-        infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 990,
-                settings: {
-                    dots: true,
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    dots: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+// this.setupSlider = function ($currentSlider) {
+//     $currentSlider.not('.slick-initialized').slick({
+//         infinite: false,
+//         slidesToShow: 4,
+//         slidesToScroll: 3,
+//         responsive: [
+//             {
+//                 breakpoint: 990,
+//                 settings: {
+//                     dots: true,
+//                     slidesToShow: 2,
+//                     slidesToScroll: 2
+//                 }
+//             },
+//             {
+//                 breakpoint: 480,
+//                 settings: {
+//                     dots: true,
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     });
 
-    //ajusta para mobile - prateleira slider
-    //$('section.slider .prateleira-slider .prateleira ul').find('.detalhes>a').addClass('col-xs-6 col-md-12');
-};
+//     //ajusta para mobile - prateleira slider
+//     //$('section.slider .prateleira-slider .prateleira ul').find('.detalhes>a').addClass('col-xs-6 col-md-12');
+// };
