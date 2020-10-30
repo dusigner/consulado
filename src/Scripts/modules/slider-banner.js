5660bf84a27f150e25ea62ee34be60ed0d4b3185
Rafael Lourenço Marques <rafael.marques@jussi.com.br>
2020-10-30 16:29:25 -0300

    ajustes para teste ab;

--- src/Scripts/modules/slider-banner.js
+++ src/Scripts/modules/slider-banner.js
@@ -4,9 +4,13 @@ Nitro.module('slider-banner', [], function () {
 	var $buttonOpenRegulamento = $('.open-regulamento');
 
 	this.setupMainSlider = function () {
+		this.dataAttribute();
+		this.cloneMainSlider();
+
 		var qtdBanners,
 			//$bannerPrincipal = $(window).width() >= 470 ? $('.banners .banner-principal') : $('.banners-mobile .banner-principal');
-			$bannerPrincipal = $('.banners .banner-principal')
+			$bannerPrincipal = $('.banners .banner-principal'),
+			$bannerPrincipalSlides = $('.banners .banner-principal-gbp .banner-slides');
 
 		$bannerPrincipal.on('init', function () {
 			//qtdBanners = $('.banners .banner-principal.slides .slick-slide:not(.slick-cloned)').length;
@@ -14,9 +18,29 @@ Nitro.module('slider-banner', [], function () {
 			//if ($(window).width() <= 470) qtdBanners = $('.banners-mobile .banner-principal.slides .slick-slide:not(.slick-cloned)').length;
 		});
 
+		$bannerPrincipal.slick({
+			autoplay: true,
+			autoplaySpeed: 7000,
+			mobileFirst: true,
+			dots: false,
+			arrows: true,
+			responsive: [{
+				breakpoint: 470,
+				settings: {
+					arrows: true,
+					dots: false
+				},
+			}]
+		}).on('afterChange', function (event, slick, currentSlide) {
+			if ($('#modal-regulamento-banner').text() !== '' && qtdBanners > 1 && currentSlide === qtdBanners - 1) {
+				$buttonOpenRegulamento.show();
+			} else {
+				$buttonOpenRegulamento.hide();
+			}
+		});
 
-		$bannerPrincipal
-			.slick({
+		if($bannerPrincipalSlides.length) {
+			$bannerPrincipalSlides.slick({
 				autoplay: true,
 				autoplaySpeed: 7000,
 				mobileFirst: true,
@@ -29,14 +53,14 @@ Nitro.module('slider-banner', [], function () {
 						dots: false
 					},
 				}]
-			})
-			.on('afterChange', function (event, slick, currentSlide) {
+			}).on('afterChange', function (event, slick, currentSlide) {
 				if ($('#modal-regulamento-banner').text() !== '' && qtdBanners > 1 && currentSlide === qtdBanners - 1) {
 					$buttonOpenRegulamento.show();
 				} else {
 					$buttonOpenRegulamento.hide();
 				}
 			});
+		}
 
 		$buttonOpenRegulamento.click(function (e) {
 			e.preventDefault();
@@ -49,6 +73,39 @@ Nitro.module('slider-banner', [], function () {
 				}
 			});
 		});
+	};
+
+	this.dataAttribute = function () {
+		const bannerPrincipal = document.querySelectorAll('.banner-principal');
+
+		if (bannerPrincipal) {
+			bannerPrincipal.forEach(bp => {
+				const boxBanner = bp.querySelectorAll('.box-banner');
+
+				if (boxBanner) {
+					boxBanner.forEach((bb, index) => {
+						bb.dataset.bannerIndex = index;
+					})
+				}
+			});
+		}
+	};
+
+	this.cloneMainSlider = function () {
+		const banners = document.querySelector('.banners');
+		const bannerPrincipal = banners.querySelector('.banner-principal');
+		const bannerPrincipalGBP = banners.querySelector('.banner-principal-gbp .banner-slides');
+
+		if(bannerPrincipal && bannerPrincipalGBP) {
+			const boxBanner = bannerPrincipal.querySelectorAll('.box-banner');
+
+			if (boxBanner) {
+				boxBanner.forEach(bb => {
+					const bbc = bb.cloneNode(true);
+					bannerPrincipalGBP.appendChild(bbc)
+				});
+			}
+		}
 	};
 
 	this.setupMainSlider();
